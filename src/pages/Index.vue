0bcb7ecb30bbafee20ad323df4b6843485e79765
Marine Boudeau <m14868@gmail.com>
2020-02-12 04:34:09 +0100

    Add more details to landing page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,7 +3,12 @@
 
     <h1>DesignGouv</h1>
 
-    <p>Bienvenue sur le site de la communauté des <strong>designers numériques de l'État</strong>. Vous y trouverez prochainement un ensemble de ressources utiles lors du développement de services publics numériques centrés sur les usagers.</p>
+    <p>Bienvenue ! Cet espace est dédié aux agents publics de l'État soucieux de créer des services numériques accessibles pour tous. Vous y trouverez très prochainement :</p>
+    <ul>
+      <li>Des guides sur le rôle du design et l'importance de concevoir des expériences utiles, utilisables et agréables</li>
+      <li>Les outils pour mieux faire (bonnes pratiques, accessibilité numérique, formations, etc.)</li>
+      <li>Notre offre de services aux administrations (<a href="https://calendly.com/designgouv" target="_blank">bureaux ouverts</a> le vendredi, Commando UX pour les porteurs de projets de l'<a href="https://observatoire.numerique.gouv.fr" target="_blank">Observatoire</a>, etc.)</li>
+    </ul>
     <p class="subtitle">Pour recevoir nos actualités, inscrivez-vous à notre newsletter :</p>
 
     <a href="http://eepurl.com/gLJCsb" class="button">Je m'inscris à la newsletter</a>

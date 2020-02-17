25bda4abd496e665c697b8ad368461f3b7f0637f
Ugo <jeanmichelpaul@icloud.com>
2020-02-17 18:05:09 +0100

    Add HomeItem component

---
+++ src/components/HomeItem.vue
@@ -0,0 +1,112 @@
+<template>
+
+  <div class="homeItem" :style="colors">
+
+    <g-image class="icon" :alt="iconAlt" :src="getImgUrl(iconSrc)"/>
+
+    <p><g-link :to="link"><h2>{{ title }}</h2></g-link></p>
+
+    <p class="homeItemText" v-html="text"></p>
+
+    <g-link :to="link" class="button">{{ buttonText }}</g-link>
+
+  </div>
+
+</template>
+
+
+<script>
+
+export default {
+  name: 'HomeItem',
+  props: {
+    title: String,
+    iconSrc: String,
+    iconAlt: String,
+    text: String,
+    buttonText: String,
+    link: String,
+    color: String,
+    colorLight: String,
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    getImgUrl (src) {
+      return require('../assets/images/' + src)
+    }
+  },
+  computed: {
+    colors () {
+      return {
+        '--color': this.color,
+        '--colorLight': this.colorLight
+      }
+    }
+  }
+}
+
+</script>
+
+
+<style scoped lang="scss">
+
+.homeItem {
+  margin: 48px 0 108px 0;
+
+  @media only screen and (max-width: 600px) {
+    margin: 28px 0 68px;
+
+    p {
+      margin: 12px 0;
+    }
+  }
+
+  h2 {
+    box-shadow:
+      inset 0 -0.05em white,
+      inset 0 -0.4em var(--colorLight);
+    display: inline;
+    transition: .1s all;
+
+    &:hover {
+      box-shadow:
+        inset 0 -0.05em white,
+        inset 0 -1.4em var(--colorLight);
+    }
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--color);
+  }
+
+  .homeItemText {
+    margin-top: 24px;
+  }
+
+  .button {
+    color: var(--color);
+    border: 2px solid var(--color);
+    background-color: white;
+    border-radius: 32px;
+    padding: 8px 24px;
+    text-decoration: none;
+    font-weight: bold;
+    display: inline-block;
+    margin: 10px 0;
+
+    @media only screen and (max-width: 600px) {
+      font-size: 0.875em;
+    }
+
+    &:hover {
+      background: var(--color);
+      color: white;
+    }
+  }
+}
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,55 +5,59 @@
       <p><g-image class="logo" alt="DesignGouv" src="~/assets/images/logo.svg"/></p>
 
       <p>
-        <a href=""><g-image class="icon" alt="Star icon" src="~/assets/images/design.svg"/></a>
-        <a href=""><g-image class="icon" alt="Tool icon" src="~/assets/images/tools.svg"/></a>
-        <a href=""><g-image class="icon" alt="Lifebuoy icon" src="~/assets/images/services.svg"/></a>
-        <a href=""><g-image class="icon" alt="Book icon" src="~/assets/images/courses.svg"/></a>
-        <a href=""><g-image class="icon" alt="Tchat icon" src="~/assets/images/community.svg"/></a>
+        <g-link to=""><g-image class="icon" alt="Star icon" src="~/assets/images/design.svg"/></g-link>
+        <g-link to=""><g-image class="icon" alt="Tool icon" src="~/assets/images/tools.svg"/></g-link>
+        <g-link to=""><g-image class="icon" alt="Lifebuoy icon" src="~/assets/images/services.svg"/></g-link>
+        <g-link to=""><g-image class="icon" alt="Book icon" src="~/assets/images/courses.svg"/></g-link>
+        <g-link to=""><g-image class="icon" alt="Tchat icon" src="~/assets/images/community.svg"/></g-link>
       </p>
 
       <h1>Bienvenue ! Cet espace est dédié aux agents publics de l'État soucieux de créer des services numériques accessibles pour tous.</h1>
     </div>
 
-    <div class="homeItem" style="--color: #2854D6; --colorLight: #ACBDF1;">
-      <g-image class="icon" alt="Star icon" src="~/assets/images/design.svg"/>
+    <HomeItem
+      title="Découvrez le Design numérique"
+      text="Le design permet de rendre les <b>services</b> numériques plus <b>simples</b>, <b>accessibles</b> et <b>agréables</b> pour tous les usagers. Concevoir par le design, c’est mettre en oeuvre une démarche de création centrée sur les usagers, tout en s’appuyant sur de bonnes pratiques reconnues. Nous vous expliquons tout cela en détails."
+      buttonText="Le design numérique en détails →"
+      link=""
+      iconSrc="design.svg"
+      iconAlt="Star icon"
+      color="#2854D6"
+      colorLight="#ACBDF1"
+    />
 
-      <p><a href=""><h2>Découvrez le Design numérique</h2></a></p>
+    <HomeItem
+      title="Les outils à votre disposition"
+      text="De nombreux outils peuvent vous aider à améliorer la qualité de vos démarches vous-même. Utilisabilité, accessibilité, protection des données personnelles, bonnes pratiques, laissez vous guider."
+      buttonText="J'accède aux outils →"
+      link=""
+      iconSrc="tools.svg"
+      iconAlt="Tool icon"
+      color="#44A74C"
+      colorLight="#84CD86"
+    />
 
-      <p class="homeItemText">Le design permet de rendre les <b>services</b> numériques plus <b>simples</b>, <b>accessibles</b> et <b>agréables</b> pour tous les usagers. Concevoir par le design, c’est mettre en oeuvre une démarche de création centrée sur les usagers, tout en s’appuyant sur de bonnes pratiques reconnues. Nous vous expliquons tout cela en détails.</p>
+    <HomeItem
+      title="Un besoin en Design ? Nous vous aidons."
+      text="Vous avez un besoin urgent ou des questions concernant l’amélioration de vos démarches, nous pouvons vous accompagner."
+      buttonText="J'ai un besoin en Design →"
+      link=""
+      iconSrc="services.svg"
+      iconAlt="Lifebuoy icon"
+      color="#FF5D52"
+      colorLight="#FFA89E"
+    />
 
-      <a href="" class="button">Le design numérique en détails →</a>
-    </div>
-
-    <div class="homeItem" style="--color: #44A74C; --colorLight: #84CD86;">
-      <g-image class="icon" alt="Tool icon" src="~/assets/images/tools.svg"/>
-
-      <p><a href=""><h2>Les outils à votre disposition</h2></a></p>
-
-      <p class="homeItemText">De nombreux outils peuvent vous aider à améliorer la qualité de vos démarches vous-même. Utilisabilité, accessibilité, protection des données personnelles, bonnes pratiques, laissez vous guider.</p>
-
-      <a href="" class="button">J'accède aux outils →</a>
-    </div>
-
-    <div class="homeItem" style="--color: #FF5D52; --colorLight: #FFA89E;">
-      <g-image class="icon" alt="Lifebuoy icon" src="~/assets/images/services.svg"/>
-
-      <p><a href=""><h2>Un besoin en Design ? Nous vous aidons.</h2></a></p>
-
-      <p class="homeItemText">Vous avez un besoin urgent ou des questions concernant l’amélioration de vos démarches, nous pouvons vous accompagner.</p>
-
-      <a href="" class="button">J'ai un besoin en Design →</a>
-    </div>
-
-    <div class="homeItem" style="--color: #AE8C24; --colorLight: #E2C56F;">
-      <g-image class="icon" alt="Book icon" src="~/assets/images/courses.svg"/>
-
-      <p><a href=""><h2>Des formations pour vous et vos équipes</h2></a></p>
-
-      <p class="homeItemText">De nombreuses formations sont disponibles, certaines accessibles gratuitement, que ce soit pour vous former personnellement ou en équipe sur les divers sujets liés au design et à la qualité des services numérique, l’accessibilité etc. Nous proposons également un accompagnement sur ces sujets.</p>
-
-      <a href="" class="button">Je découvre les formations →</a>
-    </div>
+    <HomeItem
+      title="Des formations pour vous et vos équipes"
+      text="De nombreuses formations sont disponibles, certaines accessibles gratuitement, que ce soit pour vous former personnellement ou en équipe sur les divers sujets liés au design et à la qualité des services numérique, l’accessibilité etc. Nous proposons également un accompagnement sur ces sujets."
+      buttonText="Je découvre les formations →"
+      link=""
+      iconSrc="courses.svg"
+      iconAlt="Book icon"
+      color="#AE8C24"
+      colorLight="#E2C56F"
+    />
 
     <div class="card" style="--color: #1B998B; --colorLight: #3CDDCA;">
       <g-image class="icon" alt="Tchat icon" src="~/assets/images/community.svg"/>
@@ -70,14 +74,18 @@
   </Layout>
 </template>
 
-
-<style>
-</style>
-
 <script>
+import HomeItem from '~/components/HomeItem.vue'
+
 export default {
+  components: {
+    HomeItem
+  },
   metaInfo: {
-    title: 'DesignGouv - Le design numérique au service des administrations'
+    title: "DesignGouv - Le design numérique au service des administrations",
+    meta: [
+      { name: "description", content: "Cet espace est dédié aux agents publics de l'État soucieux de créer des services numériques accessibles pour tous." }
+    ],
   }
 }
 </script>

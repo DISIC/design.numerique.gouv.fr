cd2f379a5c1a43a2e84f4b661a04e201cadd0200
Ugo <jeanmichelpaul@icloud.com>
2020-02-17 19:01:18 +0100

    Add new pages

---
+++ src/pages/Formations.vue
@@ -0,0 +1,68 @@
+<template>
+  <Layout>
+
+    <div class="cover">
+     <div class="coverContainer">
+
+      <p><h1>Les formations</h1></p>
+
+      <p>
+        <g-image class="icon" alt="Star icon" src="~/assets/images/courses.svg"/>
+        <g-image class="icon" alt="Star icon" src="~/assets/images/courses.svg"/>
+        <g-image class="icon" alt="Star icon" src="~/assets/images/courses.svg"/>
+      </p>
+
+      <p>Bienvenue ! Cet espace est dédié aux agents publics de l'État soucieux de créer des services numériques accessibles pour tous.</p>
+
+      </div>
+    </div>
+
+    <div class="card" style="--color: #1B998B; --colorLight: #3CDDCA;">
+      <g-image class="icon" alt="Tchat icon" src="~/assets/images/community.svg"/>
+
+      <p><a href="http://eepurl.com/gLJCsb"><h2>Rejoignez la communauté&nbsp;!</h2></a></p>
+
+      <p class="homeItemText">DesignGouv, c’est aussi une communauté qui réunis celles et ceux qui s’engagent pour promouvoir et faire vivre la culture design au sein des administrations. Nous organisons régulièrement des évènements, des rencontres, des discussions. Rejoignez-nous !</p>
+
+      <a href="http://eepurl.com/gLJCsb" class="button">Je m'inscris à la newsletter</a>
+
+      <p>Sur Twitter : <a href="https://twitter.com/designgouv" title="Twitter" target="_blank">@DesignGouv</a></p>
+    </div>
+
+  </Layout>
+</template>
+
+<script>
+import HomeItem from '~/components/HomeItem.vue'
+
+export default {
+  components: {
+  },
+  metaInfo: {
+    title: "DesignGouv - Le design numérique au service des administrations",
+    meta: [
+      { name: "description", content: "Cet espace est dédié aux agents publics de l'État soucieux de créer des services numériques accessibles pour tous." }
+    ],
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+@import "src/assets/scss/_vars.scss";
+
+  h1 {
+    box-shadow:
+      inset 0 -0.05em white,
+      inset 0 -0.4em $brown-light;
+    display: inline;
+    transition: .1s all;
+
+    &:hover {
+      box-shadow:
+        inset 0 -0.05em white,
+        inset 0 -1.4em $brown-light;
+    }
+  }
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,24 +2,28 @@
   <Layout>
 
     <div class="cover">
-      <p><g-image class="logo" alt="DesignGouv" src="~/assets/images/logo.svg"/></p>
+      <div class="coverContainer">
 
-      <p>
-        <g-link to=""><g-image class="icon" alt="Star icon" src="~/assets/images/design.svg"/></g-link>
-        <g-link to=""><g-image class="icon" alt="Tool icon" src="~/assets/images/tools.svg"/></g-link>
-        <g-link to=""><g-image class="icon" alt="Lifebuoy icon" src="~/assets/images/services.svg"/></g-link>
-        <g-link to=""><g-image class="icon" alt="Book icon" src="~/assets/images/courses.svg"/></g-link>
-        <g-link to=""><g-image class="icon" alt="Tchat icon" src="~/assets/images/community.svg"/></g-link>
-      </p>
+        <p><g-image class="logo" alt="DesignGouv" src="~/assets/images/logo.svg"/></p>
 
-      <h1>Bienvenue ! Cet espace est dédié aux agents publics de l'État soucieux de créer des services numériques accessibles pour tous.</h1>
+        <p>
+          <g-link to=""><g-image class="icon" alt="Star icon" src="~/assets/images/design.svg"/></g-link>
+          <g-link to=""><g-image class="icon" alt="Tool icon" src="~/assets/images/tools.svg"/></g-link>
+          <g-link to=""><g-image class="icon" alt="Lifebuoy icon" src="~/assets/images/services.svg"/></g-link>
+          <g-link to=""><g-image class="icon" alt="Book icon" src="~/assets/images/courses.svg"/></g-link>
+          <g-link to=""><g-image class="icon" alt="Tchat icon" src="~/assets/images/community.svg"/></g-link>
+        </p>
+
+        <p>Bienvenue ! Cet espace est dédié aux agents publics de l'État soucieux de créer des services numériques accessibles pour tous.</p>
+
+      </div>
     </div>
 
     <HomeItem
       title="Découvrez le Design numérique"
       text="Le design permet de rendre les <b>services</b> numériques plus <b>simples</b>, <b>accessibles</b> et <b>agréables</b> pour tous les usagers. Concevoir par le design, c’est mettre en oeuvre une démarche de création centrée sur les usagers, tout en s’appuyant sur de bonnes pratiques reconnues. Nous vous expliquons tout cela en détails."
       buttonText="Le design numérique en détails →"
-      link=""
+      link="/design"
       iconSrc="design.svg"
       iconAlt="Star icon"
       color="#2854D6"
@@ -30,7 +34,7 @@
       title="Les outils à votre disposition"
       text="De nombreux outils peuvent vous aider à améliorer la qualité de vos démarches vous-même. Utilisabilité, accessibilité, protection des données personnelles, bonnes pratiques, laissez vous guider."
       buttonText="J'accède aux outils →"
-      link=""
+      link="/outils"
       iconSrc="tools.svg"
       iconAlt="Tool icon"
       color="#44A74C"
@@ -41,7 +45,7 @@
       title="Un besoin en Design ? Nous vous aidons."
       text="Vous avez un besoin urgent ou des questions concernant l’amélioration de vos démarches, nous pouvons vous accompagner."
       buttonText="J'ai un besoin en Design →"
-      link=""
+      link="/accompagnement"
       iconSrc="services.svg"
       iconAlt="Lifebuoy icon"
       color="#FF5D52"
@@ -52,7 +56,7 @@
       title="Des formations pour vous et vos équipes"
       text="De nombreuses formations sont disponibles, certaines accessibles gratuitement, que ce soit pour vous former personnellement ou en équipe sur les divers sujets liés au design et à la qualité des services numérique, l’accessibilité etc. Nous proposons également un accompagnement sur ces sujets."
       buttonText="Je découvre les formations →"
-      link=""
+      link="/formations"
       iconSrc="courses.svg"
       iconAlt="Book icon"
       color="#AE8C24"

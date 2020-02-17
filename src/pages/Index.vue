c1b835315f8abb02be01abff86c39d47d9873193
Ugo <jeanmichelpaul@icloud.com>
2020-02-17 15:16:56 +0100

    New homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,19 +1,71 @@
 <template>
   <Layout>
 
-    <h1>DesignGouv</h1>
+    <div class="cover">
+      <p><g-image class="logo" alt="DesignGouv" src="~/assets/images/logo.svg"/></p>
 
-    <p>Bienvenue ! Cet espace est dédié aux agents publics de l'État soucieux de créer des services numériques accessibles pour tous. Vous y trouverez prochainement :</p>
-    <ul>
-      <li>Des guides sur le rôle du design et l'importance de concevoir des expériences utiles, utilisables et agréables</li>
-      <li>Les outils pour mieux faire (bonnes pratiques, accessibilité numérique, formations, etc.)</li>
-      <li>Notre offre de services aux administrations (<a href="https://calendly.com/designgouv" target="_blank">bureaux ouverts</a> le vendredi, Commando UX pour les porteurs de projets de l'<a href="https://observatoire.numerique.gouv.fr" target="_blank">Observatoire</a>, etc.)</li>
-    </ul>
-    <p class="subtitle">Pour recevoir nos actualités, inscrivez-vous à notre newsletter :</p>
+      <p>
+        <a href=""><g-image class="icon" alt="Star icon" src="~/assets/images/design.svg"/></a>
+        <a href=""><g-image class="icon" alt="Tool icon" src="~/assets/images/tools.svg"/></a>
+        <a href=""><g-image class="icon" alt="Lifebuoy icon" src="~/assets/images/services.svg"/></a>
+        <a href=""><g-image class="icon" alt="Book icon" src="~/assets/images/courses.svg"/></a>
+        <a href=""><g-image class="icon" alt="Tchat icon" src="~/assets/images/community.svg"/></a>
+      </p>
 
-    <a href="http://eepurl.com/gLJCsb" class="button">Je m'inscris à la newsletter</a>
+      <h1>Bienvenue ! Cet espace est dédié aux agents publics de l'État soucieux de créer des services numériques accessibles pour tous.</h1>
+    </div>
 
-    <p>Sur Twitter : <a href="https://twitter.com/designgouv" title="Twitter" target="_blank">@DesignGouv</a></p>
+    <div class="homeItem" style="--color: #2854D6; --colorLight: #ACBDF1;">
+      <g-image class="icon" alt="Star icon" src="~/assets/images/design.svg"/>
+
+      <p><a href=""><h2>Découvrez le Design numérique</h2></a></p>
+
+      <p class="homeItemText">Le design permet de rendre les <b>services</b> numériques plus <b>simples</b>, <b>accessibles</b> et <b>agréables</b> pour tous les usagers. Concevoir par le design, c’est mettre en oeuvre une démarche de création centrée sur les usagers, tout en s’appuyant sur de bonnes pratiques reconnues. Nous vous expliquons tout cela en détails.</p>
+
+      <a href="" class="button">Le design numérique en détails →</a>
+    </div>
+
+    <div class="homeItem" style="--color: #44A74C; --colorLight: #84CD86;">
+      <g-image class="icon" alt="Tool icon" src="~/assets/images/tools.svg"/>
+
+      <p><a href=""><h2>Les outils à votre disposition</h2></a></p>
+
+      <p class="homeItemText">De nombreux outils peuvent vous aider à améliorer la qualité de vos démarches vous-même. Utilisabilité, accessibilité, protection des données personnelles, bonnes pratiques, laissez vous guider.</p>
+
+      <a href="" class="button">J'accède aux outils →</a>
+    </div>
+
+    <div class="homeItem" style="--color: #FF5D52; --colorLight: #FFA89E;">
+      <g-image class="icon" alt="Lifebuoy icon" src="~/assets/images/services.svg"/>
+
+      <p><a href=""><h2>Un besoin en Design ? Nous vous aidons.</h2></a></p>
+
+      <p class="homeItemText">Vous avez un besoin urgent ou des questions concernant l’amélioration de vos démarches, nous pouvons vous accompagner.</p>
+
+      <a href="" class="button">J'ai un besoin en Design →</a>
+    </div>
+
+    <div class="homeItem" style="--color: #AE8C24; --colorLight: #E2C56F;">
+      <g-image class="icon" alt="Book icon" src="~/assets/images/courses.svg"/>
+
+      <p><a href=""><h2>Des formations pour vous et vos équipes</h2></a></p>
+
+      <p class="homeItemText">De nombreuses formations sont disponibles, certaines accessibles gratuitement, que ce soit pour vous former personnellement ou en équipe sur les divers sujets liés au design et à la qualité des services numérique, l’accessibilité etc. Nous proposons également un accompagnement sur ces sujets.</p>
+
+      <a href="" class="button">Je découvre les formations →</a>
+    </div>
+
+    <div class="card" style="--color: #1B998B; --colorLight: #3CDDCA;">
+      <g-image class="icon" alt="Tchat icon" src="~/assets/images/community.svg"/>
+
+      <p><a href="http://eepurl.com/gLJCsb"><h2>Rejoignez la communauté&nbsp;!</h2></a></p>
+
+      <p class="homeItemText">DesignGouv, c’est aussi une communauté qui réunis celles et ceux qui s’engagent pour promouvoir et faire vivre la culture design au sein des administrations. Nous organisons régulièrement des évènements, des rencontres, des discussions. Rejoignez-nous !</p>
+
+      <a href="http://eepurl.com/gLJCsb" class="button">Je m'inscris à la newsletter</a>
+
+      <p>Sur Twitter : <a href="https://twitter.com/designgouv" title="Twitter" target="_blank">@DesignGouv</a></p>
+    </div>
 
   </Layout>
 </template>
@@ -21,3 +73,11 @@
 
 <style>
 </style>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'DesignGouv - Le design numérique au service des administrations'
+  }
+}
+</script>

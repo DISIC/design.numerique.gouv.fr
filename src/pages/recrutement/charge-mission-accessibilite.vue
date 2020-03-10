a8cc35a5b77f90b834e5c9de8b4e9321db2e1a8c
Marine Boudeau <m14868@gmail.com>
2020-03-10 15:56:56 +0100

    Add recrutement pages

---
+++ src/pages/recrutement/charge-mission-accessibilite.vue
@@ -0,0 +1,106 @@
+<template>
+  <Layout>
+    <Header class="page-header">
+      <h1>Chargé-e de mission accessibilité et qualité des services numériques</h1>
+    </Header>
+    <Main>
+      <h2>Structure</h2>
+      <ul class="job-intro">
+        <li>Direction interministérielle du numérique (DINUM)</li>
+        <li>Département performance des services numériques</li>
+        <li>Pôle design des services numériques</li>
+      </ul>
+      <h2>Présentation du service</h2>
+      <p>La DINUM est un service du Premier ministre, placé sous l’autorité administrative du Secrétaire Général du Gouvernement, et relevant du ministre de l’Action et des Comptes publics, du ministre de l’Economie et des Finances et du secrétaire d’Etat chargé du Numérique.</p>
+      <p>Elle définit la stratégie relative à la transformation numérique de l’Etat et coordonne sa mise en œuvre.</p>
+      <p>Elle promeut l’innovation numérique comme levier de transformation du service public et des méthodes et outils de travail des agents publics.</p>
+      <p>Elle veille à ce que les systèmes d’information et le développement du numérique concourent à l’amélioration de la qualité, l’efficacité, l’efficience et la fiabilité du service rendu et à la simplification des relations entre les usagers et les administrations de l’Etat, et entre celles-ci et les autres autorités administratives.</p>
+      <p>Elle opère des services d’infrastructures à valeur ajoutée mutualisés entre les administrations, dont le réseau interministériel de l'Etat (RIE).</p>
+      <p>Dans le cadre de ces objectifs, <strong>le pôle Design des services numériques est chargé de</strong> :</p>
+      <ul>
+        <li>Définir une stratégie globale pour l’évolution des services en ligne rendus aux usagers, tenant compte des décisions gouvernementales, des besoins des usagers, et des possibilités de mise en œuvre par les administrations. A ce titre, il mesure et veille à la qualité des principaux services en ligne (notamment de leur design et de l’expérience utilisateur), développe la mise en œuvre du principe « dites-le-nous une fois » et la prise en compte de l’accessibilité par les personnes en situation de handicap</li>
+        <li>Faire évoluer et piloter la mise en œuvre la politique de mesure des usages des services en ligne et de la satisfaction des usagers sur ces services en ligne (en particulier la mise à jour d’un tableau de bord des services publics numériques - observatoire.numerique.gouv.fr)</li>
+        <li>Fournir ressources et aide aux administrations pour la mise en œuvre et l’améliorations de leurs services en ligne : normes, référentiels, guides, outils, briques  réutilisables (librairie de composants, API, etc.) dispositifs d’accompagnement</li>
+        <li>Animer un réseau de correspondants dans les administrations</li>
+        <li>Participer au développement d’une culture de Design au sein de l’État, et à la mise en communauté de compétences de design dans les administrations</li>
+        <li>Stimuler, en lien avec l’Agence Nationale de la Cohésion des Territoire (ANCT), en interministériel le développement d’actions en faveur d’une meilleure inclusion de l’ensemble des usagers dans le cadre de la transformation numérique des services publics</li>
+      </ul>
+
+      <h2>Missions</h2>
+      <p>À titre principal, porter la stratégie interministérielle d’accessibilité aux personnes en situation de handicap des services administratifs en ligne, et à titre complémentaire participer à l’animation des travaux des ministères dans l’amélioration de la qualité de leurs démarches ne ligne. Dans ce cadre, le titulaire du poste devra :</p>
+      <ul>
+        <li>Accompagner les administrations sur l’accélération de la mise en accessibilité de leurs services numériques, et plus globalement sur l’amélioration de la qualité de leurs services</li>
+        <li>Développer des stratégies assurant la prise en compte de l’accessibilité numérique dans les services numériques des administrations, ainsi que la formation des équipes</li>
+        <li>Piloter la déclinaison des engagements pris en matière d’accessibilité par le gouvernement et dans la réglementation</li>
+        <li>Orienter les porteurs des services numériques vers les dispositifs d’appui complémentaires adaptés aux besoins spécifiques de chacune de leurs démarches en ligne (audit accessibilité, aide au diagnostic, intervention de designers, etc.)</li>
+        <li>Assurer la relation avec les partenaires (FIPHFP, SIG, etc.) et prestataires de la DINUM dans le cadre de l’accessibilité numérique et piloter l’évolution des relations contractuelles avec ces acteurs</li>
+        <li>S’assurer que les administrations porteuses de la démarche en ligne élaborent des plans d’actions d’amélioration de la qualité et de l’accessibilité de leurs démarches en ligne, si besoin l’assister dans cette élaboration, puis veiller à sa mise en œuvre</li>
+        <li>Assurer la souveraineté et le leadership de la France dans le cadre de la stratégie Européenne sur l’accessibilité numérique et des évolutions du RGAA</li>
+      </ul>
+      <h2>Activités</h2>
+      <ul>
+        <li>Marchés : contribuer aux procédures de consultation sur les marchés de l’accessibilité numérique, expression des besoins, et être un point de contact, préparation des demandes d’achats, suivi des achats</li>
+        <li>Financements : suivi des partenariats avec les financeurs (ex : FIPHFP), recherche de nouveaux financements</li>
+        <li>Règlementaire : participer aux travaux règlementaires européens (WAD, actes d’implémentation), et piloter les travaux nationaux (évolutions RGAA)</li>
+        <li>Événements : contribution à l’organisation d’événements liés à l’accessibilité numérique ainsi qu’à l’amélioration globale de la qualité des services, animation, participation</li>
+        <li>Ressources : contribuer à la production de ressources et outils plus pertinents à une prise en main rapide du sujet par les développeurs, designers et cadres</li>
+      </ul>
+
+      <h2>Compétences</h2>
+
+      <strong>Savoirs</strong>
+      <ul>
+        <li>Connaissance des enjeux de la conception et l’amélioration de services numériques centrés sur les usagers, et de l’accessibilité numérique</li>
+        <li>Une connaissance technique du RGAA serait un plus</li>
+        <li>Une connaissance de l’administration serait un plus</li>
+      </ul>
+
+      <strong>Savoir-faire</strong>
+      <ul>
+        <li>Conseiller</li>
+        <li>Elaborer des plans d’actions</li>
+        <li>Piloter un réseau d’acteurs, animer des réunions ateliers et événements</li>
+        <li>Maitrise de la langue française orale et écrite</li>
+        <li>Maitrise des logiciels de tableurs et de traitement de texte</li>
+        <li>Gestion de partenariats (conception, contractualisation, suivi)</li>
+      </ul>
+
+      <strong>Savoir-être</strong>
+      <ul>
+        <li>Créativité</li>
+        <li>Sens des priorités</li>
+        <li>Sens du service et de la facilitation</li>
+        <li>Excellent sens des relations humaines</li>
+        <li>Empathie pour les usagers du service public</li>
+        <li>Rigueur et sens de l’organisation</li>
+        <li>Volonté d’amélioration du service public et du fonctionnement de l’Etat</li>
+      </ul>
+
+      <h2>Détails pratiques</h2>
+      <p>
+        <strong>Contrat</strong> : Ouvert aux contractuels, contrat de 3 ans<br>
+        <strong>À pourvoir au</strong> : 1er avril 2020<br>
+        <strong>Lieu</strong> : 20 avenue de Ségur, 75007 Paris. Le site est accessible aux personnes en situation de handicap.
+      </p>
+      <h2>Questions ?</h2>
+      <p>
+        Adressez vos questions via e-mail à <a href="mailto:marine.boudeau@modernisation.gouv.fr">marine.boudeau@modernisation.gouv.fr</a>.
+      </p>
+      <h2>Pour candidater</h2>
+      <p>
+        Adressez votre CV (sans photographie, âge ou situation familiale) et votre lettre de motivation à : <a href="mailto:candidatures-DINUM@pm.gouv.fr">candidatures-DINUM@pm.gouv.fr</a>
+      </p>
+      <p>Les services du Premier ministre sont engagés dans une politique active en faveur de la diversité, de l’égalité professionnelle et du handicap.</p>
+      <p>A ce titre, le ou la candidat(e) ne doit indiquer aucune information personnelle (âge, situation de famille, photographie) sur sa candidature.</p>
+      <p>Nous conserverons, pour une durée de deux ans, l’ensemble des éléments que vous nous avez transmis afin de vous faire part d’opportunités futures susceptibles de vous intéresser.</p>
+    </Main>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'stage-design'
+  }
+}
+</script>
---
+++ src/pages/recrutement/developpeur-se-accessibilite.vue
@@ -0,0 +1,95 @@
+<template>
+  <Layout>
+    <Header class="page-header">
+      <h1>Expert-e design et accessibilité numérique (niveau développeur-se)</h1>
+    </Header>
+    <Main>
+      <h2>Structure</h2>
+      <ul class="job-intro">
+        <li>Direction interministérielle du numérique (DINUM)</li>
+        <li>Département performance des services numériques</li>
+        <li>Pôle design des services numériques</li>
+      </ul>
+      <h2>Présentation du service</h2>
+      <p>La DINUM est un service du Premier ministre, placé sous l’autorité administrative du Secrétaire Général du Gouvernement, et relevant du ministre de l’Action et des Comptes publics, du ministre de l’Economie et des Finances et du secrétaire d’Etat chargé du Numérique.</p>
+      <p>Elle définit la stratégie relative à la transformation numérique de l’Etat et coordonne sa mise en œuvre.</p>
+      <p>Elle promeut l’innovation numérique comme levier de transformation du service public et des méthodes et outils de travail des agents publics.</p>
+      <p>Elle veille à ce que les systèmes d’information et le développement du numérique concourent à l’amélioration de la qualité, l’efficacité, l’efficience et la fiabilité du service rendu et à la simplification des relations entre les usagers et les administrations de l’Etat, et entre celles-ci et les autres autorités administratives.</p>
+      <p>Elle opère des services d’infrastructures à valeur ajoutée mutualisés entre les administrations, dont le réseau interministériel de l'Etat (RIE).</p>
+      <p>Dans le cadre de ces objectifs, <strong>le pôle Design des services numériques est chargé de</strong> :</p>
+      <ul>
+        <li>Définir une stratégie globale pour l’évolution des services en ligne rendus aux usagers, tenant compte des décisions gouvernementales, des besoins des usagers, et des possibilités de mise en œuvre par les administrations. A ce titre, il mesure et veille à la qualité des principaux services en ligne (notamment de leur design et de l’expérience utilisateur), développe la mise en œuvre du principe « dites-le-nous une fois » et la prise en compte de l’accessibilité par les personnes en situation de handicap</li>
+        <li>Faire évoluer et piloter la mise en œuvre la politique de mesure des usages des services en ligne et de la satisfaction des usagers sur ces services en ligne (en particulier la mise à jour d’un tableau de bord des services publics numériques - observatoire.numerique.gouv.fr)</li>
+        <li>Fournir ressources et aide aux administrations pour la mise en œuvre et l’améliorations de leurs services en ligne : normes, référentiels, guides, outils, briques  réutilisables (librairie de composants, API, etc.) dispositifs d’accompagnement</li>
+        <li>Animer un réseau de correspondants dans les administrations</li>
+        <li>Participer au développement d’une culture de Design au sein de l’État, et à la mise en communauté de compétences de design dans les administrations</li>
+        <li>Stimuler, en lien avec l’Agence Nationale de la Cohésion des Territoire (ANCT), en interministériel le développement d’actions en faveur d’une meilleure inclusion de l’ensemble des usagers dans le cadre de la transformation numérique des services publics</li>
+      </ul>
+
+      <h2>Missions</h2>
+      <p>Accompagner les administrations et leurs équipes techniques dans l’accélération de la mise en accessibilité de leurs services pour les personnes en situation de handicap, et plus largement sur l’amélioration de la qualité de leurs services :</p>
+      <ul>
+        <li>Développer des outils mieux adaptés aux équipes techniques pour faciliter l’internalisation, par les différents acteurs dans les administrations (chefs-ffes de projets/produits, designers, développeurs-ses) des enjeux de l’accessibilité des services numériques aux personnes en situation de handicap</li>
+        <li>Accompagner les porteurs des services numériques sur la mise en accessibilité de leurs démarches administratives et les orienter vers des dispositifs d’accompagnement complémentaires en fonction de leurs besoins (audits, recommandations, de l’aide au diagnostic, intervention de designers, etc.) et atteindre l’objectif fixé par le gouvernement : 80% des démarches de l’Observatoire (observatoire.numerique.gouv.fr) satisfaisant au critère « accessibilité » en  2022. Plus généralement, accompagner ces acteurs pour améliorer la qualité et la satisfaction des usagers de leurs services numériques.</li>
+        <li>Produire et/ou diffuser des ressources, organiser des événements et animer le réseau des porteurs de démarches administratives</li>
+        <li>Participer aux évolutions du RGAA, expertise technique sur le RGAA</li>
+      </ul>
+      <h2>Activités</h2>
+      <ul>
+        <li>Développer, intégrer et/ou référencer de nouveaux outils pour faciliter la prise en main de l’accessibilité et maintenir des outils existants</li>
+        <li>Audits, conseils et formation : réaliser des audits d’accessibilité pour faire avancer les équipes et les accompagner dans l’internalisation de ces compétences</li>
+        <li>Événements : contribution à l’organisation d’événements liés à l’accessibilité numérique ainsi qu’à l’amélioration globale de la qualité des services, animation, participation, interventions</li>
+        <li>Ressources : contribuer à la production de ressources et outils plus pertinents à une prise en main rapide du sujet par les développeurs, designers et cadres</li>
+      </ul>
+
+      <h2>Compétences</h2>
+
+      <strong>Savoirs</strong>
+      <ul>
+        <li>Connaissance indispensable des enjeux de l’accessibilité numérique et du RGAA</li>
+        <li>Connaissance en matière de design et de qualité des services numériques pour les usagers</li>
+      </ul>
+
+      <strong>Savoir-faire</strong>
+      <ul>
+        <li>Expérience en tant que développeur ou développeuse web</li>
+        <li>Capacités d’audit des services en ligne</li>
+      </ul>
+
+      <strong>Savoir-être</strong>
+      <ul>
+        <li>Sens du service et de la facilitation</li>
+        <li>Sens des relations humaines</li>
+        <li>Empathie pour les usagers du service public</li>
+        <li>Rigueur et sens de l’organisation</li>
+        <li>Volonté d’amélioration du service public et du fonctionnement de l’Etat</li>
+      </ul>
+
+      <h2>Détails pratiques</h2>
+      <p>
+        <strong>Contrat</strong> : Ouvert aux contractuels, contrat de 3 ans<br>
+        <strong>À pourvoir au</strong> : 1er avril 2020<br>
+        <strong>Lieu</strong> : 20 avenue de Ségur, 75007 Paris. Le site est accessible aux personnes en situation de handicap.
+      </p>
+      <h2>Questions ?</h2>
+      <p>
+        Adressez vos questions via e-mail à <a href="mailto:marine.boudeau@modernisation.gouv.fr">marine.boudeau@modernisation.gouv.fr</a>.
+      </p>
+      <h2>Pour candidater</h2>
+      <p>
+        Adressez votre CV (sans photographie, âge ou situation familiale) et votre lettre de motivation à : <a href="mailto:candidatures-DINUM@pm.gouv.fr">candidatures-DINUM@pm.gouv.fr</a>
+      </p>
+      <p>Les services du Premier ministre sont engagés dans une politique active en faveur de la diversité, de l’égalité professionnelle et du handicap.</p>
+      <p>A ce titre, le ou la candidat(e) ne doit indiquer aucune information personnelle (âge, situation de famille, photographie) sur sa candidature.</p>
+      <p>Nous conserverons, pour une durée de deux ans, l’ensemble des éléments que vous nous avez transmis afin de vous faire part d’opportunités futures susceptibles de vous intéresser.</p>
+    </Main>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'stage-design'
+  }
+}
+</script>

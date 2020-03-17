2033c12e925f9789a4cc587b8f787c2b1e0a31fa
Marine Boudeau <m14868@gmail.com>
2020-03-17 20:06:23 +0100

    Minor changes

--- src/pages/Covid-19.vue
+++ src/pages/Covid-19.vue
@@ -6,9 +6,9 @@
     <p>Le 16 mars 2020, le Président de la République a pris des mesures pour réduire les contacts et déplacements au strict minimum sur l’ensemble du territoire à compter du mardi 17 mars à 12h00, pour quinze jours minimum.</p>
     <p>Des dérogations sur attestation sont possibles (déplacements entre le domicile et le lieu de travail, achats de première nécessité, motifs de santé, motif familial, assistance aux personnes vulnérables ou garde d’enfants et enfin pour des déplacements brefs, à proximité du domicile, liés à l’activité physique et aux besoins des animaux de compagnie).
     </p>
-    <p class="small">Si vous n’avez <strong>pas d’imprimante</strong>, vous pouvez recopier ce document sur une feuille de papier. Si vous ne pouvez pas imprimer ce formulaire ou le recopier, vous pouvez aussi le présenter sur votre téléphone.</p>
-
     <p>Accédez aux attestations et justificatifs ci-dessous :</p>
+    <p class="small">Si vous n’avez <strong>pas d’imprimante</strong>, vous pouvez recopier ces documents sur une feuille de papier. Si vous ne pouvez pas imprimer ce formulaire ou le recopier, vous pouvez aussi les présenter sur votre téléphone.</p>
+
 
     <h2>Télécharger au format .txt</h2>
     <ul>

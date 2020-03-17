dda57bf6e81bd49975801f6def7ae134eafeabf4
Marine Boudeau <m14868@gmail.com>
2020-03-17 14:29:44 +0100

    Update covid page

--- src/pages/Covid-19.vue
+++ src/pages/Covid-19.vue
@@ -1,12 +1,17 @@
 <template>
   <Layout>
     <h1>Covid-19</h1>
-    <p>Téléchargez les documents ci-dessous :</p>
+    <p>Depuis janvier 2020, une épidémie de Coronavirus COVID-19 (ex 2019-nCoV) s’est propagée depuis la Chine. Retrouvez toutes les réponses officielles aux questions que vous vous posez sur ce qu’est le Coronavirus COVID-19 et les recommandations pour votre santé sur la plateforme : <a href="https://gouvernement.fr/info-coronavirus">gouvernement.fr/info-coronavirus</a>.
+    </p>
+    <p>Le 16 mars 2020, le Président de la République a décidé de prendre des mesures pour réduire les contacts et déplacements au strict minimum sur l’ensemble du territoire à compter du mardi 17 mars à 12h00, pour quinze jours minimum.</p>
+    <p>Des dérogations sur attestation sont possibles (déplacements entre le domicile et le lieu de travail, achats de première nécessité, motifs de santé, motif familial, assistance aux personnes vulnérables ou garde d’enfants et enfin pour des déplacements brefs, à proximité du domicile, liés à l’activité physique et aux besoins des animaux de compagnie).
+    </p>
+    <p>Téléchargez les attestations et justificatifs ci-dessous :</p>
     <ul>
       <li><a href="/covid-19/attestation-deplacement-fr.txt" download="attestation-deplacement-fr">Attestation de déplacement dérogatoire (Français)</a></li>
       <li><a href="/covid-19/attestation-deplacement-en.txt" download="attestation-deplacement-en">Attestation de déplacement dérogatoire (English)</a></li>
-      <li><a href="/covid-19/justificatif-deplacement-professionnel-fr.txt" download="justificatif-deplacement-professionnel-fr">Justificatif deplacement professionnel (Français)</a></li>
-      <li><a href="/covid-19/justificatif-deplacement-professionnel-en.txt" download="justificatif-deplacement-professionnel-en">Justificatif deplacement professionnel (English)</a></li>
+      <li><a href="/covid-19/justificatif-deplacement-professionnel-fr.txt" download="justificatif-deplacement-professionnel-fr">Justificatif déplacement professionnel (Français)</a></li>
+      <li><a href="/covid-19/justificatif-deplacement-professionnel-en.txt" download="justificatif-deplacement-professionnel-en">Justificatif déplacement professionnel (English)</a></li>
     </ul>
   </Layout>
 </template>

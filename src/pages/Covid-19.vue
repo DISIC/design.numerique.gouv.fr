f53d40861f34990cfd879ce2f88565a660c85bd1
Marine Boudeau <m14868@gmail.com>
2020-03-17 13:48:15 +0100

    Add page so it can be easily downloaded

---
+++ src/pages/Covid-19.vue
@@ -0,0 +1,15 @@
+<template>
+  <Layout>
+    <h1>Covid-19</h1>
+    <p>Téléchargez les documents ci-dessous :</p>
+    <ul>
+      <li><a href="/covid-19/attestation-deplacement-fr.txt" download="attestation-deplacement-fr">Attestation de déplacement dérogatoire (Français)</a></li>
+      <li><a href="/covid-19/attestation-deplacement-en.txt" download="attestation-deplacement-en">Attestation de déplacement dérogatoire (English)</a></li>
+    </ul>
+  </Layout>
+</template>
+
+<script>
+export default {
+}
+</script>

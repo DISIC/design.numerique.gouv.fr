cb91ad0cd004657412bb57577e4fd5be8a66f205
Marine Boudeau <m14868@gmail.com>
2020-03-17 14:33:48 +0100

    Update copy

--- src/pages/Covid-19.vue
+++ src/pages/Covid-19.vue
@@ -1,9 +1,9 @@
 <template>
   <Layout>
     <h1>Covid-19</h1>
-    <p>Depuis janvier 2020, une épidémie de Coronavirus COVID-19 (ex 2019-nCoV) s’est propagée depuis la Chine. Retrouvez toutes les réponses officielles aux questions que vous vous posez sur ce qu’est le Coronavirus COVID-19 et les recommandations pour votre santé sur la plateforme : <a href="https://gouvernement.fr/info-coronavirus">gouvernement.fr/info-coronavirus</a>.
+    <p>Depuis janvier 2020, une épidémie de Coronavirus COVID-19 (ex 2019-nCoV) s’est propagée depuis la Chine. Retrouvez toutes les réponses officielles aux questions que vous vous posez sur ce qu’est le Coronavirus COVID-19 sur : <a href="https://gouvernement.fr/info-coronavirus">gouvernement.fr/info-coronavirus</a>.
     </p>
-    <p>Le 16 mars 2020, le Président de la République a décidé de prendre des mesures pour réduire les contacts et déplacements au strict minimum sur l’ensemble du territoire à compter du mardi 17 mars à 12h00, pour quinze jours minimum.</p>
+    <p>Le 16 mars 2020, le Président de la République a pris des mesures pour réduire les contacts et déplacements au strict minimum sur l’ensemble du territoire à compter du mardi 17 mars à 12h00, pour quinze jours minimum.</p>
     <p>Des dérogations sur attestation sont possibles (déplacements entre le domicile et le lieu de travail, achats de première nécessité, motifs de santé, motif familial, assistance aux personnes vulnérables ou garde d’enfants et enfin pour des déplacements brefs, à proximité du domicile, liés à l’activité physique et aux besoins des animaux de compagnie).
     </p>
     <p>Téléchargez les attestations et justificatifs ci-dessous :</p>

77e16edede0b12c7d110137ef2132806ac15d550
Marine Boudeau <m14868@gmail.com>
2020-03-18 11:07:17 +0100

    Update copy

--- src/pages/covid-19/justificatif-deplacement-professionnel-fr.vue
+++ src/pages/covid-19/justificatif-deplacement-professionnel-fr.vue
@@ -3,17 +3,11 @@
     <p class="recopier">Recopier le texte dans l'encadré sur une feuille de papier, puis le remplir :</p>
     <main class="covid-form">
       <h1>Justificatif de déplacement professionnel</h1>
-      <p>En application de l’article 1er du décret du 16 mars 2020 portant réglementation des déplacements dans le cadre de la lutte contre la propagation du virus Covid-19.</p>
+      <p>En application de l’article 1er du décret du 16 mars 2020 portant réglementation des déplacements dans le cadre de la lutte contre la propagation du virus Covid-19 :</p>
       <br>
 
-      <p>Nom de la personne certifiant (employeur) :</p>
+      <p>Je soussigné(e), ___________________, (fonction) ____________________________________ certifie que les déplacements de la personne ci-après, entre son domicile et son lieu d’activité professionnelle, ne peuvent être différés ou sont indispensables à l’exercice d’activités ne pouvant être organisées sous forme de télétravail (au sens du 1er du 2e alinéa de l’article 1er du décret du 16 mars 2020 portant règlementation des déplacements dans le cadre de la lutte contre la propagation du virus Covid-19) :</p>
 
-      <p>Prénom de la personne certifiant (employeur) :</p>
-
-      <p>Fonction de la personne certifiant (employeur) :</p>
-      <br>
-
-      <p>Je certifie que les déplacements de la personne ci-après, entre son domicile et son lieu d’activité professionnelle, ne peuvent être différés ou sont indispensables à l’exercice d’activités ne pouvant être organisées sous forme de télétravail (au sens du 1er du 2e alinéa de l’article 1er du décret du 16 mars 2020 portant réglementation des déplacements dans le cadre de la lutte contre la propagation du virus Covid-19) :</p>
 
       <p>Nom :</p>
       <p>Prénom :</p>
@@ -24,9 +18,9 @@
       <p>Trajet de déplacement :</p>
       <p>Moyen de déplacement :</p>
       <br>
-      <p>Nom de l'employeur :</p>
+      <p>Nom et cachet de l'employeur :</p>
       <p>Fait à :</p>
-      <p>Date :</p>
+      <p>Le :</p>
 
     </main>
 

61d9d3baf05ec5551a477428e7eb5edc6cce4bc6
Marine Boudeau <m14868@gmail.com>
2020-03-22 19:41:31 +0100

    Add layout updates

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -11,77 +11,81 @@
       </div>
     </div>
 
-    <div class="message">
+    <div class="content">
 
-        <strong>COVID-19</strong>
+      <div class="message">
 
-        <p>Nous avons créé des attestations de déplacements accessibles et inclusives pour toutes et tous, à imprimer ou à recopier.</p>
+          <strong>COVID-19</strong>
 
-        <g-link to="/covid-19" class="button">
-          Accéder aux documents
-          <span class='arrow'>→</span>
-        </g-link>
+          <p>Nous avons créé des attestations de déplacements accessibles et inclusives pour toutes et tous, à imprimer ou à recopier.</p>
+
+          <g-link to="/covid-19" class="button">
+            Accéder aux documents
+            <span class='arrow'>→</span>
+          </g-link>
+
+      </div>
+
+      <HomeItem
+        title="Découvrez le Design numérique"
+        text="Le design permet de rendre les <b>services</b> numériques plus <b>simples</b>,
+        <b>accessibles</b> et <b>agréables</b> pour tous. Concevoir par
+        le design, c’est mettre en œuvre des solutions centrées sur les
+        usagers, tout en s’appuyant sur de bonnes pratiques reconnues. Et c'est aussi un métier&nbsp;! Nous vous présenterons tout cela en détail bientôt."
+        buttonText="Bientôt disponible !"
+        v-bind:buttonActivated="false"
+        link="/"
+        iconSrc="design.svg"
+        iconAlt="Star icon"
+        color="#2854D6"
+        colorLight="#ACBDF1"
+      />
+
+      <HomeItem
+        title="Accessibilité numérique"
+        text="De nombreux outils peuvent vous aider à améliorer vous-même la qualité de vos
+        démarches. <b>Bonnes pratiques</b>, <b>expérience utilisateur</b>, <b>accessibilité</b>&nbsp;:
+        nous les avons réunis pour vous."
+        buttonText="J'accède aux outils"
+        v-bind:buttonActivated="true"
+        link="/accessibilite-numerique"
+        iconSrc="tools.svg"
+        iconAlt="Tool icon"
+        color="#37863D"
+        colorLight="#84CD86"
+      />
+
+      <HomeItem
+        title="Besoin d'aide ? Nous vous accompagnons."
+        text="Vous avez des besoins en design&nbsp;?
+          Vous vous <b>posez des questions</b> sur les sujets d'accessibilité&nbsp;? Vous souhaitez <b>obtenir des
+          retours</b>, <b>demander des conseils</b> pour le développement ou l'amélioration
+          de votre service&nbsp;? Nous vous aidons&nbsp;!"
+        buttonText="J'ai besoin d'aide"
+        v-bind:buttonActivated="true"
+        link="/services"
+        iconSrc="services.svg"
+        iconAlt="Lifebuoy icon"
+        color="#EB1000"
+        colorLight="#FFA89E"
+      />
+
+      <HomeItem
+        title="Des formations pour vous et vos équipes"
+        text="De nombreuses formations sont disponibles, certaines accessibles
+          <b>gratuitement</b>, pour vous former <b>personnellement</b>
+          ou <b>en équipe</b> sur les divers sujets liés au design, à la qualité des
+          services numériques et à l’accessibilité."
+        buttonText="Je découvre les formations"
+        v-bind:buttonActivated="true"
+        link="/formations"
+        iconSrc="courses.svg"
+        iconAlt="Book icon"
+        color="#90731D"
+        colorLight="#E2C56F"
+      />
 
     </div>
-
-    <HomeItem
-      title="Découvrez le Design numérique"
-      text="Le design permet de rendre les <b>services</b> numériques plus <b>simples</b>,
-      <b>accessibles</b> et <b>agréables</b> pour tous. Concevoir par
-      le design, c’est mettre en œuvre des solutions centrées sur les
-      usagers, tout en s’appuyant sur de bonnes pratiques reconnues. Et c'est aussi un métier&nbsp;! Nous vous présenterons tout cela en détail bientôt."
-      buttonText="Bientôt disponible !"
-      v-bind:buttonActivated="false"
-      link="/"
-      iconSrc="design.svg"
-      iconAlt="Star icon"
-      color="#2854D6"
-      colorLight="#ACBDF1"
-    />
-
-    <HomeItem
-      title="Accessibilité numérique"
-      text="De nombreux outils peuvent vous aider à améliorer vous-même la qualité de vos
-      démarches. <b>Bonnes pratiques</b>, <b>expérience utilisateur</b>, <b>accessibilité</b>&nbsp;:
-      nous les avons réunis pour vous."
-      buttonText="J'accède aux outils"
-      v-bind:buttonActivated="true"
-      link="/accessibilite-numerique"
-      iconSrc="tools.svg"
-      iconAlt="Tool icon"
-      color="#37863D"
-      colorLight="#84CD86"
-    />
-
-    <HomeItem
-      title="Besoin d'aide ? Nous vous accompagnons."
-      text="Vous avez des besoins en design&nbsp;?
-        Vous vous <b>posez des questions</b> sur les sujets d'accessibilité&nbsp;? Vous souhaitez <b>obtenir des
-        retours</b>, <b>demander des conseils</b> pour le développement ou l'amélioration
-        de votre service&nbsp;? Nous vous aidons&nbsp;!"
-      buttonText="J'ai besoin d'aide"
-      v-bind:buttonActivated="true"
-      link="/services"
-      iconSrc="services.svg"
-      iconAlt="Lifebuoy icon"
-      color="#EB1000"
-      colorLight="#FFA89E"
-    />
-
-    <HomeItem
-      title="Des formations pour vous et vos équipes"
-      text="De nombreuses formations sont disponibles, certaines accessibles
-        <b>gratuitement</b>, pour vous former <b>personnellement</b>
-        ou <b>en équipe</b> sur les divers sujets liés au design, à la qualité des
-        services numériques et à l’accessibilité."
-      buttonText="Je découvre les formations"
-      v-bind:buttonActivated="true"
-      link="/formations"
-      iconSrc="courses.svg"
-      iconAlt="Book icon"
-      color="#90731D"
-      colorLight="#E2C56F"
-    />
 
   </Layout>
 </template>
@@ -171,8 +175,8 @@ export default {
     text-align: center;
     background-color: $light-gray;
     padding: 24px 48px 16px 48px;
-    margin-top: -48px;
-    margin-bottom: 108px;
+    margin-top: -30px;
+    margin-bottom: 50px;
     border-radius: 32px;
 
     @media only screen and (max-width: $mobileMaxWidth) {

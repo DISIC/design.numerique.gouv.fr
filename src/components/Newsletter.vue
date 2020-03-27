4bb58d6a902faaf2a552eda063585b30ca2e86d6
Ugo <jeanmichelpaul@icloud.com>
2020-03-27 10:59:23 +0100

    Minor fix

--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -19,3 +19,72 @@ export default {
   name: 'Newsletter'
 }
 </script>
+
+<style lang="scss" scoped>
+
+  @import "src/assets/scss/_vars.scss";
+
+  .card {
+    margin: 80px 0 120px 0;
+    text-align: center;
+    padding: 32px 32px 24px 32px;
+    background: #FFFFFF;
+    box-shadow: 0 0 32px -8px #C9D3DF;
+    border-radius: 32px;
+
+    @media only screen and (max-width: $mobileMaxWidth) {
+      margin: 64px 0 64px 0;
+      padding: 24px 8px 16px 8px;
+    }
+
+    p {
+      margin: 8px;
+    }
+
+    a {
+      color: var(--color);
+    }
+
+    .title {
+      color: $dark;
+      font-family: "Marianne", "Helvetica Neue", Arial, sans-serif;
+      line-height: 1.2em;
+      font-size: 2em;
+      font-weight: 700;
+      text-decoration: none;
+      display: inline;
+      transition: .1s all;
+
+      @media only screen and (max-width: $mobileMaxWidth) {
+        font-size: 1.75em;
+      }
+    }
+
+    .description {
+      margin-top: 24px;
+    }
+
+    .button {
+      text-decoration: none;
+      color: white;
+      border: 2px solid var(--color);
+      background-color: var(--color);
+      border-radius: 32px;
+      padding: 8px 24px;
+      text-decoration: none;
+      font-weight: bold;
+      display: inline-block;
+      margin: 10px 0;
+
+      @media only screen and (max-width: $mobileMaxWidth) {
+        font-size: 0.875em;
+      }
+
+      &:hover {
+        background: white;
+        color: var(--color);
+      }
+    }
+  }
+
+</style>

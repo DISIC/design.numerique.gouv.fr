64bb7aa8babd55b88d9854e6a305b9100ce19fb9
Ugo <jeanmichelpaul@icloud.com>
2020-05-06 10:12:00 +0200

    Change subtitle

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,7 +9,7 @@
 
         <h1>Concevons des services publics numériques <span class="highlight">accessibles</span>, <span class="highlight">inclusifs</span> et <span class="highlight">humains</span>.</h1>
 
-        <p class="subtitle"><b>DesignGouv</b> rassemble les agents publics soucieux de la qualité des services numériques et met à disposition des outils pour leur amélioration.</p>
+        <p class="subtitle">Des idées et des outils à la disposition des agents publics pour améliorer l'expérience usager des services publics numériques.</p>
 
       </div>
     </div>

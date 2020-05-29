06fe190027fc7841d4b19cb713c7289fd3a47288
Ugo <jeanmichelpaul@icloud.com>
2020-05-29 14:51:04 +0200

    Remove covid page commented sections

--- src/pages/Covid-19.vue
+++ src/pages/Covid-19.vue
@@ -14,133 +14,7 @@
     </div>
 
     <div class="content">
-
-      <!-- <section class="entete">
-        <p>Retrouvez toutes les réponses officielles sur le Coronavirus sur : <a href="https://gouvernement.fr/info-coronavirus">gouvernement.fr/info-coronavirus</a>.</p>
-        <p>A compter du 17 mars 2020, des mesures ont été prises pour réduire les contacts et déplacements au strict minimum sur l’ensemble du territoire, pour quinze jours minimum. Des dérogations sur attestation sont possibles. Si vous n’avez <strong>pas d’imprimante</strong>, vous pouvez les recopier sur une feuille de papier. Une <a href="https://media.interieur.gouv.fr/deplacement-covid-19/" target="_blank">version numérique de l'attestation de déplacement dérogatoire</a> est maintenant disponible.</p>
-      </section> -->
-
-      <!-- <section id="attestations">
-        <h2>Attestation de déplacement dérogatoire</h2>
-
-        <a href="https://media.interieur.gouv.fr/deplacement-covid-19/" title="En numérique, à remplir"  target="_blank">
-          <div class="file">
-            <p class="description">En numérique, à remplir</p>
-            <p class="download">
-              <span class="link">Accéder <font-awesome :icon="['fas', 'arrow-right']"/></span>
-              <font-awesome :icon="['fas', 'mobile-alt']" size="2x" transform="shrink-3" alt="Icon"/>
-            </p>
-          </div>
-        </a>
-
-        <a href="/covid-19/pdf/attestation-deplacement-fr.pdf" download="attestation-deplacement-fr.pdf" title="Télécharger au format PDF">
-          <div class="file">
-            <p class="description">Format PDF</p>
-            <p class="download">
-              <span class="link">Télécharger <font-awesome :icon="['fas', 'arrow-down']"/></span>
-              <font-awesome :icon="['far', 'file-pdf']" size="2x" transform="shrink-3" alt="Icon"/>
-            </p>
-          </div>
-        </a>
-
-        <a href="/covid-19/txt/attestation-deplacement-fr.txt" download="attestation-deplacement-fr.txt" title="Télécharger au format TXT">
-          <div class="file">
-            <p class="description">Format TXT</p>
-            <p class="download">
-              <span class="link">Télécharger <font-awesome :icon="['fas', 'arrow-down']"/></span>
-              <font-awesome :icon="['far', 'file-alt']" size="2x" transform="shrink-3" alt="Icon"/>
-            </p>
-          </div>
-        </a>
-
-        <a href="/covid-19/docx/attestation-deplacement-fr.docx" download="attestation-deplacement-fr.docx" title="Télécharger au format Word">
-          <div class="file">
-            <p class="description">Format Word</p>
-            <p class="download">
-              <span class="link">Télécharger <font-awesome :icon="['fas', 'arrow-down']"/></span>
-              <font-awesome :icon="['far', 'file-word']" size="2x" transform="shrink-3" alt="Icon"/>
-            </p>
-          </div>
-        </a>
-
-        <a href="/covid-19/docx/attestation-deplacement-en.docx" download="attestation-deplacement-en.docx" title="Download the Word file">
-          <div class="file">
-            <p class="description">WORD file in english</p>
-            <p class="download">
-              <span class="link">Download <font-awesome :icon="['fas', 'arrow-down']"/></span>
-              <font-awesome :icon="['far', 'file-word']" size="2x" transform="shrink-3" alt="Icon"/>
-            </p>
-          </div>
-        </a>
-
-        <a href="/covid-19/pdf/attestation-deplacement-falc.pdf" download="attestation-deplacement-fr.pdf" title="Télécharger en FALC (facile à lire et à comprendre)">
-          <div class="file">
-            <p class="description">En FALC (facile à lire)</p>
-            <p class="download">
-              <span class="link">Télécharger <font-awesome :icon="['fas', 'arrow-down']"/></span>
-              <font-awesome :icon="['far', 'file-image']" size="2x" transform="shrink-3" alt="Icon"/>
-            </p>
-          </div>
-        </a>
-      </section>
-
-      <section>
-        <h2>Justificatif de déplacement professionnel</h2>
-
-        <g-link to="/covid-19/justificatif-deplacement-professionnel-fr/" title="Afficher sur le web pour recopier">
-          <div class="file">
-            <p class="description">Sur le web, à recopier</p>
-            <p class="download">
-              <span class="link">Afficher <font-awesome :icon="['fas', 'arrow-right']"/></span>
-              <font-awesome :icon="['far', 'edit']" size="2x" transform="shrink-5" alt="Icon"/>
-            </p>
-          </div>
-        </g-link>
-
-        <a href="/covid-19/pdf/justificatif-deplacement-professionnel-fr.pdf" download="justificatif-deplacement-professionnel-fr.pdf" title="Télécharger au format PDF">
-          <div class="file">
-            <p class="description">Format PDF</p>
-            <p class="download">
-              <span class="link">Télécharger <font-awesome :icon="['fas', 'arrow-down']"/></span>
-              <font-awesome :icon="['far', 'file-pdf']" size="2x" transform="shrink-3" alt="Icon"/>
-            </p>
-          </div>
-        </a>
-
-        <a href="/covid-19/txt/justificatif-deplacement-professionnel-fr.txt" download="justificatif-deplacement-professionnel-fr.txt" title="Télécharger au format TXT">
-          <div class="file">
-            <p class="description">Format TXT</p>
-            <p class="download">
-              <span class="link">Télécharger <font-awesome :icon="['fas', 'arrow-down']"/></span>
-              <font-awesome :icon="['far', 'file-alt']" size="2x" transform="shrink-3" alt="Icon"/>
-            </p>
-          </div>
-        </a>
-
-        <a href="/covid-19/docx/justificatif-deplacement-professionnel-fr.docx" download="justificatif-deplacement-professionnel-fr.docx" title="Télécharger au format Word">
-          <div class="file">
-            <p class="description">Format Word</p>
-            <p class="download">
-              <span class="link">Télécharger <font-awesome :icon="['fas', 'arrow-down']"/></span>
-              <font-awesome :icon="['far', 'file-word']" size="2x" transform="shrink-3" alt="Icon"/>
-            </p>
-          </div>
-        </a>
-
-        <a href="/covid-19/docx/justificatif-deplacement-professionnel-en.docx" download="justificatif-deplacement-professionnel-en.docx" title="Download the Word file">
-          <div class="file">
-            <p class="description">WORD file in english</p>
-            <p class="download">
-              <span class="link">Download <font-awesome :icon="['fas', 'arrow-down']"/></span>
-              <font-awesome :icon="['far', 'file-word']" size="2x" transform="shrink-3" alt="Icon"/>
-            </p>
-          </div>
-        </a>
-
-      </section> -->
-
       <section id="initiatives">
-        <!-- <h2>Me porter volontaire</h2> -->
 
         <div class="website">
           <p class="logo"><g-image alt="Logo Réserve civique" src="~/assets/images/websites/rc.png"/></p>
@@ -206,15 +80,6 @@
           <a href="https://fr.ulule.com/channels/solidarite-coronavirus/" target="_blank">Accéder au site <font-awesome :icon="['fas', 'arrow-right']" transform="shrink-2"/></a>
         </div>
 
-        <!-- Supprimé car site web plus disponible -->
-        <!-- <div class="website">
-          <p class="logo"><g-image alt="Logo Open Covid" src="~/assets/images/websites/opencovid.png"/></p>
-          <p class="name">Data Against COVID-19 / FR</p>
-          <p class="tags"><span class="tag">Initiative citoyenne</span><span class="tag">Data</span></p>
-          <p class="description">Une organisation informelle issue de la société civile avec des développeurs, des data scientist, des ingénieurs, des chefs de projets et talents de tous corps de métiers qui s'organisent ensemble pour fournir des données consolidées sur l'épidémie de covid 19 et proposer des outils de visualisation.</p>
-          <a href="https://opencovid19.fr/" target="_blank">Accéder au site <font-awesome :icon="['fas', 'arrow-right']" transform="shrink-2"/></a>
-        </div> -->
-
         <div class="website">
           <p class="logo"><g-image alt="Logo Faire mes courses" src="~/assets/images/websites/faire-mes-courses.svg"/></p>
           <p class="name">FaireMesCourses.fr - Devenir ambassadeur ?</p>
@@ -234,7 +99,6 @@
         <p><i>Vous souhaitez ajouter une initiative ? <a href="mailto:contact@design.numerique.gouv.fr" target="_blank">Contactez-nous</a> ou contribuez sur notre <a href="https://github.com/DISIC/design.numerique.gouv.fr" target="_blank">github</a> !</i></p>
 
       </section>
-
     </div>
 
   </Layout>
@@ -302,55 +166,6 @@ export default {
       }
     }
   }
-
-  // .entete {
-  //   margin-bottom: 64px;
-  // }
-
-  // .file {
-  //   display: inline-block;
-  //   padding: 16px;
-  //   margin: 20px 20px 0 0;
-  //   border: 2px solid $gray;
-  //   background-color: $gray;
-  //   border-radius: 16px;
-  //   width: 192px;
-  //
-  //   p {
-  //     margin: 0;
-  //   }
-  //
-  //   @media only screen and (max-width: $mobileMaxWidth) {
-  //     margin: 12px 12px 0 0;
-  //     width: 80%;
-  //   }
-  //
-  //   &:hover, &:focus {
-  //     border: 2px solid $blue;
-  //   }
-  //
-  //   .description {
-  //     font-weight: bold;
-  //     color: $dark;
-  //     margin-bottom: 8px;
-  //   }
-  //
-  //   .download {
-  //     display: flex;
-  //     align-items: flex-end;
-  //     justify-content: space-between;
-  //
-  //     .link {
-  //       text-align: left;
-  //       font-size: 0.875em;
-  //       color: $blue;
-  //
-  //       svg {
-  //         padding-left: 4px;
-  //       }
-  //     }
-  //   }
-  // }
 
   .website {
     display: inline-block;

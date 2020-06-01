25a3ce56d99b3f6fcfe491ecef905c2b98458aea
Marine Boudeau <m14868@gmail.com>
2020-06-01 11:11:20 +0200

    Update copy for commando

--- src/pages/commando-ux.vue
+++ src/pages/commando-ux.vue
@@ -29,13 +29,13 @@
       <section class="subsection">
         <h3>Vous travaillez sur un projet de l'Observatoire ?</h3>
         <p>
-          Vous avez jusqu'au 19 juin 2020 pour nous soumettre le projet de l'Observatoire sur lequel vous travaillez et pour lequel vous souhaitez drastiquement améliorer l'expérience pour les usagers.
+          Vous avez jusqu'au <strong>19 juin 2020</strong> pour nous soumettre le projet de l'Observatoire sur lequel vous travaillez et pour lequel vous souhaitez drastiquement améliorer l'expérience pour les usagers.
         </p>
       </section>
       <section class="subsection">
         <h3>À la clé pour vous</h3>
         <p>
-          Au terme des 2 semaines d'appel à défis, nous selectionnerons les 5 à 10 projets les plus impactants pour les usagers. Nous recruterons des designers et développeurs qui auront 4 mois, de septembre à décembre, pour améliorer concrètement la qualité de l'expérience perçue sur votre service numérique.
+          Au terme des 2 semaines d'appel à défis, nous selectionnerons les 5 à 10 projets les plus impactants pour les usagers. Nous recruterons des designers et développeurs qui auront 4 mois, de septembre 2020 à décembre 2020, pour améliorer concrètement la qualité de l'expérience perçue sur votre service numérique.
         </p>
         <p>
           En fonction des défis, nous recruterons des designers, avec un appui potentiel de développeurs si vos équipes en manquent. Les développeurs devront connaitre les technologies et langages utilisés sur vos services numériques. Dans tous les cas, la possibilité pour les designers de voir leurs préconisations mises en œuvre par des développeurs est une condition indispensable.
@@ -49,50 +49,74 @@
         </p>
       </section>
 
-      <section class="subsection">
-        <h2>
-          <CommandoUX class="icon" aria-hidden="true"/>Ce qu'on cherche dans vos défis
-        </h2>
+      <section class="quote">
+        <blockquote>Il ne suffit pas de concevoir des produits fonctionnels, compréhensibles et utilisables, nous devons également créer des produits qui procurent de la joie et de l'excitation, du plaisir, du désir et, effectivement de la beauté dans la vie des gens.</blockquote>
+        <cite>— Don Norman</cite>
       </section>
-      <section class="subsection">
-        <h3>Améliorer la satisfaction à l’usage des services</h3>
-        <ul>
-          <li>Une meilleure analyse et compréhensions des besoins des usagers sur chacun de ces services, en exploitant les données sur l’avis des usagers qui sont disponibles et/ou en en collectant de nouvelles</li>
-          <li>Parcours simplifiés et agréables (notion de plaisir et de lisibilité à l’utilisation) pour les usagers avec une approche « mobile first » qui démontre un accès sur mobile exemplaire</li>
-          <li>Mise en place du bouton « Je donne mon avis » ainsi que les moyens d’étudier en continu les retours usagers</li>
-          <li>Mise en accessibilité des démarches (prise en compte handicap)</li>
-        </ul>
-      </section>
+
       <section>
-        <h3>Accélérer la numérisation des services</h3>
+        <h2>
+          <CommandoUX class="icon" aria-hidden="true"/>Apporter du plaisir aux usagers
+        </h2>
+        <p>À travers ce programme, notre objectif est de vous accompagner pour ensemble transformer ces services afin qu'ils soient mieux pensés pour les usagers, plus efficaces et qu'ils apportent du plaisir.</p>
         <ul>
-          <li>Accompagnement complet en proposant une démarche pour la dématérialisation d’un service</li>
-          <li>Meilleure utilisation des APIs et du principe du Dites-le-nous une fois (DLNUF).</li>
+          <li>Développer une meilleure analyse et compréhensions des besoins des usagers sur chacun de ces services en exploitant les données sont disponibles et/ou en en collectant de nouvelles</li>
+          <li>Concevoir des parcours simplifiés et agréables (notion de plaisir et de lisibilité à l’utilisation) pour les usagers avec une approche « mobile first » qui démontre un accès sur mobile exemplaire</li>
+          <li>S'assurer que tous les usagers, qu'ils soient en situation de handicap, ou simplement éloignés du numérique aient un égal accès à ces services</li>
+          <li>Exploiter les APIs de l'État pour réduire le nombre d'informations demandées aux usagers (principe du Dites-le-nous une fois)</li>
         </ul>
       </section>
 
       <section>
         <h2>
-          <CommandoUX class="icon" aria-hidden="true"/>Critères de selection
+          <CommandoUX class="icon" aria-hidden="true"/>Nos critères de selection
         </h2>
+        <p>Il vous suffit de remplir les conditions suivantes :</p>
         <ul>
-          <li>Objectifs et indicateurs de réussite clairement énoncés : cibles, complexités levées, délais de mise en œuvre, nombre d'usagers impactés</li>
-          <li>Qualité de l’équipe d’agents publics impliquée dans le projet : temps consacré au défi, motivation et sponsorship hiérarchique</li>
+          <li>Que vos objectifs et indicateurs de réussite soient clairement énoncés : cibles, complexités levées, délais de mise en œuvre, nombre d'usagers impactés par les améliorations</li>
+          <li>Que vous soyiez impliqué dans la réussite du projet : temps consacré au défi, motivation et sponsorship hiérarchique</li>
         </ul>
+      </section>
+
+      <section>
+        <h2>
+          <CommandoUX class="icon" aria-hidden="true"/>Vous êtes designer ou développeur
+        </h2>
+        <p>
+          Vous souhaitez travailler sur un projet d'intérêt général à fort impact ? Vous êtes libre de septembre à décembre 2020 ? <strong>RDV le lundi 29 juin 2020</strong> pour candidater sur le programme Commando UX. Si vous êtes retenu, vous intégrerez l'équipe Commando UX, serez détaché dans un des défis et aurez 4 mois pour améliorer l'expérience des usagers sur le service.
+        </p>
+        <p>
+          Nous cherchons avant tout des designers techniques, et des développeurs qui s'intéressent aux besoins des usagers.
+        </p>
+        <p>
+          L'équipe Design des services numériques de la Direction interministérielle du numérique (DINUM) vous accompagnera tout au long du projet.
+        </p>
       </section>
 
       <section>
         <h2>
           <CommandoUX class="icon" aria-hidden="true"/>Dates clé
         </h2>
-        <h3>8 – 19 juin 2020 (2 semaines)</h3>
-        <p>Appel à défis : vous soumettez votre projet</p>
-        <h3>22 – 26 juin 2020 (1 semaine)</h3>
-        <p>Étude et sélection des défis : nous selectionnons les 5 à 10 projets les plus impactants</p>
-        <h3>29 juin – 17 juillet 2020 (3 semaines)</h3>
-        <p>Appel à candidatures : vous êtes designer ou développeur et ce programme vous séduit ? Candidatez !</p>
-        <h3>20 juillet 2020 – 31 juillet 2020 (2 semaines)</h3>
-        <p>Recrutement des lauréats (designers et développeurs)</p>
+        <p>
+          <strong>8 juin – 19 juin 2020</strong><br>
+          Appel à défis : administrations, soumettez votre projet de l'Observatoire. Un formulaire sera prévu à cet effet.
+        </p>
+        <p>
+          <strong>22 juin – 26 juin 2020</strong><br>
+          Étude et sélection des défis : nous selectionnons les 5 à 10 projets les plus impactants.
+        </p>
+        <p>
+          <strong>29 juin – 17 juillet 2020</strong><br>
+          Appel à candidatures : vous êtes designer ou développeur et ce programme vous séduit ? Candidatez !
+        </p>
+        <p>
+          <strong>20 juillet – 31 juillet 2020</strong><br>
+          Recrutement Commando UX : nous recrutons les designers et développeurs.
+        </p>
+        <p>
+          <strong>7 septembre – 31 décembre 2020</strong><br>
+          Commando UX en action : designers et développeurs intègrent les défis.
+        </p>
       </section>
 
     </div>
@@ -159,6 +183,39 @@
 
     .subsection {
       margin-bottom: 40px;
+    }
+
+    .quote {
+      blockquote {
+        font-family: "Marianne";
+        font-weight: 100;
+        font-size: 1.5rem;
+        max-width: 600px;
+        line-height: 1.4;
+        position: relative;
+        margin: 0;
+        padding: .5rem 0;
+        color: $blue;
+
+        &:before {
+          font-family: "Marianne";
+          font-weight: 500;
+          position: absolute;
+          color: $red;
+          font-size: 8rem;
+          width: 4rem;
+          height: 4rem;
+          content: '«';
+          left: -5rem;
+          top: -4rem;
+        }
+      }
+
+      cite {
+        color: $mid-gray;
+        line-height: 3;
+        text-align: left;
+      }
     }
   }
 

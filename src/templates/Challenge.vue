54e69ce8eaf71aeb8affc6f1c77a863a9caf265a
Faustine <faustine@MacBook-Pro.local>
2020-06-25 18:09:56 +0200

    challenge template and content file

--- src/templates/Challenge.vue
+++ src/templates/Challenge.vue
@@ -1,6 +1,5 @@
 <template>
   <Layout>
-
     <nav aria-label="Breadcrumb" class="breadcrumb">
       <ol>
         <li>
@@ -10,45 +9,92 @@
           <g-link to="/commando-ux/">Commando UX</g-link>
         </li>
         <li>
-          <g-link to="/accessibilite-numerique/cadre-legal" aria-current="page">Obligations légales et sanctions</g-link>
+          <g-link
+            to="/accessibilite-numerique/cadre-legal"
+            aria-current="page"
+          ><span v-html="$page.challenge.title" /></g-link>
         </li>
       </ol>
     </nav>
 
     <div class="cover cover--with-breadcrumb">
-     <div class="cover__container">
-       <p class="cover__subhead"><Services class="cover__subhead-icon" aria-hidden="true"/>Commando UX</p>
-       <h1 class="highlight">{{ $page.challenge.name }}</h1>
-     </div>
-   </div>
+      <div class="cover__container">
+        <CommandouxIllustration class="cover__illustration--small" aria-hidden="true" />
+        <!-- <p class="cover__subhead"><Services class="cover__subhead-icon" aria-hidden="true"/>Commando UX</p> -->
+        <h1>{{ $page.challenge.title }}</h1>
+        <p class="cover__subtitle">
+          <span v-html="$page.challenge.name" />
+        </p>
+        <!-- tag designer et/ou developpeur a ajouter -->
+      </div>
+    </div>
 
-   <div class="content">
-      <div v-html="$page.challenge.content" />
-   </div>
+    <div class="content challenge">
+      <section>
+        <h2>
+        Éléments clé
+        </h2>
+        <p>
+          <strong>Ministère bénéficiant du défi</strong>
+          <br />
+          <span v-html="$page.challenge.departement" />
+        </p>
+        <p>
+          <strong>Administration bénéficiant du défi</strong>
+          <br />
+          <span v-html="$page.challenge.direction" />
+        </p>
+        <p>
+          <strong>Localisation du défi</strong>
+          <br />
+          <span v-html="$page.challenge.place" />
+        </p>
+        <p>
+          <strong>Profils recherchés</strong>
+          <br />
+          <span v-html="$page.challenge.poste" />
+        </p>
 
+        <div v-html="$page.challenge.content" />
+      </section>
+
+      <section>
+        <div class="cta">
+        <a
+          href="https://www.demarches-simplifiees.fr/commencer/appel-a-defis-commando-ux"
+          target="_blank"
+          class="button"
+        >Canditater à ce défi !</a>
+        <p>Jusqu'au vendredi 19 juin 2020 à 23h59</p>
+        </div>
+      </section>
+    </div>
   </Layout>
 </template>
 
 <script>
-
-import Services from "~/assets/images/services.svg"
+import Services from "~/assets/images/services.svg";
+import CommandoUX from "~/assets/images/services.svg";
+import CommandouxIllustration from "~/assets/images/commandouxIllustration.svg";
 
 export default {
   components: {
     Services,
+    CommandoUX,
+    CommandouxIllustration
   },
-  metaInfo () {
+  metaInfo() {
     return {
-      title: this.$page.challenge.name,
+      title: this.$page.challenge.title,
       meta: [
         {
-          name: 'description',
+          name: "description",
           content: this.$page.challenge.name
         }
       ]
-    }
+    };
   }
-}
+};
 </script>
 
 <page-query>
@@ -58,9 +104,89 @@ query Challenge ($id: ID!) {
     departement
     direction
     content
+    place
+    title
+    poste
   }
 }
 </page-query>
 
 <style lang="scss">
+@import "src/assets/scss/_vars.scss";
+
+// p{
+//   margin:0;
+// }
+
+.challenge{
+  h3 {
+    color: $blue;
+    margin-top:36px;
+  }
+
+  h2{
+    margin-top: 54px;
+  }
+
+  // h3, h4{
+  //   margin-bottom:0px;
+  // }
+}
+
+.content {
+  .button {
+    text-decoration: none;
+    border-color: $blue;
+    background-color: $blue;
+    color: white;
+    border-radius: 32px;
+    padding: 8px 24px;
+    font-weight: bold;
+    display: inline-block;
+    margin: 10px 0;
+
+    @media only screen and (max-width: $mobile-max-width) {
+      font-size: 0.875em;
+    }
+
+    &:hover {
+      background: $red;
+      border-color: $red;
+    }
+  }
+
+  a {
+    svg {
+      padding-right: 12px;
+      transition: 0.1s all;
+    }
+
+    &:hover {
+      svg {
+        padding-left: 8px;
+        padding-right: 4px;
+      }
+    }
+  }
+
+  .subsection {
+    margin-bottom: 40px;
+  }
+
+  .cta {
+    text-align: center;
+
+    > p {
+      margin: 0;
+      display: block;
+      font-size: 0.9em;
+      color: $mid-gray;
+    }
+
+    @media only screen and (max-width: $mobile-max-width) {
+      text-align: left;
+    }
+  }
+}
 </style>
+

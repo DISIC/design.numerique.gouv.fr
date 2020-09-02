f5d62a85b582aa09c10ecc703be107eb6d7d93c1
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2020-09-02 08:58:23 +0200

    ajout du module skip-link

--- src/components/Skip-link.vue
+++ src/components/Skip-link.vue
@@ -10,11 +10,44 @@
     </div>
 
 </template>
-
 <script>
-export default {
-  name: 'Skiplink'
-}
+
+  export default {
+    name: 'Skiplink',
+    methods: {
+      skipLinks() {
+        var matches = Element.prototype.matches || Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+        var closest = Element.prototype.closest;
+
+        if (!closest) {
+          closest = function(s) {
+            var el = this;
+            do {
+              if (matches.call(el, s)) return el;
+              el = el.parentElement || el.parentNode;
+            } while (el !== null && el.nodeType === 1);
+            return null;
+          };
+        }
+
+        function toggleSkipLinks(evt){
+          var link = closest.call(evt.target, ".skip-link a");
+          if(!link) {
+            return;
+          }
+          var container = closest.call(link, '.container');
+          if(event.type === 'focus'){
+            container.classList.add('skip-link-focus');
+          }
+          else {
+            container.classList.remove('skip-link-focus');
+          }
+        }
+        document.body.addEventListener("focus", toggleSkipLinks, true);
+        document.body.addEventListener("blur", toggleSkipLinks, true);    
+      }
+    }
+  }
 </script>
 
 <style lang="scss" scoped>

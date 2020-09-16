faa0b47c508638c81f08f8695b02ea2beebbda7a
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2020-09-16 15:53:17 +0200

    fix focus

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -246,8 +246,12 @@ export default {
           top: 0;
           bottom: 0;
           left: 0;
-        right: 0;
-        }    
+          right: 0;
+        }
+        a:focus {
+          background-color: none;
+        }
+
       }
     }
     .header__logos {

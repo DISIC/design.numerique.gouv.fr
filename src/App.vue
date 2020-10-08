4c60db9f0fa4f86af594cf84d01fd6c154ac8663
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2020-10-08 17:12:33 +0200

    Ajout du DS

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <router-view />
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+    siteDescription
+  }
+}
+</static-query>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$static.metadata.siteName,
+      meta: [
+        {
+          key: 'description',
+          name: 'description',
+          content: this.$static.metadata.siteDescription
+        }
+      ]
+    }
+  }
+}
+</script>
+
+#

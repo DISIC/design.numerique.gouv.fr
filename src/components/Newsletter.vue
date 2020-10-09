b279fbc0a3eba717a201039db870e22cdb973c21
Erwan-le-Gall <72549390+Erwan-le-Gall@users.noreply.github.com>
2020-10-09 18:30:02 +0200

    orthotypo

--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -5,7 +5,7 @@
 
     <p>DesignGouv, c’est aussi des événements, des rencontres et des discussions pour faire vivre la culture design au sein des administrations. Rejoignez-nous&nbsp;!</p>
 
-    <a href="http://eepurl.com/gLJCsb" title="Je m'inscris à la newsletter - Nouvelle fenêtre" target="_blank" class="button">Je m'inscris à la <span lang="en">newsletter</span></a>
+    <a href="http://eepurl.com/gLJCsb" title="Je m’inscris à la newsletter - Nouvelle fenêtre" target="_blank" class="button">Je m’inscris à la <span lang="en">newsletter</span></a>
 
     <p>Sur Twitter : <a href="https://twitter.com/design_gouv" title="Twitter DesignGouv - Nouvelle fenêtre" target="_blank">@design_gouv</a></p>
   </div>

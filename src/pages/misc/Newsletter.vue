3b6d7cab9116e8545cbf567575c32aba30a16a23
Ugo <jeanmichelpaul@icloud.com>
2020-11-10 19:33:34 +0100

    Create Newsletter page with form to mailchimp

---
+++ src/pages/misc/Newsletter.vue
@@ -0,0 +1,234 @@
+<template>
+  <Layout class="accessibility-page">
+
+    <nav aria-label="Breadcrumb" class="breadcrumb">
+      <ol>
+        <li>
+          <g-link to="/">Accueil</g-link>
+        </li>
+        <li>
+          <span aria-current="page">Newsletter</span>
+        </li>
+      </ol>
+    </nav>
+
+    <div class="cover">
+     <div class="cover__container">
+
+       <h1>Newsletter</h1>
+
+      </div>
+
+      <p class="cover__subtitle">
+        DesignGouv, c’est aussi des événements, des rencontres et des discussions pour faire vivre la culture design au sein des administrations. Rejoignez-nous !
+      </p>
+
+    </div>
+
+    <div class="content">
+
+      <form action="https://gouv.us5.list-manage.com/subscribe/post?u=c921e95d674341b87fd4fb6e6&amp;id=bc185cd5f1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+
+        <div class="indicates-required"><span class="asterisk">*</span> champs obligatoires</div>
+
+        <div class="mc-field-group">
+          <label for="mce-EMAIL">
+            Votre e-mail  <span class="asterisk">*</span>
+          </label>
+          <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+        </div>
+
+        <div class="mc-field-group">
+          <label for="mce-FNAME">Votre prénom </label>
+          <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+        </div>
+
+        <div class="mc-field-group">
+          <label for="mce-LNAME">Votre nom </label>
+          <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+        </div>
+
+        <div class="mc-field-group">
+          <label for="mce-MMERGE6">Votre code postal </label>
+          <input type="text" value="" name="MMERGE6" class="" id="mce-MMERGE6">
+        </div>
+
+        <div class="mc-field-group">
+          <label for="mce-MMERGE5">Votre profession / titre actuel </label>
+          <input type="text" value="" name="MMERGE5" class="" id="mce-MMERGE5">
+        </div>
+
+        <div class="mc-field-group">
+          <label for="mce-group[21897]">Vous travaillez dans... </label>
+          <select name="group[21897]" class="REQ_CSS" id="mce-group[21897]">
+            <option value=""></option>
+            <option value="1">La fonction publique de l'État</option>
+            <option value="2">La fonction publique des collectivités</option>
+            <option value="4">Le privé</option>
+            <option value="8">Je suis étudiant(e)</option>
+            <option value="16">Autre</option>
+          </select>
+        </div>
+
+        <div class="mc-field-group input-group">
+          <strong>Votre expertise professionelle </strong>
+          <ul>
+            <li><input type="checkbox" value="32" name="group[21901][32]" id="mce-group[21901]-21901-0"><label for="mce-group[21901]-21901-0">Designer UX</label></li>
+            <li><input type="checkbox" value="512" name="group[21901][512]" id="mce-group[21901]-21901-1"><label for="mce-group[21901]-21901-1">Designer UI</label></li>
+            <li><input type="checkbox" value="2048" name="group[21901][2048]" id="mce-group[21901]-21901-2"><label for="mce-group[21901]-21901-2">Designer de services</label></li>
+            <li><input type="checkbox" value="64" name="group[21901][64]" id="mce-group[21901]-21901-3"><label for="mce-group[21901]-21901-3">Chef / cheffe de produit (product owner)</label></li>
+            <li><input type="checkbox" value="262144" name="group[21901][262144]" id="mce-group[21901]-21901-4"><label for="mce-group[21901]-21901-4">Chercheur / chercheuse utilisateur (user researcher)</label></li>
+            <li><input type="checkbox" value="1024" name="group[21901][1024]" id="mce-group[21901]-21901-5"><label for="mce-group[21901]-21901-5">Scrum master</label></li>
+            <li><input type="checkbox" value="128" name="group[21901][128]" id="mce-group[21901]-21901-6"><label for="mce-group[21901]-21901-6">Développeur / développeuse front-end</label></li>
+            <li><input type="checkbox" value="4096" name="group[21901][4096]" id="mce-group[21901]-21901-7"><label for="mce-group[21901]-21901-7">Développeur / développeuse back-end</label></li>
+            <li><input type="checkbox" value="8192" name="group[21901][8192]" id="mce-group[21901]-21901-8"><label for="mce-group[21901]-21901-8">Développeur / développeuse accessibilité</label></li>
+            <li><input type="checkbox" value="16384" name="group[21901][16384]" id="mce-group[21901]-21901-9"><label for="mce-group[21901]-21901-9">Spécialiste en sciences comportementales</label></li>
+            <li><input type="checkbox" value="32768" name="group[21901][32768]" id="mce-group[21901]-21901-10"><label for="mce-group[21901]-21901-10">Rédacteur (UX writer)</label></li>
+            <li><input type="checkbox" value="65536" name="group[21901][65536]" id="mce-group[21901]-21901-11"><label for="mce-group[21901]-21901-11">Juriste</label></li>
+            <li><input type="checkbox" value="131072" name="group[21901][131072]" id="mce-group[21901]-21901-12"><label for="mce-group[21901]-21901-12">Data scientist</label></li>
+            <li><input type="checkbox" value="256" name="group[21901][256]" id="mce-group[21901]-21901-13"><label for="mce-group[21901]-21901-13">Autre</label></li>
+          </ul>
+        </div>
+
+        <div id="mce-responses" class="clear">
+          <div class="response" id="mce-error-response" style="display:none"></div>
+          <div class="response" id="mce-success-response" style="display:none"></div>
+        </div>
+
+        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c921e95d674341b87fd4fb6e6_bc185cd5f1" tabindex="-1" value=""></div>
+        <div class="clear"><input type="submit" value="Je m'inscris" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+
+      </form>
+
+      <p class="rgpd">
+        Les données recueillies sur ce formulaire sont traitées par les équipes de la DINUM. Elles nous permettent de vous informer via e-mail des actualités de la communauté DesignGouv.
+      </p>
+
+      <p class="rgpd rgpd--small">
+        Conformément à la règlementation, vous disposez d’un droit d’opposition et d’un droit à la limitation du traitement de données vous concernant, ainsi que d’un droit d’accès, de rectification, de portabilité et d’effacement de vos données. Vous pouvez exercer vos droits en nous écrivant à l’adresse électronique suivante : contact@design.numerique.gouv.fr.
+      </p>
+
+    </div>
+
+  </Layout>
+</template>
+
+
+<script>
+
+  export default {
+    // mounted() {
+    //     (
+    //       function ($) {
+    //       window.fnames = new Array();
+    //       window.ftypes = new Array();
+    //       fnames[0]="EMAIL";
+    //       ftypes[0]="email";
+    //       fnames[1]="FNAME";
+    //       ftypes[1]="text";
+    //       fnames[2]="LNAME";
+    //       ftypes[2]="text";
+    //       fnames[6]="MMERGE6";
+    //       ftypes[6]="text";
+    //       fnames[5]="MMERGE5";
+    //       ftypes[5]="text";
+    //       fnames[4]="PHONE";
+    //       ftypes[4]="phone";
+    //       $.extend(
+    //         $.validator.messages,
+    //         {
+    //           required: "Ce champ est requis.",
+    //           remote: "Veuillez remplir ce champ pour continuer.",
+    //           email: "Veuillez entrer une adresse email valide.",
+    //           url: "Veuillez entrer une URL valide.",
+    //           date: "Veuillez entrer une date valide.",
+    //           dateISO: "Veuillez entrer une date valide (ISO).",
+    //           number: "Veuillez entrer un nombre valide.",
+    //           digits: "Veuillez entrer (seulement) une valeur numérique.",
+    //           creditcard: "Veuillez entrer un numéro de carte de crédit valide.",
+    //           equalTo: "Veuillez entrer une nouvelle fois la même valeur.",
+    //           accept: "Veuillez entrer une valeur avec une extension valide.",
+    //           maxlength: $.validator.format("Veuillez ne pas entrer plus de {0} caractères."),
+    //           minlength: $.validator.format("Veuillez entrer au moins {0} caractères."),
+    //           rangelength: $.validator.format("Veuillez entrer entre {0} et {1} caractères."),
+    //           range: $.validator.format("Veuillez entrer une valeur entre {0} et {1}."),
+    //           max: $.validator.format("Veuillez entrer une valeur inférieure ou égale à {0}."),
+    //           min: $.validator.format("Veuillez entrer une valeur supérieure ou égale à {0}.")
+    //         }
+    //       );
+    //     }(jQuery));
+    //     var $mcj = jQuery.noConflict(true);
+    //   },
+    metaInfo: {
+      title: "Accessibilité",
+      meta: [{
+        name: "robots",
+        content: "noindex"
+      }],
+      // script: [
+      //   { src: "//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js" },
+      // ],
+    }
+  }
+
+</script>
+
+
+<style lang="scss">
+
+  @import "src/assets/scss/_vars.scss";
+
+  .accessibility-page {
+
+    .content {
+
+      width: 480px;
+      margin:auto;
+
+      .rgpd {
+
+        &--small {
+          color: $dark-gray;
+          font-size: 12px;
+          margin-bottom: 32px;
+        }
+      }
+
+      #mc-embedded-subscribe {
+        margin: 8px 0 32px 0;
+      }
+
+      /* MailChimp Form Embed Code - Classic - 12/17/2015 v10.7 */
+      form {display:block; position:relative; text-align:left;}
+      input {border: 2px solid $black;}
+      input[type=checkbox]{-webkit-appearance:checkbox;}
+      input[type=radio]{-webkit-appearance:radio;}
+
+      .mc-field-group {clear:left; position:relative; width:96%; padding-bottom:3%; min-height:50px;}
+      .mc-field-group label {display:block; margin-bottom:4px;}
+      .mc-field-group input {display:block; width:100%; padding:8px 0; text-indent:2%;}
+      .mc-field-group select {display:inline-block; width:99%; padding:5px 0; margin-bottom:2px;}
+
+      .indicates-required {text-align:right; font-size:12px; margin-right:4%;}
+      .asterisk {color:#e85c41; font-size:150%; font-weight:normal; position:relative; top:5px;}
+      .clear {clear:both;}
+
+      .mc-field-group.input-group ul {margin:0; padding:5px 0; list-style:none;}
+      .mc-field-group.input-group ul li {display:block; padding:3px 0; margin:0;}
+      .mc-field-group.input-group label {display:inline;}
+      .mc-field-group.input-group input {display:inline; width:auto; border:none;}
+
+      // div#mce-responses {float:left; top:-1.4em; padding:0em .5em 0em .5em; overflow:hidden; width:90%; margin: 0 5%; clear: both;}
+      // div.response {margin:1em 0; padding:1em .5em .5em 0; font-weight:bold; float:left; top:-1.5em; z-index:1; width:80%;}
+      // #mce-error-response {display:none;}
+      // #mce-success-response {color:#529214; display:none;}
+      // label.error {display:block; float:none; width:auto; margin-left:1.05em; text-align:left; padding:.5em 0;}
+
+      // #mc-embedded-subscribe-form div.mce_inline_error {display:inline-block; margin:2px 0 1em 0; padding:5px 10px; background-color:rgba(255,255,255,0.85); -webkit-border-radius: 3px; -moz-border-radius: 3px; border-radius: 3px; font-size:14px; font-weight:normal; z-index:1; color:#e85c41;}
+      // #mc-embedded-subscribe-form input.mce_inline_error {border:2px solid #e85c41;}
+
+    }
+  }
+
+</style>

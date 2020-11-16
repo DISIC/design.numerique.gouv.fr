56216377ecd1dd43283f27d141b7d208bde11b45
Ugo <34340568+ugodtn@users.noreply.github.com>
2020-11-16 12:56:36 +0100

    Fix text again

--- src/components/messages/Offer.vue
+++ src/components/messages/Offer.vue
@@ -3,7 +3,7 @@
   <div class="message">
     <h2>Vous avez besoin d'aide&nbsp;?</h2>
 
-    <p>Vous êtes une administration et vous souhaitez améliorer l'expérience usager (UX) et l'accessibilité de l'un de vos services ? Nous pouvons vous accomagner&nbsp;!</p>
+    <p>Vous êtes une administration et vous souhaitez améliorer l'expérience usager (UX) et l'accessibilité de l'un de vos services ? Nous pouvons vous accompagner&nbsp;!</p>
 
     <g-link class="button" to="/services" title="Nos offres d'accompagnement">Nos offres d'accompagnement</g-link>
   </div>

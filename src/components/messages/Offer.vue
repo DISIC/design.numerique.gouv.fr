511211927f98b62b61543d8e858e061ac6d1bef8
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2020-12-03 12:07:13 +0100

    Marges

--- src/components/messages/Offer.vue
+++ src/components/messages/Offer.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div class="message">
+  <div class="message rf-mt-8w">
     <h2>Vous avez besoin d'aide&nbsp;?</h2>
 
     <p>Vous êtes une administration et vous souhaitez améliorer l'expérience usager (UX) et l'accessibilité de l'un de vos services ? Nous pouvons vous accompagner&nbsp;!</p>

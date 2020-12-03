247cdf02deffb82d1f2fc8c14b41aca26c9c20a8
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2020-12-03 12:07:00 +0100

    lien d'évitement

--- src/components/Skip-link.vue
+++ src/components/Skip-link.vue
@@ -1,52 +1,20 @@
 <template>
-
-    <div class="skip-link">
-      <div id="top">
-          <ul class="container">
-              <li><a href="#main">Aller au contenu</a></li>
-              <li><a href="#menu-button">Aller au menu</a></li>
-          </ul>
-      </div>
+<div class="rf-skiplinks">
+    <div class="rf-container">
+        <ul class="rf-skiplinks__list">
+            <li>
+                <a class="rf-link" href="#contenu">Accéder au contenu</a>
+            </li>
+        </ul>
     </div>
+</div>
+
 
 </template>
 <script>
 
   export default {
-    name: 'SkipLink',
-    methods: {
-      SkipLink: function() {
-        var matches = Element.prototype.matches || Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
-        var closest = Element.prototype.closest;
-
-        if (!closest) {
-          closest = function(s) {
-            var el = this;
-            do {
-              if (matches.call(el, s)) return el;
-              el = el.parentElement || el.parentNode;
-            } while (el !== null && el.nodeType === 1);
-            return null;
-          };
-        }
-
-        function toggleSkipLinks(evt){
-          var link = closest.call(evt.target, ".skip-link a");
-          if(!link) {
-            return;
-          }
-          var container = closest.call(link, '.container');
-          if(event.type === 'focus'){
-            container.classList.add('skip-link-focus');
-          }
-          else {
-            container.classList.remove('skip-link-focus');
-          }
-        }
-        document.body.addEventListener("focus", toggleSkipLinks, true);
-        document.body.addEventListener("blur", toggleSkipLinks, true);    
-      }
-    }
+    name: 'Skiplink'
   }
 </script>
 

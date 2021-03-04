0c72e37c7d7acbdcca082363681457604540d735
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2021-03-04 11:02:35 +0100

    le diag flash remplace la chekclist-rapide

--- src/pages/misc/PlanDuSite.vue
+++ src/pages/misc/PlanDuSite.vue
@@ -40,7 +40,7 @@
           <li><a href="/accessibilite-numerique/jeu-de-oaa/">Organiser l’amélioration de l‘accessibilité</a></li>
           <li><a href="/outils/checklist-dev">La checklist des développeurs et développeuses</a></li>
           <li><a href="/outils/memo-dev">Le mémo développement</a></li>
-          <li><a href="/outils/checklist-rapide">L‘évaluation rapide</a></li>
+          <li><a href="/outils/diag">Le diagnostic flash</a></li>
           <li><a href="/outils/audit-rapide">L‘audit rapide RGAA</a></li>
           </ul>
           <h2><a href="/formations/">Formations</a></h2>

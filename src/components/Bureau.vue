55e753e5ebf8d4522a428ac00a7980627df012cf
Ugo <jeanmichelpaul@icloud.com>
2021-03-16 19:36:36 +0100

    Add modal on BDD

--- src/components/Bureau.vue
+++ src/components/Bureau.vue
@@ -5,31 +5,86 @@
       <h2><Design class="design-office__title-icon" focusable="false" aria-hidden="true"/>Le bureau du Design</h2>
       <p class="design-office__date" v-html="(new Date()).toLocaleDateString('fr-FR', {weekday: 'short', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric'}).replace(' à ', ' ')"></p>
     </div>
+
+
     <div class="design-office__content">
+
       <div class="design-office__top">
-        <div class="">
-          <h3><File class="design-office__element-icon" focusable="false" aria-hidden="true"/><span>Lisez-moi</span></h3>
+
+        <div class="design-office__container">
+          <div class="design-office__element">
+            <p><File class="design-office__element-icon" focusable="false" aria-hidden="true"/><button aria-expanded="false" aria-controls="readme">Lisez-moi</button></p>
+
+            <dialog aria-labelledby="rf-modal-readme" id="readme" class="rf-modal">
+                <div class="rf-container">
+                    <div class="rf-grid-row rf-grid-row--center">
+                        <div class="rf-col-xs-12 rf-col-sm-10 rf-col-md-6">
+                            <div class="rf-modal__body">
+                                <div class="rf-modal__header">
+                                    <h1 id="rf-modal-readme" class="rf-modal__title">Lisez-moi </h1>
+                                    <button class="rf-link--close rf-link" title="Fermer la fenêtre modale" aria-controls="readme">Fermer</button>
+                                </div>
+                                <div class="rf-modal__content">
+                                  <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum hendrerit, dolor pulvinar feugiat aliquam, risus mi venenatis magna, id ornare quam quam vitae sapien. Donec sed lorem condimentum, semper mauris eu, dictum velit. Etiam ac magna euismod, malesuada sapien a, aliquam risus. Proin viverra malesuada cursus. Nulla viverra congue leo, vel semper diam pretium sit amet. Curabitur laoreet, urna eu semper volutpat, ligula metus auctor mauris, non convallis eros nunc eget ipsum. Nulla facilisi. Suspendisse potenti. Maecenas luctus lectus ac sapien facilisis scelerisque. In mi massa, ultrices nec interdum a, porttitor ac augue. Donec quis placerat elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                                  </p>
+                                  <p>
+                                    Nam mauris urna, tincidunt a libero ac, pharetra congue tellus. Vestibulum mauris ligula, vehicula non libero quis, eleifend aliquet eros. Pellentesque facilisis eros sit amet ligula egestas accumsan. Vivamus a mollis magna. Aliquam dapibus urna ac sodales dapibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut elit est, rhoncus in sodales in, mollis vitae orci. Phasellus vel justo varius, egestas nibh sed, malesuada massa. Nunc elit sem, venenatis a lorem in, rutrum tempor leo. Duis hendrerit ipsum a leo porta volutpat. Curabitur at malesuada nibh.
+                                  </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </dialog>
+
+          </div>
         </div>
-        <div class="">
-          <h3><Folder class="design-office__element-icon" focusable="false" aria-hidden="true"/><span>Actualité</span></h3>
+
+        <div class="design-office__container">
+          <div class="design-office__element">
+            <p><Folder class="design-office__element-icon" focusable="false" aria-hidden="true"/><span>Actualité</span></p>
+          </div>
         </div>
+
       </div>
+
+
       <div class="design-office__middle">
-        <div class="">
-          <h3><Folder class="design-office__element-icon" focusable="false" aria-hidden="true"/><span>Avant</span></h3>
-          <h3><Folder class="design-office__element-icon" focusable="false" aria-hidden="true"/><span>Pendant</span></h3>
-          <h3><Folder class="design-office__element-icon" focusable="false" aria-hidden="true"/><span>Après</span></h3>
+
+        <div class="design-office__container">
+          <div class="design-office__element">
+            <h3><Folder class="design-office__element-icon" focusable="false" aria-hidden="true"/><span>Avant</span></h3>
+          </div>
+          <div class="design-office__element">
+            <h3><Folder class="design-office__element-icon" focusable="false" aria-hidden="true"/><span>Pendant</span></h3>
+          </div>
+          <div class="design-office__element">
+            <h3><Folder class="design-office__element-icon" focusable="false" aria-hidden="true"/><span>Après</span></h3>
+          </div>
         </div>
       </div>
+
+
       <div class="design-office__bottom">
-        <div class="">
-          <p><g-image src="~/assets/images/bureau/macron.png" class="design-office__element-image" width="64px" alt="Emmanuel Macron souriant"/><span>Image.bmp</span></p>
+
+        <div class="design-office__container">
+          <div class="design-office__element">
+            <p><g-image src="~/assets/images/bureau/macron.png" class="design-office__element-image" width="64px" alt="Emmanuel Macron souriant"/><span>Image.bmp</span></p>
+          </div>
         </div>
-        <div class="">
-          <h3><Slack class="design-office__element-icon" focusable="false" aria-hidden="true"/><span>Slack</span></h3>
-          <h3><Support class="design-office__element-icon" focusable="false" aria-hidden="true"/><g-link to="/accompagnement/">Accompagnement</g-link></h3>
+
+        <div class="design-office__container">
+          <div class="design-office__element">
+            <p><Slack class="design-office__element-icon" focusable="false" aria-hidden="true"/><span>Slack</span></p>
+          </div>
+          <div class="design-office__element">
+            <p><Support class="design-office__element-icon" focusable="false" aria-hidden="true"/><g-link to="/accompagnement/">Accompagnement</g-link></p>
+          </div>
         </div>
+
       </div>
+
     </div>
 
   </div>
@@ -113,37 +168,69 @@
       display: flex;
       flex-direction: row;
       justify-content: space-between;
+    }
 
-      div {
+    &__container {
+      display: flex;
+      flex-direction: row;
+      align-items: flex-end;;
+    }
+
+    &__element {
+
+      h3, p {
+        font-family: "Marianne", "Helvetica Neue", Arial, sans-serif;
+        margin: 1rem 2rem;
         display: flex;
-        flex-direction: row;
-        align-items: flex-end;;
+        flex-direction: column;
+        align-items: center;
+        position: relative;
 
-        h3, p {
-          margin: 1rem 2rem;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          position: relative;
+        span, a, button {
+          font-size: 0.875rem;
+          margin-top: 0.25rem;
+          padding: 0.125rem 0.5rem;
+          background-color: white;
+          font-weight: bold;
+          border: none !important;
 
-          span, a {
-            font-size: 0.875rem;
-            margin-top: 0.25rem;
-            padding: 0.125rem 0.5rem;
-            background-color: white;
-            font-weight: bold;
-            border-bottom: none !important;
-
-            &:after {
-              position: absolute;
-              content: "";
-              top: 0;
-              bottom: 0;
-              left: 0;
-              right: 0;
-            }
+          &:after {
+            position: absolute;
+            content: "";
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
           }
         }
+
+        button {
+          color: $blue;
+
+          &:hover {
+            color: $red;
+            cursor: pointer;
+          }
+
+          &:focus {
+            background-color: $light;
+            outline-color: $blue;
+          }
+
+          &:active {
+            color: $blue;
+            background-color: $light;
+          }
+        }
+      }
+
+      &-icon {
+        display: block;
+        margin-right: -0.25rem;
+      }
+
+      &-image {
+        display: block;
       }
     }
 
@@ -153,13 +240,36 @@
       }
     }
 
-    &__element-icon {
-      display: block;
-      margin-right: -0.25rem;
-    }
+    .rf-modal {
 
-    &__element-image {
-      display: block;
+      &__body {
+        background-color: $light-gray;
+        border: 0.125rem solid $black;
+        box-shadow: 0.25rem 0.25rem 0 $dark-gray;
+        padding: 0;
+      }
+
+      &__header {
+        border-bottom: 0.125rem solid $black;
+        padding: 0;
+        margin: 0;
+        display: flex;
+        justify-content: space-between;
+
+        button {
+          border-radius: 0;
+        }
+      }
+
+      &__title {
+        font-size: 1rem !important;
+        margin: 0 !important;
+        padding-left: 1rem;
+      }
+
+      &__content {
+        margin: 1rem !important;
+      }
     }
   }
 

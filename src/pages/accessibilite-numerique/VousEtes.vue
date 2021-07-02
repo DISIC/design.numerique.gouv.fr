20965ac5e9527cacff1ffb8c7231f56de9787163
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2021-07-02 15:40:23 +0200

    Tuiles, meta et fix wording

--- src/pages/accessibilite-numerique/VousEtes.vue
+++ src/pages/accessibilite-numerique/VousEtes.vue
@@ -20,10 +20,66 @@
         <div class="cover__subhead"><Accessibilite class="cover__subhead-icon" focusable="false" aria-hidden="true"/>Accessibilité</div>
 
         <h1>Vous avez un <span class="text-highlight">rôle à jouer</span></h1>
+         <p class="cover__subtitle">Quel que soit votre rôle dans l’organisation, vous pouvez mettre en place des actions pour prendre en compte l’accessibilité dans vos projets numériques.</p>
+
       </div>  
     </div>
 
     <div class="content">
+
+
+<div class="rf-grid-row rf-grid-row--gutters">
+    <div class="rf-col-4">
+        <div class="rf-tile rf-enlarge-link rf-tile--horizontal">
+            <div class="rf-tile__body">
+                <h4 class="rf-tile__title">
+                    <a class="rf-tile__link" href="manager/">Décideur et décideuse</a>
+                </h4>
+                <p class="rf-tile__desc">Vous décidez que l’<strong>accessibilité est essentielle</strong>.</p>
+            </div>
+        </div>
+    </div>
+    <div class="rf-col-4">
+        <div class="rf-tile rf-enlarge-link rf-tile--horizontal">
+            <div class="rf-tile__body">
+                <h4 class="rf-tile__title">
+                    <a class="rf-tile__link" href>Designer</a>
+                </h4>
+                <p class="rf-tile__desc">Vous concevez un parcours ou une interface en prenant en compte <strong>tous les usagers</strong>.</p>
+            </div>
+        </div>
+    </div>
+    <div class="rf-col-4">
+        <div class="rf-tile rf-enlarge-link rf-tile--horizontal">
+            <div class="rf-tile__body">
+                <h4 class="rf-tile__title">
+                    <a class="rf-tile__link" href>Développeur et développeuse</a>
+                </h4>
+                <p class="rf-tile__desc">Vous utilisez ou développez des composants d’interfaces <strong>accessibles et conformes</strong> au RGAA.</p>
+            </div>
+        </div>
+    </div>
+    <div class="rf-col-4">
+        <div class="rf-tile rf-enlarge-link rf-tile--horizontal">
+            <div class="rf-tile__body">
+                <h4 class="rf-tile__title">
+                    <a class="rf-tile__link" href>Chef et cheffe de produit, de projet</a>
+                </h4>
+                <p class="rf-tile__desc">Vous assurez la bonne prise en compte <strong>à chaque étape</strong> de l’accessibilité.</p>
+            </div>
+        </div>
+    </div>
+    <div class="rf-col-4">
+        <div class="rf-tile rf-enlarge-link rf-tile--horizontal">
+            <div class="rf-tile__body">
+                <h4 class="rf-tile__title">
+                    <a class="rf-tile__link" href>Référent et référente accessibilité numérique</a>
+                </h4>
+                <p class="rf-tile__desc">Vous mettez en place des les <strong>actions</strong> nécessaires au suivi du <strong>schéma pluriannuel</strong>.</p>
+            </div>
+        </div>
+    </div>
+</div>
 
       <p>Que vous soyez <g-link to="/accessibilite-numerique/vous-etes/manager/">décideur ou décideuse</g-link>, <g-link to="/designer/">designer</g-link>, <g-link to="/developper/">développeur ou développeuses</g-link>, <g-link to="/po/">responsable de projet ou de produit</g-link>, vous avez un rôle à jouer dans la mise au conformité RGAA de votre projet web.</p>
       <p>Ces fiches ont pour but de vous accompagner au mieux </p>
@@ -41,10 +97,10 @@ export default {
     Accessibilite,
   },
   metaInfo: {
-    title: "RGAA à emporter",
+    title: "Vous avez un rôle à jouer.",
     meta: [{
       name: 'description',
-      content: "Afficher la conformité RGAA, s’organiser et se faire aider pour améliorer l’accessibilité."
+      content: "Quel que soit votre rôle dans l’organisation, vous pouvez mettre en place des actions pour prendre en compte l’accessibilité dans vos projets numériques."
     },
     {
       property: 'og:title',
@@ -63,5 +119,5 @@ export default {
 </script>
 
 <style lang="scss">
-
+h4.rf-tile__title { margin:0 auto;}
 </style>

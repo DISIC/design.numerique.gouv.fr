7b3e3c5727e40e34b0231718ae4da048f582a255
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2021-07-02 17:26:36 +0200

    orthotypo

--- src/pages/accessibilite-numerique/vous-etes/manager.vue
+++ src/pages/accessibilite-numerique/vous-etes/manager.vue
@@ -22,7 +22,7 @@
       <div class="cover__container">
         <div class="cover__subhead"><Accessibilite class="cover__subhead-icon" focusable="false" aria-hidden="true"/>Accessibilité</div>
         <h1>Vous êtes <span class="text-highlight">décideur ou décideuse</span></h1>
-        <p class="cover__subtitle">Cette fiche est destiné à toute personne confrontée aux enjeux relatifs à l’accessibilité numérique, notamment l’obligation de rendre les sites et applications web conformes au RGAA, et amenée à mettre en place une politique d’accessibilité numérique dans son organisation.</p>
+        <p class="cover__subtitle">Cette fiche est destinée à toute personne confrontée aux enjeux relatifs à l’accessibilité numérique, notamment l’obligation de rendre les sites et applications web conformes au RGAA, et amenée à mettre en place une politique d’accessibilité numérique dans son organisation.</p>
 
       </div>  
     </div>
@@ -31,7 +31,7 @@
       <h3>Du handicap</h3>
       <p>15 à 20% de la population est en situation de handicap, vous êtes à peu près sûr que certains de « vos » utilisateurs peuvent rencontrer des difficultés.</p>
       <h3>De la législation</h3>
-      <p>La loi de 2005 pour l'égalité des droits et des chances, la loi pour une république numérique de 2016, le référentiel général d’amélioration de l’accessibilité (RGAA) : les dispositifs règlementaires sont applicables à tous.</p>
+      <p>La loi de 2005 pour l’égalité des droits et des chances, la loi pour une république numérique de 2016, le référentiel général d’amélioration de l’accessibilité (RGAA) : les dispositifs règlementaires sont applicables à tous.</p>
 
       <h2>Désigner un ou une référente accessibilité</h2>
       <p>La direction doit nommer un membre de l’encadrement de l’organisme en tant que « référent accessibilité ». Cette personne doit avoir la responsabilité et l’autorité en particulier pour :</p>
@@ -46,7 +46,7 @@
       <h3>Former</h3>
       <p><strong>Toutes</strong> les personnes doivent être sensibilisées.</p>
       <p>Les designers, développeurs et développeuses<strong></strong> doivent être formées.</p>
-      <p>Vous pouvez même former un expert RGAA pour internaliser les tâches d’audit et d’acompagnement.</p>
+      <p>Vous pouvez même former un expert RGAA pour internaliser les tâches d’audit et d’accompagnement.</p>
       <h3>Organiser</h3>
       <p>S’assurer que chaque étape du projet, chaque process, chaque intervention prennent en compte l’accessibilité.</p>
       <h3>Vérifier</h3>
@@ -55,7 +55,7 @@
       <h2>Se faire accompagner</h2>
       <p>La présence d’un expert en accessibilité numérique dans une équipe est précieuse : pour sensibiliser et former les équipes, optimiser les process, faire des revues de code, etc.</p>
 
-      <p>Si vous n’avez pas les ressources nécessaires en interne pour le faire, faites appel à l’<g-link to="/accessibilite-numerique/accord-cadre-dae">accord-cadre interministériel DAE</g-link> pour de l’<strong>accompagnement</strong> et de la <strong>formation</strong>. Tous les sujets y sont couverts : audits RGAA, formations de vos équipes, tests avec des utilisateurs en situation de handicap, constitution d’un schéma pluriannuel de mise en accessiblité, etc.</p>
+      <p>Si vous n’avez pas les ressources nécessaires en interne pour le faire, faites appel à l’<g-link to="/accessibilite-numerique/accord-cadre-dae">accord-cadre interministériel DAE</g-link> pour de l’<strong>accompagnement</strong> et de la <strong>formation</strong>. Tous les sujets y sont couverts : audits RGAA, formations de vos équipes, tests avec des utilisateurs en situation de handicap, constitution d’un schéma pluriannuel de mise en accessibilité, etc.</p>
     </div>
 
   </Layout>

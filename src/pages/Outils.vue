d7d3343b6a2dab13877c84a2a9323608863f6bba
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2021-07-20 18:28:37 +0200

    add anchors

--- src/pages/Outils.vue
+++ src/pages/Outils.vue
@@ -25,7 +25,7 @@
     <div class="content">
 
       <section>
-        <h2><Outils class="h2__icon" focusable="false" aria-hidden="true"/>Tech</h2>
+        <h2 id="tech"><Outils class="h2__icon" focusable="false" aria-hidden="true"/>Tech</h2>
         <ul>
           <li>La <a href="checklist-dev">checklist dev</a> regroupe les tests et vérifications rapides à faire soi-même avant de livrer son travail.</li>
           <li>Le <a href="memo-dev">mémo dev</a> regroupe sur une seule et même page les points d'attention pour <strong>les développeurs et les développeuses front</strong> ainsi que les liens vers les ressources adéquates.</li>
@@ -34,7 +34,7 @@
       </section>
 
       <section>
-        <h2><Outils class="h2__icon" focusable="false" aria-hidden="true"/>Méthode</h2>
+        <h2 id="methode"><Outils class="h2__icon" focusable="false" aria-hidden="true"/>Méthode</h2>
         <ul>
           <li>Le <a href="/accessibilite-numerique/jeu-de-oaa/">jeu de l'organisation de l’amélioration de l’accessibilité (OAA)</a> vous guide dans la mise en accessibilité de votre service numérique.</li>
           <li>Le <a href="diagnostic-flash">diagnostic flash</a> permet d’estimer de manière partielle la qualité, l’accessibilité et le design d’un site ou d’une démarche en ligne, sans avoir d’expertise particulière et en n’y consacrant que peu de temps.</li>
@@ -44,20 +44,20 @@
         </ul>
       </section>
       <section>
-        <h2><Outils class="h2__icon" focusable="false" aria-hidden="true"/>Obligations légales</h2>
+        <h2 id="declaration"><Outils class="h2__icon" focusable="false" aria-hidden="true"/>Obligations légales</h2>
         <ul>
           <li>Un <a href="/outils/exemple/">exemple de déclaration d’accessibilité</a> prêt à remplir.</li>
           <li>Le <a href="https://betagouv.github.io/a11y-generateur-declaration/" title="Générateur de déclaration d’accessibilité - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">générateur de déclaration d’accessibilité</a> produit par <a target="_blank" title="BetaGouv - nouvelle fenêtre" href="https://beta.gouv.fr">BetaGouv</a>.</li>
         </ul>
       </section>
       <section>
-        <h2><Outils class="h2__icon" focusable="false" aria-hidden="true"/>Tests usagers en situation de handicap</h2>
+        <h2 id="tests"><Outils class="h2__icon" focusable="false" aria-hidden="true"/>Tests usagers en situation de handicap</h2>
         <h3>Mode d’utilisation du groupe d’utilisateurs handicapés</h3>
         <p>La DINUM a créé un groupe d’utilisateurs handicapés pour tester l’accessibilité des sites et applications mobiles publics. Il inclut actuellement une trentaine d’utilisateurs, majoritairement handicapés visuels : non-voyants et mal voyants, une dizaine de personnes handicapées cognitives et quelques personnes handicapées mobiles. Mais il a vocation à accueillir un plus grand nombre d’internautes handicapés appartenant à toutes les catégories de personnes handicapées. Ces utilisateurs ne sont pas des experts en accessibilité, mais ont pour point commun d’avoir l’habitude de naviguer sur internet et de posséder à titre professionnel ou personnel les matériels et logiciels nécessaires à cette fin. Les tests ainsi effectués ne remplacent ni ceux réalisés à l’aide des logiciels de test, ni ceux réalisés par des professionnels experts en accessibilité. Ils  peuvent avoir, néanmoins, une grande utilité pour montrer le degré d’efficacité des publics concernés sur les sites et applications testés, d’une façon concrète, sans nécessairement se référer au taux de conformité à déterminer aux normes en vigueur.</p>
         <p>La demande de test doit être adressée par mail à <strong>jamshid.kohandel@modernisation.gouv.fr</strong>.</p> 
       </section>
       <section>
-        <h2><Outils class="h2__icon" focusable="false" aria-hidden="true"/>Le système de design de l'État</h2>
+        <h2 id="dsfr"><Outils class="h2__icon" focusable="false" aria-hidden="true"/>Le système de design de l'État</h2>
         <ul>
           <li>Le <a href="https://systeme-de-design.gouv.fr/" title="système de design de l‘État - nouvelle fenêtre" target="_blank">système de design de l‘État</a> est un ensemble de composants accessibles et réutilisables, guidé par des standards et une gouvernance, pouvant être assemblés pour construire plusieurs sites internet.</li>
         </ul>

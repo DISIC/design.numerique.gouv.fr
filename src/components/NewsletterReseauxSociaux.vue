7b7f85680ce4912f0681d56d9159883c3e3c2fdb
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2021-08-11 15:28:09 +0200

    Fix hx et margin

--- src/components/NewsletterReseauxSociaux.vue
+++ src/components/NewsletterReseauxSociaux.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="fr-follow">
+    <div class="fr-mt-8w fr-follow">
         <div class="fr-container">
             <div class="fr-grid-row">
                 <div class="fr-col-12 fr-col-md-8">
                     <div class="fr-follow__newsletter">
                         <div>
-                            <h5 class="fr-h5 fr-follow__title">Abonnez-vous à notre lettre d’information</h5>
+                            <h2 class="fr-h5 fr-follow__title">Abonnez-vous à notre lettre d’information</h2>
                             <p class="fr-text--sm fr-follow__desc">DesignGouv, c’est aussi des événements, des rencontres et des discussions pour faire vivre la culture design au sein des administrations. Rejoignez-nous !</p>
                         </div>
                         <div>
@@ -17,8 +17,7 @@
                 </div>
                 <div class="fr-col-12 fr-col-md-4">
                     <div class="fr-follow__social">
-                        <p class="fr-h5 fr-mb-3v fr-mb-3v">Suivez-nous
-                            <br> sur les réseaux sociaux</p>
+                        <h2 class="fr-h5 fr-mb-3v fr-mb-3v">Suivez-nous<br> sur les réseaux sociaux</h2>
                         <ul class="fr-links-group fr-links-group--lg">
                             <li>
                                 <a class="fr-link--twitter fr-link" title="twitter - ouvre une nouvelle fenêtre" href="https://twitter.com/design_gouv/" target="_blank">twitter</a>

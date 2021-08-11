4bdfbd0fd30d38bb6584718db103da4b5f9bccb2
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2021-08-11 12:46:26 +0200

    Composant newsletter et RS du DSFR

---
+++ src/components/NewsletterReseauxSociaux.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="fr-follow">
+        <div class="fr-container">
+            <div class="fr-grid-row">
+                <div class="fr-col-12 fr-col-md-8">
+                    <div class="fr-follow__newsletter">
+                        <div>
+                            <h5 class="fr-h5 fr-follow__title">Abonnez-vous à notre lettre d’information</h5>
+                            <p class="fr-text--sm fr-follow__desc">DesignGouv, c’est aussi des événements, des rencontres et des discussions pour faire vivre la culture design au sein des administrations. Rejoignez-nous !</p>
+                        </div>
+                        <div>
+                            <a class="fr-btn button" href="/newsletter">
+                                S’abonner
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="fr-col-12 fr-col-md-4">
+                    <div class="fr-follow__social">
+                        <p class="fr-h5 fr-mb-3v fr-mb-3v">Suivez-nous
+                            <br> sur les réseaux sociaux</p>
+                        <ul class="fr-links-group fr-links-group--lg">
+                            <li>
+                                <a class="fr-link--twitter fr-link" title="twitter - ouvre une nouvelle fenêtre" href="https://twitter.com/design_gouv/" target="_blank">twitter</a>
+                            </li>
+                            <li>
+                                <a class="fr-link--instagram fr-link" title="instagram - ouvre une nouvelle fenêtre" href="https://www.instagram.com/design_gouv/" target="_blank">instagram</a>
+                            </li>
+                            <li>
+                                <a class="fr-link--linkedin fr-link" title="linkedin - ouvre une nouvelle fenêtre" href="https://www.linkedin.com/company/designgouv" target="_blank">linkedin</a>
+                            </li>
+                            <li>
+                                <a class="fr-link--youtube fr-link" title="youtube - ouvre une nouvelle fenêtre" href="https://www.youtube.com/channel/UCMH9lC8dSlRVRfb0LoKuJZw" target="_blank">youtube</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'NewsletterResauxSociaux'
+  }
+</script>

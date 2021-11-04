5e0eb7b55572c4b171e59babb7878c2aad112de4
Ugo <jeanmichelpaul@icloud.com>
2021-11-04 10:46:11 +0100

    Add formation alert on homepage

---
+++ src/components/alerts/Formation.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div class="alert">
+
+      <h2>Nouveau programme de formations !</h2>
+      <p>
+        Nous lançons <strong>trois formations gratuites</strong> en ligne, sur les sujets indispensables à la conception de services publics numériques : <strong>accessibilité</strong>, <strong>design</strong> et <strong>recherche utilisateur</strong>.
+      </p>
+
+      <g-link to="/articles/2021-10-29-formations-design-accessibilite-recherche/">
+        Informations et inscriptions<font-awesome aria-hidden="true" class="button__icon" :icon="['fas', 'arrow-right']" transform="shrink-3" height="16" width="16"/>
+      </g-link>
+
+  </div>
+
+</template>
+
+
+<script>
+
+  export default {
+    name: 'Formation'
+  }
+
+</script>
+
+
+<style lang="scss">
+
+</style>

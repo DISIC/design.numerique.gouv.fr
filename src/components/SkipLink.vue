dfdc9908420816befdbd946f7686c7d4a4403d18
Yaacov <yaacov@goodimpact.studio>
2021-12-07 11:46:14 +0100

    Fix CSS class typo

--- src/components/SkipLink.vue
+++ src/components/SkipLink.vue
@@ -6,7 +6,7 @@
                 <a class="fr-link" href="#main"  @click.stop>Contenu</a>
             </li>
             <li>
-                <a class="rf-link" href="#footer"  @click.stop>Pied de page</a>
+                <a class="fr-link" href="#footer"  @click.stop>Pied de page</a>
             </li>
         </ul>
     </div>

b2d7ff31fa56b8d60e7ab2d15bcda7020c289c35
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2021-12-08 15:10:08 +0100

    articles use cards

--- src/pages/Articles.vue
+++ src/pages/Articles.vue
@@ -13,14 +13,22 @@
     </nav>
     <div class="content">
       <h1>Articles</h1>
-      <section class="articles">
-        <article v-for="{ node } in $page.allArticle.edges" :key="node.id">
-          <g-image :src="node.illustration" focusable="false" alt="" aria-hidden="true"/>
-          <p class="articles__date">{{ node.publishedDate }}</p>
-          <h2><g-link :to="node.path">{{ node.title }}</g-link></h2>
-          <p>{{ node.description }}</p>
-        </article>
-      </section>
+      <div class="fr-grid-row fr-grid-row--gutters">
+        <div class="fr-col-12 fr-col-sm-6" v-for="{ node } in $page.allArticle.edges" :key="node.id">
+          <div class="fr-card fr-enlarge-link" >
+            <div class="fr-card__body">
+                <h2 class="fr-card__title">
+                  <g-link :to="node.path" class="fr-card__link">{{ node.title }}</g-link>
+                </h2>
+                <p class="fr-card__desc">{{ node.description }}.</p>
+                <p class="fr-card__detail">{{ node.publishedDate }}</p>
+            </div>
+            <div class="fr-card__img">
+                <g-image :src="node.illustration" class="fr-responsive-img" alt=""/>
+            </div>
+          </div>
+        </div>
+      </div>  
     </div>
   </Layout>
 </template>

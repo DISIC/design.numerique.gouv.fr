273c0d91128763ef4108332630e929e7130edbf4
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2021-12-09 15:10:19 +0100

    Formation recherche utilisateur : checked !

--- src/pages/formations/RechercheUtilisateur.vue
+++ src/pages/formations/RechercheUtilisateur.vue
@@ -28,9 +28,9 @@
   <div class="content fr-container">
     <div class="fr-grid-row fr-grid-row--center">
         <div class="fr-col-12 fr-col-md-8">
-          <h2>Jeudi 9 décembre 10h à 12h</h2>
-          <p>En <a href="https://www.youtube.com/channel/UCMH9lC8dSlRVRfb0LoKuJZw/featured" target="_blank" title="visio sur notre chaîne YouTube - Nouvelle fenêtre"><strong>visio</strong> sur notre chaîne YouTube</a>.</p>
-
+          <h2>Revivez la formation en vidéo</h2>
+          <p>La formation a eu lieu en direct le 9 décembre. Vous pouvez la revivre <a href="https://www.youtube.com/watch?v=0V-WIpVtPCg&t=4s" target="_blank" title="sur notre chaîne YouTube - Nouvelle fenêtre">sur notre chaîne YouTube</a>.</p>
+ 
           <h3>Objectifs</h3>
           <ul>
             <li>Connaître l'importance et l'utilité de la recherche utilisateur dans la conception et développement de services numériques</li>
@@ -48,32 +48,10 @@
             <li>Quand et comment faire de la recherche utilisateur ?</li>
             <li>Les outils à votre disposition</li>
           </ul>
-          <p>C’est important, c’est gratuit, c’est ludique, c’est opérationnel et, franchement, c’est bien fait donc : venez !</p>
-          <p>N'oubliez pas, nous proposons trois autres formations : <g-link to="/formations/design/">design</g-link>, <g-link to="/formations/accessibilite/">accessibilité</g-link> et <g-link to="/formations/ecoconception/">écoconception</g-link> (formulaires distincts).</p>
 
+          <p>C’est important, c’est gratuit, c’est ludique, c’est opérationnel et c’est toujours disponible <a href="https://www.youtube.com/watch?v=0V-WIpVtPCg&t=4s" target="_blank" title="sur notre chaîne YouTube - Nouvelle fenêtre">sur notre chaine Youtube</a> !</p>
+          <p>N'oubliez pas, nous proposons trois autres formations : <g-link to="/formations/design/">design</g-link>, <g-link to="/formations/accessibilite/">accessibilité</g-link> et <g-link to="/formations/ecoconception/">écoconception</g-link>.</p>
 
-          <h3>Inscription</h3>
-          <p>Pour que nous puissions vous communiquer d'éventuelles précisions et que nous sachions qui se trouvera de l'autre côté de l'écran, vous pouvez nous envoyer ce formulaire.</p>
-          <form class="form" v-on:submit.prevent="addParticipant">
-              <div class="fr-input-group">
-                  <label class="fr-label" for="email">Votre adresse e-mail<span class="fr-hint-text">L’adresse e-mail doit être au format prenom@mail.fr</span></label>
-                  <input class="fr-input" type="email" id="email" v-model="form.email" required>
-              </div>
-              <div class="fr-input-group">
-                  <label class="fr-label" for="organisme">Votre organisme</label>
-                  <input class="fr-input" type="text" id="organisme" v-model="form.organisme" required>
-              </div>
-
-              <p class="rgpd">
-                Les données recueillies sur ce formulaire sont traitées par les équipes de la DINUM. Elles nous permettent de vous informer via e-mail des nouvelles liées à cet événement.
-              </p>
-
-              <p class="rgpd rgpd--small">
-                Conformément à la règlementation, vous disposez d’un droit d’opposition et d’un droit à la limitation du traitement de données vous concernant, ainsi que d’un droit d’accès, de rectification, de portabilité et d’effacement de vos données. Vous pouvez exercer vos droits en nous écrivant à contact@design.numerique.gouv.fr.
-              </p>
-
-              <button class="button button--strong" id="submit" type="submit">S'inscrire</button>
-          </form>
         </div>
       </div>
     </div>

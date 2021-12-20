1b320f0b9d40b1100660761a9d32d68bcdbd9fe3
Ugo <jeanmichelpaul@icloud.com>
2021-12-20 17:02:17 +0100

    Make all message component clickable

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -6,7 +6,7 @@
       <p class="fr-mb-2w">
         Nous lançons <strong>quatre formations gratuites</strong> en ligne, sur les sujets indispensables à la conception de services publics numériques : <strong>accessibilité</strong>, <strong>design</strong>, <strong>recherche utilisateur</strong> et <strong>écoconception</strong>.<br>
       </p>
-      <g-link class="fr-link fr-fi-arrow-right-line fr-link--icon-right" to="/articles/2021-10-29-formations-design-accessibilite-recherche-ecoconception/">
+      <g-link class="dg-message__link fr-link fr-fi-arrow-right-line fr-link--icon-right" to="/articles/2021-10-29-formations-design-accessibilite-recherche-ecoconception/">
         Infos et inscriptions à nos formations
       </g-link>
     </div>
@@ -25,8 +25,18 @@
 <style lang="scss" scoped>
 
   .dg-message {
+    position: relative;
     background-color: var(--background-default-grey);
     border: 1px solid var(--border-default-grey);
+
+    &__link:after {
+      position: absolute;
+      content: "";
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+    }
   }
 
 </style>

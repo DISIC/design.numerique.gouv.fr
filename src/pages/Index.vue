49ec45e0acdf7f0bf614a42f3354195b5af520b0
Ugo <jeanmichelpaul@icloud.com>
2021-12-20 13:25:42 +0100

    Tweak cover style on homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,7 +3,11 @@
     <div class="content fr-mt-md-4w fr-mt-2w">
 
       <h1 class="fr-display-sm dg-text--center">Concevons des services publics numériques <strong>accessibles</strong>, <strong>inclusifs</strong> et <strong>humains</strong>.</h1>
-      <p class="fr-text--lead dg-text--center">Les services publics numériques sont encore trop souvent conçus sans prendre en compte l’audience qu’ils servent. Nous mettons à la disposition des administrations des idées et outils pour améliorer l’expérience usager de leurs services.</p>
+      <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--center">
+          <div class="fr-col-lg-10">
+            <p class="fr-text--lead dg-text--center">Les services publics numériques sont encore trop souvent conçus sans prendre en compte l’audience qu’ils servent. Nous mettons à la disposition des administrations des idées et outils pour améliorer l’expérience usager de leurs services.</p>
+          </div>
+        </div>
       <Formation/>
 
       <section class="fr-mt-4w">

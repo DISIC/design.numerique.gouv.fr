9d103058e4bd596a5b2897a7a484cb98bfe76a50
Ugo <jeanmichelpaul@icloud.com>
2021-12-23 10:02:39 +0100

    Rename design page

---
+++ src/pages/DesignNumerique.vue
@@ -0,0 +1,38 @@
+<template>
+  <Layout>
+
+    <nav role="navigation" class="fr-breadcrumb" aria-label="vous êtes ici :">
+      <ol class="fr-breadcrumb__list">
+        <li>
+          <g-link to="/" class="fr-breadcrumb__link">Accueil</g-link>
+        </li>
+        <li>
+          <span aria-current="page">Design numérique</span>
+        </li>
+      </ol>
+    </nav>
+
+    <div>
+      <h1 class="text-highlight"><Design class="h1__icon" focusable="false" aria-hidden="true"/>Design numérique</h1>
+      <p class="cover__subtitle">Le design permet de rendre les services numériques plus
+        simples, accessibles et agréables pour tous. Concevoir par le design, c’est
+        mettre en œuvre des solutions centrées sur les usagers, tout en s’appuyant
+        sur de bonnes pratiques reconnues.</p>
+    </div>
+
+  </Layout>
+</template>
+
+
+<script>
+
+  export default {
+    metaInfo: {
+      title: "Design numérique",
+      meta: [
+        { name: "description", content: "Cet espace est dédié aux agents publics soucieux de créer des services numériques accessibles pour tous." }
+      ],
+    }
+  }
+
+</script>

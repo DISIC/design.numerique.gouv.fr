a0b0d54d5a3a4476e86a64fb6932ccf5a76bbc64
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2021-12-28 09:46:42 +0100

    les commandes hors marchés pour les nul·le·s

--- src/pages/accessibilite-numerique/prestationsAccessibiliteNumerique.vue
+++ src/pages/accessibilite-numerique/prestationsAccessibiliteNumerique.vue
@@ -19,13 +19,49 @@
 
        <div class="cover__subhead"><Accessibilite class="cover__subhead-icon" focusable="false" aria-hidden="true"/>Accessibilité</div>
        <h1>Prestations <span class="text-highlight">accessibilité numérique</span></h1>
-        <p>L’accord cadre accompagnement et formations de la <abbr title="Direction des achats de l’État">DAÉ</abbr> a pris fin le 20/11/21. Dans l’attente de son renouvellement en 2022, vous pouvez tout de même faire appel à des professionnels du secteur.</p>
+        <p>L’<a href="#accord-cadre">accord cadre accompagnement et formations</a> de la <abbr title="Direction des achats de l’État">DAÉ</abbr> a pris fin le 20/11/21. Dans l’attente de son renouvellement en 2022, vous pouvez tout de même faire appel à des professionnels du secteur.</p>
       </div>
     </div>
-
     <div class="content">
       <section>
-        <h2>Les dispositions règlementaires</h2>
+        <h2>Les commandes hors marché restent possibles</h2>
+        <p>En fonction du montant de la commande, le processus n’est pas le même.</p>
+        <h3>Montant inférieur à 40 000 € <abbr title="hors taxe">HT</abbr></h3>
+        <ul>
+          <li>Vous sélectionnez librement <strong>au moins deux</strong> prestataires ;</li>
+          <li>vous leur soumettez <strong>votre besoin</strong> par écrit ou par oral ;</li>
+          <li>vous <strong>négociez et choisissez</strong> celui qui vous convient le mieux.</li>
+        </ul>
+        <h3>Montant inférieur à 80 000 € <abbr title="hors taxe">HT</abbr></h3>
+        <ul>
+          <li>Vous <strong>publiez votre besoin</strong> dans la presse spécialisée ou locale ;</li>
+          <li>vous <strong>sélectionnez la meilleure réponse</strong> des offres reçues.</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Combien ça coûte ?</h2>
+        <h3>Quel est le coût d’un audit ?</h3>
+        <p>Le coût d’un audit de conformité au RGAA dépend du niveau de complexité du site (exemples : nombre de pages différentes, types de composants mis en œuvre, technologies, etc.).<br/>Il est compris entre 2 000 et 5 000 € HT.</p>
+        <h3>Quel est le coût d’une formation ?</h3>
+        <p>Il existe plusieurs types de formation à l’accessibilité numérique dont le contenu et la durée diffèrent selon le public cible et les objectifs :</p>
+        <ul>  
+          <li>Une sensibilisation à l’accessibilité dure entre deux heures et une journée ;</li>
+          <li>Une formation « gestion de projet web accessible » dure entre un et trois jours ;</li>
+          <li>Une formation « développer un site web accessible » dure au minimum trois jours ;</li>
+          <li>Une formation « auditeur ou auditrice RGAA » dure environ cinq jours.</li>
+        </ul>
+      </section>
+      <div class="fr-grid-row fr-grid-row--center">
+        <div class="fr-callout fr-col-lg-10 fr-mt-2w fr-mb-8w">
+          <h3 class="fr-callout__title">Le <abbr title="Fonds pour l’Insertion des Personnes Handicapées dans la Fonction Publique">FIPHFP</abbr> peut vous aider</h3>
+          <p class="fr-callout__text">Le FIPHFP peut apporter une aide financière sur les prestations d’audit, d’accompagnement et de <strong>formation des agents de la fonction publique</strong>.<br>
+          Retrouvez les informations du <a href="http://www.fiphfp.fr/Espace-employeur/Interventions-du-FIPHFP" target="_blank" title="financement accessibilité numérique - nouvelle fenêtre">financement accessibilité numérique</a>.</p>
+        </div>
+      </div>
+
+      <section>
+        <h2 id="dispositions-reglementaires">Le détail des dispositions règlementaires</h2>
         <p>Les services souhaitant acheter une prestation d’audit, d’accompagnement en accessibilité numérique peuvent s’appuyer sur plusieurs dispositions règlementaires de la commande publique. (<a href="https://www.service-public.fr/professionnels-entreprises/vosdroits/F32049" target="_blank" title="les procédures de marchés publics - nouvelle fenêtre">Les procédures de marchés publics</a>).</p>
         <h3>Montant inférieur à 40 000 € HT (soit 39 999,99 € HT)</h3>
         <p>L’acheteur peut passer un marché sans publicité ni mise en concurrence préalables pour répondre à un besoin dont la valeur estimée est inférieure à 40 000 euros hors taxes ou pour les lots dont le montant est inférieur à 40 000 € HT et qui remplissent la condition prévue au b du 2° de l’article R. 2123-1.</p>
@@ -47,31 +83,13 @@
         <p>Au sein des marchés à procédure adaptée (Mapa), il existe un seuil intermédiaire de 90 000 € HT qui n’influe que sur la publicité et pas sur la procédure.</p>
         <p>En dessous de ce seuil, l’acheteur choisit librement les critères de publicité (exemple : profil d’acheteur, BOAMP, presse spécialisée, presse régionale, etc.).</p>
       </section>
-      <section>
-        <h3>Quel est le coût d’un audit ?</h3>
-        <p>Le coût d’un audit de conformité au RGAA dépend du niveau de complexité du site (exemples : nombre de pages différentes, types de composants mis en œuvre, technologies, etc.).<br/>Il est compris entre 2 000 et 5 000 € HT.</p>
-        <h3>Quel est le coût d’une formation ?</h3>
-        <p>Il existe plusieurs types de formation à l’accessibilité numérique dont le contenu et la durée diffèrent selon le public cible et les objectifs :</p>
-        <ul>  
-          <li>Une sensibilisation à l’accessibilité dure entre deux heures et une demi-journée ;</li>
-          <li>Une formation « gestion de projet web accessible » dure entre un et trois jours ;</li>
-          <li>Une formation « développer un site web accessible » dure au minimum trois jours ;</li>
-          <li>Une formation « auditeur ou auditrice RGAA » dure environ cinq jours.</li>
-        </ul>
-      </section>
-      <div class="fr-grid-row fr-grid-row--center">
-        <div class="fr-callout fr-col-lg-10 fr-mt-2w fr-mb-8w">
-          <h3 class="fr-callout__title">Le <abbr title="Fonds pour l’Insertion des Personnes Handicapées dans la Fonction Publique">FIPHFP</abbr> peut vous aider</h3>
-          <p class="fr-callout__text">Le FIPHFP peut apporter une aide financière sur les prestations d’audit, d’accompagnement et de <strong>formation des agents de la fonction publique</strong>.<br>
-          Retrouvez les informations du <a href="http://www.fiphfp.fr/Espace-employeur/Interventions-du-FIPHFP" target="_blank" title="financement accessibilité numérique - nouvelle fenêtre">financement accessibilité numérique</a>.</p>
-        </div>
-      </div>
 
       <section>
+        <h2 id="accord-cadre">Les prestations de l’accord cadre</h2>
         <p>Pour rappel, le précédent accord-cadre contenait deux lots : « accompagnement et audit » et « formations ».</p>
-        <h2 id="lot-1">Lot 1 : accompagnement et audit</h2>
+        <h3 id="lot-1">Lot 1 : accompagnement et audit</h3>
         <p>Le lot 1 concerne les prestations d’accompagnement et d’audit des services de communication au public en ligne de l’État et des établissements publics dans le cadre de la mise en conformité avec les exigences en matière d’accessibilité (RGAA ou ses documents annexes).</p>
-        <h3>Les prestations d’accompagnement</h3>
+        <h4>Les prestations d’accompagnement</h4>
         <ul>
           <li><strong>Schéma pluriannuel</strong> : Accompagnement pour la mise en œuvre et le suivi du schéma pluriannuel de mise en accessibilité des services de communication au public en ligne</li>
           <li><strong>Écoute usagers internes</strong> : Accompagnement pour la mise en place ou l’amélioration de l’écoute des utilisateurs internes en situation de handicap</li>
@@ -80,16 +98,15 @@
           <li><strong>Cadrage de la recette</strong> : Accompagnement pour contrôler la prise en compte de l’accessibilité lors de la phase de recette</li>
           <li><strong>Opérations de recette</strong> : Accompagnement dans les opérations de recette</li>
         </ul>
-        <h3>Les prestations d’audit</h3>
+        <h4>Les prestations d’audit</h4>
         <ul>
           <li><strong>Diagnostic rapide</strong> d’un ou plusieurs services de communication au public en ligne</li>
           <li><strong>Audit initial</strong> d’un service de communication au public en ligne</li>
           <li><strong>Audit de contrôle</strong> d’un service de communication au public en ligne</li>
           <li><strong>Audit accompagné</strong> d’un service de communication au public en ligne</li>
         </ul>
-        <h2 id="lot-2">Lot 2 : formations</h2>
+        <h3 id="lot-2">Lot 2 : formations</h3>
         <p>Le lot 2 concerne les prestations de formation relatives au RGAA et l’accessibilité numérique à destination de l’État et des établissements publics.</p>
-        <h3>Les prestations de formation</h3>
         <ul>
           <li>Sensibilisation</li>
           <li>Formation conception et développement</li>

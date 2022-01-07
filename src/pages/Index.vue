ecb064614dfb87bdc352f86ed558e757df289fdd
Ugo <jeanmichelpaul@icloud.com>
2022-01-07 17:28:34 +0100

    Remove content div on homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,186 +1,186 @@
 <template>
   <Layout>
-    <div class="content">
 
-      <section class="dg-cover">
-        <div class="dg-cover-inner fr-grid-row fr-grid-row--gutters fr-grid-row--center">
-          <div class="fr-col-xl-10 fr-mt-6w dg-cover-container">
-            <h1 class="fr-display-sm dg-text-center fr-px-6w">Concevons des services publics numériques <strong>accessibles</strong>, <strong>inclusifs</strong> et <strong>humains</strong>.</h1>
-            <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--center fr-px-4w">
-              <div class="fr-col-lg-10">
-                <p class="fr-text--lead dg-text-center fr-mb-5w">Les services publics numériques sont encore trop souvent conçus sans prendre en compte l’audience qu’ils servent. Nous mettons à la disposition des administrations des idées et outils pour améliorer l’expérience usager de leurs services.</p>
-              </div>
+    <section class="dg-cover">
+      <div class="dg-cover-inner fr-grid-row fr-grid-row--gutters fr-grid-row--center">
+        <div class="fr-col-xl-10 fr-mt-6w dg-cover-container">
+          <h1 class="fr-display-sm dg-text-center fr-px-6w">Concevons des services publics numériques <strong>accessibles</strong>, <strong>inclusifs</strong> et <strong>humains</strong>.</h1>
+          <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--center fr-px-4w">
+            <div class="fr-col-lg-10">
+              <p class="fr-text--lead dg-text-center fr-mb-5w">Les services publics numériques sont encore trop souvent conçus sans prendre en compte l’audience qu’ils servent. Nous mettons à la disposition des administrations des idées et outils pour améliorer l’expérience usager de leurs services.</p>
             </div>
-            <Message/>
           </div>
+          <Message/>
         </div>
-      </section>
+      </div>
+    </section>
 
-      <section class="dg-block fr-grid-row fr-grid-row--center fr-mt-8w">
-        <div class="fr-col-lg-10 fr-my-8w">
-          <div class="fr-grid-row fr-grid-row--gutters">
-            <div class="fr-col-sm-6">
-              <div class="fr-tile fr-enlarge-link">
-                <div class="fr-tile__body">
-                  <h2 class="fr-tile__title">
-                    <g-link class="fr-tile__link fr-text--lg" to="/design-numerique/">Le design</g-link>
-                  </h2>
-                  <p class="fr-tile__desc">Rendre les services numériques plus <strong>simples</strong>, <strong>accessibles</strong> et <strong>agréables</strong> pour les usagers.</p>
-                </div>
-                <div class="fr-tile__img dg-picto">
-                  <img svg-inline src="../assets/images/design-picto.svg" class="fr-responsive-img" aria-hidden="true">
-                </div>
+    <section class="dg-block fr-grid-row fr-grid-row--center fr-mt-8w">
+      <div class="fr-col-lg-10 fr-my-8w">
+        <div class="fr-grid-row fr-grid-row--gutters">
+          <div class="fr-col-sm-6">
+            <div class="fr-tile fr-enlarge-link">
+              <div class="fr-tile__body">
+                <h2 class="fr-tile__title">
+                  <g-link class="fr-tile__link fr-text--lg" to="/design-numerique/">Le design</g-link>
+                </h2>
+                <p class="fr-tile__desc">Rendre les services numériques plus <strong>simples</strong>, <strong>accessibles</strong> et <strong>agréables</strong> pour les usagers.</p>
               </div>
-            </div>
-            <div class="fr-col-sm-6">
-              <div class="fr-tile fr-enlarge-link">
-                <div class="fr-tile__body">
-                  <h2 class="fr-tile__title">
-                    <g-link class="fr-tile__link fr-text--lg" to="/accessibilite-numerique/">L'accessibilité</g-link>
-                  </h2>
-                  <p class="fr-tile__desc">Rendre les services en ligne accessibles aux personnes en <strong>situation de handicap</strong>. </p>
-                </div>
-                <div class="fr-tile__img dg-picto">
-                  <img svg-inline src="../assets/images/accessibilite-picto.svg" class="fr-responsive-img" aria-hidden="true">
-                </div>
+              <div class="fr-tile__img dg-picto">
+                <img svg-inline src="../assets/images/design-picto.svg" class="fr-responsive-img" aria-hidden="true">
               </div>
             </div>
           </div>
-        </div>
-      </section>
-
-      <section class="fr-grid-row fr-grid-row--center fr-mt-8w">
-        <div class="dg-section-with-link fr-col-lg-10">
-          <h2 class="dg-section-with-link_title">Nos offres</h2>
-          <g-link class="dg-section-with-link__link fr-link fr-link--lg fr-fi-arrow-right-line fr-link--icon-right" to="/accompagnement/">Voir toutes nos offres</g-link>
-          <div class="dg-section-with-link__blocks fr-grid-row fr-grid-row--gutters">
-            <div class="fr-col-sm-6">
-              <div class="fr-tile fr-enlarge-link fr-tile--horizontal">
-                <div class="fr-tile__body">
-                  <h2 class="fr-tile__title">
-                    <g-link class="fr-tile__link fr-text--lg" to="/accompagnement/">Les experts</g-link>
-                  </h2>
-                  <p class="fr-tile__desc">Nos experts pour vous accompagner au quotidien 🌟</p>
-                </div>
-                <div class="fr-tile__img">
-                  <img svg-inline src="../assets/images/experts.svg" class="fr-responsive-img" aria-hidden="true">
-                </div>
+          <div class="fr-col-sm-6">
+            <div class="fr-tile fr-enlarge-link">
+              <div class="fr-tile__body">
+                <h2 class="fr-tile__title">
+                  <g-link class="fr-tile__link fr-text--lg" to="/accessibilite-numerique/">L'accessibilité</g-link>
+                </h2>
+                <p class="fr-tile__desc">Rendre les services en ligne accessibles aux personnes en <strong>situation de handicap</strong>. </p>
               </div>
-            </div>
-            <div class="fr-col-sm-6">
-              <div class="fr-tile fr-enlarge-link fr-tile--horizontal">
-                <div class="fr-tile__body">
-                  <h2 class="fr-tile__title">
-                    <g-link class="fr-tile__link fr-text--lg" to="/commando-ux/">Le Commando UX</g-link>
-                  </h2>
-                  <p class="fr-tile__desc">Des designers déployés au sein de votre équipe 👍 </p>
-                </div>
-                <div class="fr-tile__img">
-                  <img svg-inline src="../assets/images/commando.svg" class="fr-responsive-img" aria-hidden="true">
-                </div>
-              </div>
-            </div>
-            <div class="fr-col-sm-6">
-              <div class="fr-tile fr-enlarge-link fr-tile--horizontal">
-                <div class="fr-tile__body">
-                  <h2 class="fr-tile__title">
-                    <a class="fr-tile__link fr-text--lg" href="https://observatoire.numerique.gouv.fr/Aide/Donner%20son%20avis" title="Je donne mon avis - Nouvel onglet" target="_blank">Je donne mon avis</a>
-                  </h2>
-                  <p class="fr-tile__desc">Le bouton pour mesurer la satisfaction de vos usagers 💬  </p>
-                </div>
-                <div class="fr-tile__img">
-                  <img svg-inline src="../assets/images/jdma.svg" class="fr-responsive-img" aria-hidden="true">
-                </div>
-              </div>
-            </div>
-            <div class="fr-col-sm-6">
-              <div class="fr-tile fr-enlarge-link fr-tile--horizontal">
-                <div class="fr-tile__body">
-                  <h2 class="fr-tile__title">
-                    <a class="fr-tile__link fr-text--lg" href="https://observatoire.numerique.gouv.fr/" title="L'observatoire - Nouvel onglet" target="_blank">L'Observatoire</a>
-                  </h2>
-                  <p class="fr-tile__desc">Les 250 démarches les plus utilisées passées à la loupe 🔎 </p>
-                </div>
-                <div class="fr-tile__img">
-                  <img svg-inline src="../assets/images/observatoire.svg" class="fr-responsive-img" aria-hidden="true">
-                </div>
+              <div class="fr-tile__img dg-picto">
+                <img svg-inline src="../assets/images/accessibilite-picto.svg" class="fr-responsive-img" aria-hidden="true">
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
 
-      <section class="dg-block fr-grid-row fr-grid-row--center fr-mt-8w">
-        <div class="fr-col-lg-10 fr-my-8w">
-          <div class="fr-grid-row fr-grid-row--gutters">
-            <div class="fr-col-sm-6">
-              <div class="fr-tile fr-enlarge-link">
-                <div class="fr-tile__body">
-                  <h2 class="fr-tile__title">
-                    <g-link class="fr-tile__link fr-text--lg" to="/outils/">Outils</g-link>
+    <section class="fr-grid-row fr-grid-row--center fr-mt-8w">
+      <div class="dg-section-with-link fr-col-lg-10">
+        <h2 class="dg-section-with-link_title">Nos offres</h2>
+        <g-link class="dg-section-with-link__link fr-link fr-link--lg fr-fi-arrow-right-line fr-link--icon-right" to="/accompagnement/">Voir toutes nos offres</g-link>
+        <div class="dg-section-with-link__blocks fr-grid-row fr-grid-row--gutters">
+          <div class="fr-col-sm-6">
+            <div class="fr-tile fr-enlarge-link fr-tile--horizontal">
+              <div class="fr-tile__body">
+                <h2 class="fr-tile__title">
+                  <g-link class="fr-tile__link fr-text--lg" to="/accompagnement/">Les experts</g-link>
+                </h2>
+                <p class="fr-tile__desc">Nos experts pour vous accompagner au quotidien 🌟</p>
+              </div>
+              <div class="fr-tile__img">
+                <img svg-inline src="../assets/images/experts.svg" class="fr-responsive-img" aria-hidden="true">
+              </div>
+            </div>
+          </div>
+          <div class="fr-col-sm-6">
+            <div class="fr-tile fr-enlarge-link fr-tile--horizontal">
+              <div class="fr-tile__body">
+                <h2 class="fr-tile__title">
+                  <g-link class="fr-tile__link fr-text--lg" to="/commando-ux/">Le Commando UX</g-link>
+                </h2>
+                <p class="fr-tile__desc">Des designers déployés au sein de votre équipe 👍 </p>
+              </div>
+              <div class="fr-tile__img">
+                <img svg-inline src="../assets/images/commando.svg" class="fr-responsive-img" aria-hidden="true">
+              </div>
+            </div>
+          </div>
+          <div class="fr-col-sm-6">
+            <div class="fr-tile fr-enlarge-link fr-tile--horizontal">
+              <div class="fr-tile__body">
+                <h2 class="fr-tile__title">
+                  <a class="fr-tile__link fr-text--lg" href="https://observatoire.numerique.gouv.fr/Aide/Donner%20son%20avis" title="Je donne mon avis - Nouvel onglet" target="_blank">Je donne mon avis</a>
+                </h2>
+                <p class="fr-tile__desc">Le bouton pour mesurer la satisfaction de vos usagers 💬  </p>
+              </div>
+              <div class="fr-tile__img">
+                <img svg-inline src="../assets/images/jdma.svg" class="fr-responsive-img" aria-hidden="true">
+              </div>
+            </div>
+          </div>
+          <div class="fr-col-sm-6">
+            <div class="fr-tile fr-enlarge-link fr-tile--horizontal">
+              <div class="fr-tile__body">
+                <h2 class="fr-tile__title">
+                  <a class="fr-tile__link fr-text--lg" href="https://observatoire.numerique.gouv.fr/" title="L'observatoire - Nouvel onglet" target="_blank">L'Observatoire</a>
+                </h2>
+                <p class="fr-tile__desc">Les 250 démarches les plus utilisées passées à la loupe 🔎 </p>
+              </div>
+              <div class="fr-tile__img">
+                <img svg-inline src="../assets/images/observatoire.svg" class="fr-responsive-img" aria-hidden="true">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="dg-block fr-grid-row fr-grid-row--center fr-mt-8w">
+      <div class="fr-col-lg-10 fr-my-8w">
+        <div class="fr-grid-row fr-grid-row--gutters">
+          <div class="fr-col-sm-6">
+            <div class="fr-tile fr-enlarge-link">
+              <div class="fr-tile__body">
+                <h2 class="fr-tile__title">
+                  <g-link class="fr-tile__link fr-text--lg" to="/outils/">Outils</g-link>
+                </h2>
+                <p class="fr-tile__desc">Nous mettons à votre disposition un ensemble d’outils pour vous guider sur les sujets du <strong>design</strong> et de l’<strong>accessibilité</strong>.</p>
+              </div>
+              <div class="fr-tile__img dg-picto">
+                <img svg-inline src="../assets/images/outils-picto.svg" class="fr-responsive-img" aria-hidden="true">
+              </div>
+            </div>
+          </div>
+          <div class="fr-col-sm-6">
+            <div class="fr-tile fr-enlarge-link">
+              <div class="fr-tile__body">
+                <h2 class="fr-tile__title">
+                  <g-link class="fr-tile__link fr-text--lg" to="/formations/">Formations</g-link>
+                </h2>
+                <p class="fr-tile__desc">De nombreuses formations sont disponibles, que ce soit pour vous former <strong>personnellement</strong> ou en <strong>équipe</strong>. </p>
+              </div>
+              <div class="fr-tile__img dg-picto">
+                <img svg-inline src="../assets/images/formation-picto.svg" class="fr-responsive-img" aria-hidden="true">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="fr-grid-row fr-grid-row--center fr-mt-8w">
+      <div class="dg-section-with-link fr-col-lg-10">
+        <h2 class="dg-section-with-link_title">Nos articles</h2>
+        <g-link class="dg-section-with-link__link fr-link fr-link--lg fr-fi-arrow-right-line fr-link--icon-right" to="/articles/">Voir tous nos articles</g-link>
+        <div class="dg-section-with-link__blocks fr-grid-row fr-grid-row--gutters">
+          <div class="fr-col-12 fr-col-sm-6" v-for="{ node } in $page.allArticle.edges" :key="node.id">
+            <div class="fr-card fr-enlarge-link" >
+              <div class="fr-card__body">
+                  <h2 class="fr-card__title">
+                    <g-link :to="node.path" class="fr-card__link">{{ node.title }}</g-link>
                   </h2>
-                  <p class="fr-tile__desc">Nous mettons à votre disposition un ensemble d’outils pour vous guider sur les sujets du <strong>design</strong> et de l’<strong>accessibilité</strong>.</p>
-                </div>
-                <div class="fr-tile__img dg-picto">
-                  <img svg-inline src="../assets/images/outils-picto.svg" class="fr-responsive-img" aria-hidden="true">
-                </div>
+                  <p class="fr-card__desc">{{ node.description }}.</p>
+                  <p class="fr-card__detail">{{ node.publishedDate }}</p>
               </div>
-            </div>
-            <div class="fr-col-sm-6">
-              <div class="fr-tile fr-enlarge-link">
-                <div class="fr-tile__body">
-                  <h2 class="fr-tile__title">
-                    <g-link class="fr-tile__link fr-text--lg" to="/formations/">Formations</g-link>
-                  </h2>
-                  <p class="fr-tile__desc">De nombreuses formations sont disponibles, que ce soit pour vous former <strong>personnellement</strong> ou en <strong>équipe</strong>. </p>
-                </div>
-                <div class="fr-tile__img dg-picto">
-                  <img svg-inline src="../assets/images/formation-picto.svg" class="fr-responsive-img" aria-hidden="true">
-                </div>
+              <div class="fr-card__img">
+                  <g-image :src="node.illustration" class="fr-responsive-img" alt=""/>
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
 
-      <section class="fr-grid-row fr-grid-row--center fr-mt-8w">
-        <div class="dg-section-with-link fr-col-lg-10">
-          <h2 class="dg-section-with-link_title">Nos articles</h2>
-          <g-link class="dg-section-with-link__link fr-link fr-link--lg fr-fi-arrow-right-line fr-link--icon-right" to="/articles/">Voir tous nos articles</g-link>
-          <div class="dg-section-with-link__blocks fr-grid-row fr-grid-row--gutters">
-            <div class="fr-col-12 fr-col-sm-6" v-for="{ node } in $page.allArticle.edges" :key="node.id">
-              <div class="fr-card fr-enlarge-link" >
-                <div class="fr-card__body">
-                    <h2 class="fr-card__title">
-                      <g-link :to="node.path" class="fr-card__link">{{ node.title }}</g-link>
-                    </h2>
-                    <p class="fr-card__desc">{{ node.description }}.</p>
-                    <p class="fr-card__detail">{{ node.publishedDate }}</p>
-                </div>
-                <div class="fr-card__img">
-                    <g-image :src="node.illustration" class="fr-responsive-img" alt=""/>
-                </div>
-              </div>
-            </div>
+    <section class="fr-grid-row fr-grid-row--center fr-mt-8w">
+      <div class="fr-col-lg-10">
+        <h2>Notre équipe</h2>
+        <div class="dg-team">
+          <g-image class="dg-team__image" src="~/assets/images/team-photos/equipe-designgouv.jpg" alt="L’équipe DesignGouv sourire aux lèvres"/>
+          <div class="dg-team__description">
+            <p>DesignGouv c’est avant tout une équipe, <strong>le pôle Design des services numériques</strong>, diverse et heureuse de contribuer aux côtés des administrations à l’amélioration du quotidien des Français et des Françaises ☀️.</p>
+            <g-link to="/equipe/" class="fr-link fr-fi-arrow-right-line fr-link--icon-right">Découvrir l’équipe et notre mission</g-link>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
 
-      <section class="fr-grid-row fr-grid-row--center fr-mt-8w">
-        <div class="fr-col-lg-10">
-          <h2>Notre équipe</h2>
-          <div class="dg-team">
-            <g-image class="dg-team__image" src="~/assets/images/team-photos/equipe-designgouv.jpg" alt="L’équipe DesignGouv sourire aux lèvres"/>
-            <div class="dg-team__description">
-              <p>DesignGouv c’est avant tout une équipe, <strong>le pôle Design des services numériques</strong>, diverse et heureuse de contribuer aux côtés des administrations à l’amélioration du quotidien des Français et des Françaises ☀️.</p>
-              <g-link to="/equipe/" class="fr-link fr-fi-arrow-right-line fr-link--icon-right">Découvrir l’équipe et notre mission</g-link>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
   </Layout>
 </template>
+
 
 <script>
   import Message from "~/components/Message.vue"
@@ -238,7 +238,6 @@
 
 
 <page-query>
-
   query {
     allArticle (limit: 2, sortBy: "publishedDate", order: DESC) {
       edges {
@@ -253,8 +252,8 @@
       }
     }
   }
-
 </page-query>
+
 
 <style lang="scss" scoped>
 
@@ -284,5 +283,4 @@
       }
     }
   }
-
 </style>

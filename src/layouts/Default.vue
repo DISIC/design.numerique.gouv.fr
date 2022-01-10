24eac89dc4416cf384caa341ab488700465276c5
Ugo <jeanmichelpaul@icloud.com>
2022-01-10 17:02:24 +0100

    Remove fr-container margins

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -12,7 +12,7 @@
     <Header />
 
     <main class="layout" id="main" role="main">
-      <div class="fr-container fr-mt-1w">
+      <div class="fr-container fr-container--fluid fr-mt-1w">
         <div class="fr-grid-row fr-grid-row--center">
           <div class="fr-col-xl-10">
             <slot/>

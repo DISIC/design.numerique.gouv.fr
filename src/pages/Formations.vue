8e1ecb59ed8b41df5b25d4febda6fd4ca2215de9
Ugo <jeanmichelpaul@icloud.com>
2022-01-20 11:24:50 +0100

    Change formation rédaction ux url

--- src/pages/Formations.vue
+++ src/pages/Formations.vue
@@ -51,7 +51,7 @@
         </ul>
         <h3>Simplification de l'information</h3>
         <ul>
-          <li><g-link to="/formations/redaction-ux/"><font-awesome :icon="['fas', 'arrow-right']" transform="shrink-2" height="16px" aria-hidden="true"/>Formation initiale à la simplification de l'information</g-link>, 2 heures, gratuit, niveau débutant.</li>
+          <li><g-link to="/formations/simplification-information/"><font-awesome :icon="['fas', 'arrow-right']" transform="shrink-2" height="16px" aria-hidden="true"/>Formation initiale à la simplification de l'information</g-link>, 2 heures, gratuit, niveau débutant.</li>
         </ul>
       </section>
 

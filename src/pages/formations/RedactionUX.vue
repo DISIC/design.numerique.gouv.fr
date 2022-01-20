eba0d8637185319937748a973b3a3df32d917166
Ugo <jeanmichelpaul@icloud.com>
2022-01-20 11:22:50 +0100

    Tweak texts

--- src/pages/formations/RedactionUX.vue
+++ src/pages/formations/RedactionUX.vue
@@ -10,7 +10,7 @@
         <g-link to="/formations/" class="fr-breadcrumb__link">Formations</g-link>
       </li>
       <li>
-        <span aria-current="page">Simplifier l’information à destination des usagers</span>
+        <span aria-current="page">Formation à la simplification de l'information</span>
       </li>
     </ol>
   </nav>
@@ -18,7 +18,7 @@
   <div class="cover">
       <div class="cover__container">
         <!-- <div class="cover__subhead"><Formations class="cover__subhead-icon" focusable="false" aria-hidden="true"/>Formations</div> -->
-        <h1>Simplifier l’information à destination des usagers</h1>
+        <h1>Formation à la simplification de l'information</h1>
         <p class="cover__subtitle">
           Pourquoi et comment employer un langage simple et clair pour nos services publics, en explorant les subtilités du fonctionnement de notre cerveau.
         </p>
@@ -90,14 +90,14 @@
       Formations,
     },
     metaInfo: {
-      title: "Formation - Simplifier l’information à destination des usagers",
+      title: "Formation à la simplification de l'information",
       meta: [{
         name: "description",
         content: "Pourquoi et comment employer un langage simple et clair pour nos services publics, en explorant les subtilités du fonctionnement de notre cerveau."
       },
       {
         property: "og:title",
-        content: "Formation - Simplifier l’information à destination des usagers - DesignGouv"
+        content: "Formation à la simplification de l'information - DesignGouv"
       },
       {
         property: "og:description",
@@ -117,7 +117,7 @@
       },
       {
         name: "twitter:title",
-        content: "Formation - Simplifier l’information à destination des usagers - DesignGouv"
+        content: "Formation à la simplification de l'information - DesignGouv"
       },
       {
         name: "twitter:description",

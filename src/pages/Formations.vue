4e74d0a3729046322c7b266f4385e3beb14c5567
Ugo <jeanmichelpaul@icloud.com>
2022-01-20 13:00:49 +0100

    Tweak texts

--- src/pages/Formations.vue
+++ src/pages/Formations.vue
@@ -31,6 +31,11 @@
         </h2>
         <p>Pour se poser les bonnes questions aux bons moments, pour prendre les bonnes décisions avec les bonnes méthodes et les bons outils, pour travailler avec les bons interlocuteurs, nous vous proposons quatre formations gratuites indispensables&nbsp;:</p>
 
+        <h3>Simplification de l'information</h3>
+        <ul>
+          <li><g-link to="/formations/simplification-information/"><font-awesome :icon="['fas', 'arrow-right']" transform="shrink-2" height="16px" aria-hidden="true"/>Formation initiale à la simplification de l'information</g-link>, 2 heures, gratuit, niveau débutant. <strong>En direct le jeudi 17 février !</strong></li>
+        </ul>
+
         <h3>Accessibilité</h3>
         <ul>
           <li><g-link to="/formations/accessibilite/"><font-awesome :icon="['fas', 'arrow-right']" transform="shrink-2" height="16px" aria-hidden="true" />Formation initiale à l’accessibilité numérique</g-link>, 2 heures, gratuit, niveau débutant.</li>
@@ -45,13 +50,10 @@
         <ul>
           <li><g-link to="/formations/recherche-utilisateur/"><font-awesome :icon="['fas', 'arrow-right']" transform="shrink-2" height="16px" aria-hidden="true"/>Formation initiale à la recherche utilisateur</g-link>, 2 heures, gratuit, niveau débutant.</li>
         </ul>
+
         <h3>Écoconception</h3>
         <ul>
           <li><g-link to="/formations/ecoconception/"><font-awesome :icon="['fas', 'arrow-right']" transform="shrink-2" height="16px" aria-hidden="true"/>Formation initiale à l’écoconception</g-link>, 2 heures, gratuit, niveau débutant.</li>
-        </ul>
-        <h3>Simplification de l'information</h3>
-        <ul>
-          <li><g-link to="/formations/simplification-information/"><font-awesome :icon="['fas', 'arrow-right']" transform="shrink-2" height="16px" aria-hidden="true"/>Formation initiale à la simplification de l'information</g-link>, 2 heures, gratuit, niveau débutant.</li>
         </ul>
       </section>
 

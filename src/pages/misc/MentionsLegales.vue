6be020ff9677bb53aab91c15f0ab9f02b9957448
Ugo <jeanmichelpaul@icloud.com>
2022-01-21 16:14:46 +0100

    Fix width for misc pages

--- src/pages/misc/MentionsLegales.vue
+++ src/pages/misc/MentionsLegales.vue
@@ -1,100 +1,96 @@
 <template>
   <Layout>
+    <div class="dg-content fr-px-2w">
 
-    <nav role="navigation" class="fr-breadcrumb" aria-label="vous êtes ici :">
-      <ol class="fr-breadcrumb__list">
-        <li>
-          <g-link to="/" class="fr-breadcrumb__link">Accueil</g-link>
-        </li>
-        <li>
-          <span aria-current="page">Mentions légales</span>
-        </li>
-      </ol>
-    </nav>
+      <nav role="navigation" class="fr-breadcrumb" aria-label="vous êtes ici :">
+        <ol class="fr-breadcrumb__list">
+          <li>
+            <g-link to="/" class="fr-breadcrumb__link">Accueil</g-link>
+          </li>
+          <li>
+            <span aria-current="page">Mentions légales</span>
+          </li>
+        </ol>
+      </nav>
 
-    <div class="content">
+      <h1>Mentions légales</h1>
 
-       <h1>Mentions légales</h1>
+      <section>
+        <h2>Éditeur</h2>
+        <p>
+          Ce site est édité par l’équipe Design des services numériques, au sein de la direction interministérielle
+          du numérique, un service du Premier ministre.
+        </p>
+        <p>
+          DINUM</br>
+          20 avenue de Ségur</br>
+          75007 Paris</br>
+          Tel. accueil : 01.71.21.01.70</br>
+        </p>
+        <p>
+          SIRET : 12000101100010 (secrétariat général du gouvernement)</br>
+          SIREN : 120 001 011
+        </p>
+      </section>
 
+      <section>
+        <h2>Directeur de publication</h2>
+        <p>
+          Pour les sujets relevant de la DINUM (transformation numérique de l’État) :
+          Nadi Bou Hanna, directeur interministériel du numérique.
+        </p>
+      </section>
 
-        <section>
-          <h2>Éditeur</h2>
-          <p>
-            Ce site est édité par l’équipe Design des services numériques, au sein de la direction interministérielle
-            du numérique, un service du Premier ministre.
-          </p>
-          <p>
-            DINUM</br>
-            20 avenue de Ségur</br>
-            75007 Paris</br>
-            Tel. accueil : 01.71.21.01.70</br>
-          </p>
-          <p>
-            SIRET : 12000101100010 (secrétariat général du gouvernement)</br>
-            SIREN : 120 001 011
-          </p>
-        </section>
+      <section>
+        <h2>Conception et gestion du site</h2>
+        <p>
+          Ce site est conçu et développé par l’équipe Design des services numériques. De nouvelles
+          fonctionnalités sont ajoutées en continu.
+        </p>
+      </section>
 
-        <section>
-          <h2>Directeur de publication</h2>
-          <p>
-            Pour les sujets relevant de la DINUM (transformation numérique de l’État) :
-            Nadi Bou Hanna, directeur interministériel du numérique.
-          </p>
-        </section>
+      <section>
+        <h2>Code source du site</h2>
+        <p>
+          Le code source du site est <a href="https://github.com/DISIC/design.numerique.gouv.fr" target="_blank" rel="noreferrer noopener">disponible sur Github</a>.
+        </p>
+      </section>
 
-        <section>
-          <h2>Conception et gestion du site</h2>
-          <p>
-            Ce site est conçu et développé par l’équipe Design des services numériques. De nouvelles
-            fonctionnalités sont ajoutées en continu.
-          </p>
-        </section>
+      <section>
+        <h2>Propriété intellectuelle et liens</h2>
+        <p>
+          Tout site public ou privé est autorisé à établir, sans autorisation préalable,
+          un lien (y compris profond) vers les informations diffusées sur design.numerique.gouv.fr.
+        </p>
+        <p>
+          Sauf mention de propriété intellectuelle détenue par des tiers (notamment
+          un crédit sur certaines images), les contenus de ce site sont proposés
+          sous <a href="https://github.com/DISIC/design.numerique.gouv.fr/blob/master/LICENSE.md#licence-ouverte-20open-licence-20" target="_blank" rel="noreferrer noopener">licence ouverte 2.0</a>.
+        </p>
+        <p>
+          La mise en place de liens vers nos contenus et documents est cependant
+          est à privilégier par rapport à la reproduction sur un site tiers. La
+          mention explicite du site source design.numerique.gouv.fr est recommandée.
+        </p>
+      </section>
 
-        <section>
-          <h2>Code source du site</h2>
-          <p>
-            Le code source du site est <a href="https://github.com/DISIC/design.numerique.gouv.fr" target="_blank" rel="noreferrer noopener">disponible sur Github</a>.
-          </p>
-        </section>
-
-        <section>
-          <h2>Propriété intellectuelle et liens</h2>
-          <p>
-            Tout site public ou privé est autorisé à établir, sans autorisation préalable,
-            un lien (y compris profond) vers les informations diffusées sur design.numerique.gouv.fr.
-          </p>
-          <p>
-            Sauf mention de propriété intellectuelle détenue par des tiers (notamment
-            un crédit sur certaines images), les contenus de ce site sont proposés
-            sous <a href="https://github.com/DISIC/design.numerique.gouv.fr/blob/master/LICENSE.md#licence-ouverte-20open-licence-20" target="_blank" rel="noreferrer noopener">licence ouverte 2.0</a>.
-          </p>
-          <p>
-            La mise en place de liens vers nos contenus et documents est cependant
-            est à privilégier par rapport à la reproduction sur un site tiers. La
-            mention explicite du site source design.numerique.gouv.fr est recommandée.
-          </p>
-        </section>
-
-        <section>
-          <h2>Hébergement</h2>
-          <p>
-            Netlify Inc.</br>
-            2325 3rd Street, Suite 215</br>
-            San Francisco, California 94107</br>
-            États-Unis d’Amérique</br>
-            +1 844-899-7312</br>
-          </p>
-        </section>
+      <section>
+        <h2>Hébergement</h2>
+        <p>
+          Netlify Inc.</br>
+          2325 3rd Street, Suite 215</br>
+          San Francisco, California 94107</br>
+          États-Unis d’Amérique</br>
+          +1 844-899-7312</br>
+        </p>
+      </section>
 
     </div>
-
   </Layout>
 </template>
 
 
 <script>
-
   export default {
     metaInfo: {
       title: "Mention légales",
@@ -104,5 +100,4 @@
       }],
     }
   }
-
 </script>

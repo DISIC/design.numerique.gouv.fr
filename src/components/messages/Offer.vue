4bb20a1f692e935c38269805282b4689ad6371f7
Ugo <jeanmichelpaul@icloud.com>
2022-01-26 18:09:45 +0100

    Style offer message

--- src/components/messages/Offer.vue
+++ src/components/messages/Offer.vue
@@ -1,20 +1,20 @@
 <template>
 
-  <div class="message">
-    <h2>Vous avez besoin d’aide&nbsp;?</h2>
-
-    <p>Vous êtes une administration et vous souhaitez améliorer l’expérience usager (UX) et l’accessibilité de l’un de vos services ? Nous pouvons vous accompagner&nbsp;!</p>
-
-    <g-link class="fr-btn" to="/accompagnement/" >Voir nos accompagnements</g-link>
+  <div class="dg-content fr-px-2w">
+    <div class="fr-callout fr-callout--pink-macaron fr-fi-information-line">
+      <h2 class="fr-callout__title">Vous avez besoin d’aide&nbsp;?</h2>
+      <p class="fr-callout__text">
+        Vous êtes une administration et vous souhaitez améliorer l’expérience usager (UX) et l’accessibilité de l’un de vos services ? Nous pouvons vous accompagner&nbsp;!
+      </p>
+      <g-link class="fr-btn" to="/accompagnement/" >Voir nos accompagnements</g-link>
+    </div>
   </div>
 
 </template>
 
 
 <script>
-
   export default {
     name: 'Offer'
   }
-
 </script>

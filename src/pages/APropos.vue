66fe447655a1045701ea1117c3dc10e22500ca59
Marine Boudeau <m14868@gmail.com>
2022-02-09 13:09:11 +0100

    Fix typo

--- src/pages/APropos.vue
+++ src/pages/APropos.vue
@@ -14,7 +14,7 @@
           </ol>
         </nav>
         <h1 class="dg-cover__title"><img class="dg-picto fr-mr-2w" svg-inline src="../assets/images/recrutement-picto.svg" aria-hidden="true">Le pôle design des services numériques</h1>
-        <p class="fr-text--lead">Le pôle design des service numérique fait partie de la <a href="https://www.numerique.gouv.fr/dinum/" target="_blank" title="direction interministérielle du numérique - Nouvelle fenêtre">direction interministérielle du numérique</a>.</p>
+        <p class="fr-text--lead">Le pôle design des services numériques fait partie de la <a href="https://www.numerique.gouv.fr/dinum/" target="_blank" title="direction interministérielle du numérique - Nouvelle fenêtre">direction interministérielle du numérique</a>.</p>
       </div>
     </div>
 

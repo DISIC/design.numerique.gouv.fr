dba92a8d37394ce92f16bea14fcc2e5724514f6b
Ugo <jeanmichelpaul@icloud.com>
2022-02-09 19:15:39 +0100

    Select session on Cours pages

--- src/templates/Cours.vue
+++ src/templates/Cours.vue
@@ -26,9 +26,37 @@
 
     <div class="dg-content dg-content--xs fr-px-2w">
 
-      <div>
-        <p>{{ $page.cours.descriptionLongue }}</p>
-      </div>
+      <p>{{ $page.cours.descriptionLongue }}</p>
+
+      <form class="form" v-on:submit.prevent="addParticipant">
+        <div class="fr-input-group">
+          <label class="fr-label" for="email">Votre adresse e-mail<span class="fr-hint-text">L’adresse e-mail doit être au format prenom@mail.fr</span></label>
+          <input class="fr-input" type="email" id="email" v-model="form.email" required>
+        </div>
+        <div class="fr-input-group">
+          <label class="fr-label" for="organisme">Votre organisme</label>
+          <input class="fr-input" type="text" id="organisme" v-model="form.organisme" required>
+        </div>
+        <div class="fr-form-group">
+          <fieldset class="fr-fieldset">
+            <legend class="fr-fieldset__legend fr-text--regular" id='radio-hint-legend'>
+              Date de la session à laquelle vous souhaitez vous inscrire
+              <span class="fr-hint-text">D'autres sessions seront disponibles par la suite</span>
+            </legend>
+            <div class="fr-fieldset__content">
+              <div v-for="session in $page.cours.sessions" class="fr-radio-group">
+                <input type="radio" :id="session.id" name="session" required>
+                <label class="fr-label" :for="session.id">
+                  Le {{ session.date }} de {{ session.debut }} à {{ session.fin }}
+                </label>
+              </div>
+            </div>
+          </fieldset>
+        </div>
+        <p><small>Les données recueillies sur ce formulaire sont traitées par les équipes de la DINUM. Elles nous permettent de vous informer via e-mail des nouvelles liées à cet événement.</small></p>
+        <p><small>Conformément à la règlementation, vous disposez d’un droit d’opposition et d’un droit à la limitation du traitement de données vous concernant, ainsi que d’un droit d’accès, de rectification, de portabilité et d’effacement de vos données. Vous pouvez exercer vos droits en nous écrivant à contact@design.numerique.gouv.fr.</small></p>
+        <button class="fr-btn" id="submit" type="submit">S'inscrire</button>
+      </form>
 
     </div>
 
@@ -60,6 +88,12 @@
       descriptionCourte
       descriptionLongue
       tags
+      sessions {
+        id
+        date (format: "D MMMM YYYY", locale : "fr")
+        debut
+        fin
+      }
     }
   }
 </page-query>
@@ -109,7 +143,38 @@
           },
         ]
       };
-    }
+    },
+    data () {
+      return {
+        form: {
+          email: '',
+          organisme: '',
+        }
+      }
+    },
+    methods: {
+      addParticipant() {
+        document.getElementById('submit').disabled = true;
+        var Airtable = require('airtable');
+        var base = new Airtable({apiKey: process.env.GRIDSOME_AIRTABLE_API_KEY}).base(process.env.GRIDSOME_AIRTABLE_COURSE_NEW_BASE);
+        base('Inscriptions').create([
+        {
+          "fields": {
+            "E-mail": this.form.email,
+            "Organisme": this.form.organisme,
+            "Session": [document.querySelector('input[name="session"]:checked').id],
+          }
+        },], function(err, records) {
+          if (err) {
+            window.location.href = "/formulaire/erreur/";
+            console.error(err);
+            return;
+          } else {
+            window.location.href = "/formulaire/succes/";
+          }
+        });
+      }
+    },
   };
 </script>
 

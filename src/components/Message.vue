7111a97a04331ff1bedf8181dce21c1cf23db731
Ugo <jeanmichelpaul@icloud.com>
2022-02-28 18:42:06 +0100

    Change message subject on homescreen

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="dg-content dg-message dg-text-center fr-py-3w fr-px-4w">
     <p class="fr-badge fr-badge--new">Nouveau</p>
-    <h2 class="fr-h4 fr-mt-1w fr-mb-1w">Formation simplification de l'information</h2>
+    <h2 class="fr-h4 fr-mt-1w fr-mb-1w">Atelier : la recherche utilisateur en pratique !</h2>
     <p class="fr-mb-2w">
-      Nous lançons <strong>une nouvelle formation</strong> en ligne, sur la <strong>simplification de l'information à destination des usagers</strong> le jeudi 17 février prochain 🌼.
+      Nous lançons notre premier <strong>atelier de formation</strong> en ligne, <strong>Intégrer la recherche utilisateur dans la pratique&nbsp;!</strong> le jeudi 17 mars prochain&nbsp;🌈.
     </p>
-    <g-link class="dg-message__link fr-link fr-fi-arrow-right-line fr-link--icon-right" to="/formations/simplification-information/">
+    <g-link class="dg-message__link fr-link fr-fi-arrow-right-line fr-link--icon-right" to="/formations/recherche-utilisateur/atelier-recherche-utilisateur/">
       Infos et inscription
     </g-link>
   </div>

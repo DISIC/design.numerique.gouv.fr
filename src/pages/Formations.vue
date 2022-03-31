e1d1736b22400b8bd8022dc9829611a2433af9dc
Ugo <jeanmichelpaul@icloud.com>
2022-03-31 16:22:28 +0200

    Put recommanded courses in accordions on courses page

--- src/pages/Formations.vue
+++ src/pages/Formations.vue
@@ -55,48 +55,68 @@
       <section class="dg-contains-list fr-mt-8w">
         <h2>Nous recommandons également</h2>
 
-        <h3 class="fr-mt-6w fr-mb-2w">Accessibilité</h3>
-
-        <p class="fr-h6">Initiations :</p>
-        <ul>
-          <li><a href="https://www.fun-mooc.fr/en/cours/accessibilite-numerique/" title="L’accessibilité numérique - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">L’accessibilité numérique</a>. Ce <abbr title="Massive Open Online Course" lang="en">MOOC</abbr> est un cours généraliste d’introduction à l’accessibilité numérique. Il est disponible en mode « Archivé ouvert » et nécessite une inscription.</a></li>
-          <li><a href="https://fr.wikiversity.org/wiki/Mise_en_%C5%93uvre_de_l_accessibilite_numerique" title="Mise en œuvre de l’accessibilité numérique - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">Mise en œuvre de l’accessibilité numérique</a>, 14 heures, gratuit.</li>
-          <li>Si vous êtes agent de l’État et avez accès à la plateforme Mentor, deux parcours sont disponibles :
+        <section class="fr-accordion">
+          <h3 class="fr-accordion__title">
+            <button class="fr-accordion__btn" aria-expanded="false" aria-controls="accordion-1">Design</button>
+          </h3>
+          <div class="fr-collapse" id="accordion-1">
+            <p><strong>Initiations :</strong></p>
             <ul>
-              <li><a href="https://mentor.gouv.fr/catalog/314" title="L’accessibilité numérique selon votre métier - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">L’accessibilité numérique selon votre métier</a>, de 1h20 à 8h suivant les modules choisis dans le parcours, gratuit.</li>
-              <li><a href="https://mentor.gouv.fr/catalog/262" title="L’essentiel de l’accessibilité de la communication de l’État - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">L’essentiel de l’accessibilité de la communication de l’État</a>, 3h30, gratuit.</li>
+              <li><a href="https://openclassrooms.com/fr/courses/3013836-initiez-vous-au-design-thinking" title="Initiez-vous au Design - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">Initiez-vous au Design</a>, 6 heures, gratuit.</li>
+              <li><a href="https://openclassrooms.com/fr/courses/3013856-ux-design-decouvrez-les-fondamentaux" title="UX design : découvrez les fondamentaux - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">UX design&nbsp;: découvrez les fondamentaux</a>, 8 heures, gratuit.</li>
             </ul>
-          </li>
-        </ul>
-        <p class="fr-h6">Certification :</p>
-        <ul>
-          <li><a href="https://formations.access42.net/?p=877" title="Développer des sites web accessibles - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">Développer des sites web accessibles</a>, 9 heures, payant.</li>
-        </ul>
+          </div>
+        </section>
 
-        <h3 class="fr-mt-6w fr-mb-2w">Design</h3>
+        <section class="fr-accordion">
+          <h3 class="fr-accordion__title">
+            <button class="fr-accordion__btn" aria-expanded="false" aria-controls="accordion-2">Accessibilité</button>
+          </h3>
+          <div class="fr-collapse" id="accordion-2">
+            <p><strong>Initiations :</strong></p>
+            <ul>
+              <li><a href="https://www.fun-mooc.fr/en/cours/accessibilite-numerique/" title="L’accessibilité numérique - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">L’accessibilité numérique</a>. Ce <abbr title="Massive Open Online Course" lang="en">MOOC</abbr> est un cours généraliste d’introduction à l’accessibilité numérique. Il est disponible en mode « Archivé ouvert » et nécessite une inscription.</a></li>
+              <li><a href="https://fr.wikiversity.org/wiki/Mise_en_%C5%93uvre_de_l_accessibilite_numerique" title="Mise en œuvre de l’accessibilité numérique - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">Mise en œuvre de l’accessibilité numérique</a>, 14 heures, gratuit.</li>
+              <li>Si vous êtes agent de l’État et avez accès à la plateforme Mentor, deux parcours sont disponibles :
+                <ul>
+                  <li><a href="https://mentor.gouv.fr/catalog/314" title="L’accessibilité numérique selon votre métier - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">L’accessibilité numérique selon votre métier</a>, de 1h20 à 8h suivant les modules choisis dans le parcours, gratuit.</li>
+                  <li><a href="https://mentor.gouv.fr/catalog/262" title="L’essentiel de l’accessibilité de la communication de l’État - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">L’essentiel de l’accessibilité de la communication de l’État</a>, 3h30, gratuit.</li>
+                </ul>
+              </li>
+            </ul>
+            <p><strong>Certification :</strong></p>
+            <ul>
+              <li><a href="https://formations.access42.net/?p=877" title="Développer des sites web accessibles - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">Développer des sites web accessibles</a>, 9 heures, payant.</li>
+            </ul>
+          </div>
+        </section>
 
-        <p class="fr-h6">Initiations :</p>
-        <ul>
-          <li><a href="https://openclassrooms.com/fr/courses/3013836-initiez-vous-au-design-thinking" title="Initiez-vous au Design - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">Initiez-vous au Design</a>, 6 heures, gratuit.</li>
-          <li><a href="https://openclassrooms.com/fr/courses/3013856-ux-design-decouvrez-les-fondamentaux" title="UX design : découvrez les fondamentaux - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">UX design&nbsp;: découvrez les fondamentaux</a>, 8 heures, gratuit.</li>
-        </ul>
+        <section class="fr-accordion">
+          <h3 class="fr-accordion__title">
+            <button class="fr-accordion__btn" aria-expanded="false" aria-controls="accordion-3">Numérique responsable</button>
+          </h3>
+          <div class="fr-collapse" id="accordion-3">
+            <p><strong>Initiations :</strong></p>
+            <ul>
+              <li><a href="https://www.academie-nr.org/sensibilisation/" title="Sensibilisation au numérique responsable - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">Sensibilisation au numérique responsable</a>, 30 minutes, gratuit.</li>
+              <li><a href="https://www.academie-nr.org/" title="MOOC numérique responsable - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">MOOC Numérique Responsable</a>, 4h30, gratuit.<br>14 modules pour progresser et saisir l’ensemble des enjeux environnementaux, éthique et d’inclusion.</li>
+            </ul>
+          </div>
+        </section>
 
-        <h3 class="fr-mt-6w fr-mb-2w">Numérique responsable</h3>
+        <section class="fr-accordion">
+          <h3 class="fr-accordion__title">
+            <button class="fr-accordion__btn" aria-expanded="false" aria-controls="accordion-4">Qualité</button>
+          </h3>
+          <div class="fr-collapse" id="accordion-4">
+            <p><strong>Certification :</strong></p>
+            <ul>
+              <li><a href="https://www.opquast.com/certification/" title="Maîtrise de la qualité en projet web - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">Maîtrise de la qualité en projet web</a>, 14 heures, payant.</li>
+            </ul>
+          </div>
+        </section>
 
-        <p class="fr-h6">Initiations :</p>
-        <ul>
-          <li><a href="https://www.academie-nr.org/sensibilisation/" title="Sensibilisation au numérique responsable - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">Sensibilisation au numérique responsable</a>, 30 minutes, gratuit.</li>
-          <li><a href="https://www.academie-nr.org/" title="MOOC numérique responsable - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">MOOC Numérique Responsable</a>, 4h30, gratuit.<br>14 modules pour progresser et saisir l’ensemble des enjeux environnementaux, éthique et d’inclusion.</li>
-        </ul>
-
-        <h3 class="fr-mt-6w fr-mb-2w">Qualité</h3>
-
-        <p class="fr-h6">Certification :</p>
-        <ul>
-          <li><a href="https://www.opquast.com/certification/" title="Maîtrise de la qualité en projet web - Nouvelle fenêtre" target="_blank" rel="noreferrer noopener">Maîtrise de la qualité en projet web</a>, 14 heures, payant.</li>
-        </ul>
-
-        <p class="fr-mt-6w">Si votre organisme propose une formation certifiante et souhaite être référencé sur notre site, vous pouvez <a href="https://github.com/DISIC/design.numerique.gouv.fr/pulls" title="proposer une PR - nouvelle fenêtre"  target="_blank" rel="noreferrer noopener">proposer une <abbr lang="en" title="pull request">PR</abbr></a> ou nous contacter en utilisant l’adresse électronique contact@design.numerique.gouv.fr.</p>
+        <p class="fr-text--sm fr-mt-4w">Si votre organisme propose une formation certifiante et souhaite être référencé sur notre site, vous pouvez <a href="https://github.com/DISIC/design.numerique.gouv.fr/pulls" title="proposer une PR - nouvelle fenêtre"  target="_blank" rel="noreferrer noopener">proposer une <abbr lang="en" title="pull request">PR</abbr></a> ou nous contacter en utilisant l’adresse électronique contact@design.numerique.gouv.fr.</p>
       </section>
 
     </div>

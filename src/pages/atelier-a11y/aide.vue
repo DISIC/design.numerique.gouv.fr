b852074fbb79910cba152dcd8758ebb2719b1205
Yaacov <yaacov@goodimpact.studio>
2022-04-01 17:27:18 +0200

    Format code

--- src/pages/atelier-a11y/aide.vue
+++ src/pages/atelier-a11y/aide.vue
@@ -1,9 +1,12 @@
 <template>
   <Exercise>
-
     <section class="dg-cover dg-cover--linear fr-mb-6w">
       <div class="dg-cover__container fr-mb-1w">
-        <nav role="navigation" class="fr-breadcrumb" aria-label="vous êtes ici :">
+        <nav
+          role="navigation"
+          class="fr-breadcrumb"
+          aria-label="vous êtes ici :"
+        >
           <ol class="fr-breadcrumb__list">
             <li>
               <g-link to="/" class="fr-breadcrumb__link">Accueil</g-link>
@@ -14,39 +17,54 @@
           </ol>
         </nav>
         <h1 class="dg-cover__title">Aide</h1>
-
       </div>
     </section>
 
     <section class="fr-container fr-px-2w fr-mb-8w">
       <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--center">
-        <div class="fr-col-8">        
+        <div class="fr-col-8">
           <h2>Life is too short to ride sheet bike</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.</p
+          >
+          <p
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.</p
+          >
         </div>
       </div>
     </section>
-
   </Exercise>
 </template>
 
 <script>
-  export default {
-    metaInfo: {
-      title: "Qui sommes-nous ?",
-      meta: [{
-        name: "description",
-        content: "Exercice d’audit et de mise en conformité RGAA"
-      }],
-    }
-  }
-
+export default {
+  metaInfo: {
+    title: 'Qui sommes-nous ?',
+    meta: [
+      {
+        name: 'description',
+        content: 'Exercice d’audit et de mise en conformité RGAA',
+      },
+    ],
+  },
+};
 </script>
 
 <style type="scoped">
-a[href] { 
+a[href] {
   box-shadow: none;
-  color: var(--background-active-blue-france);;
+  color: var(--background-active-blue-france);
 }
 </style>
--- src/pages/atelier-a11y/merci.vue
+++ src/pages/atelier-a11y/merci.vue
@@ -1,9 +1,12 @@
 <template>
   <Exercise>
-
     <section class="dg-cover dg-cover--linear fr-mb-6w">
       <div class="dg-cover__container fr-mb-1w">
-        <nav role="navigation" class="fr-breadcrumb" aria-label="vous êtes ici :">
+        <nav
+          role="navigation"
+          class="fr-breadcrumb"
+          aria-label="vous êtes ici :"
+        >
           <ol class="fr-breadcrumb__list">
             <li>
               <g-link to="/" class="fr-breadcrumb__link">Accueil</g-link>
@@ -15,32 +18,32 @@
         </nav>
         <h1 class="dg-cover__title">Merci</h1>
         <p class="fr-text--lead">Votre vélo a bien été enregistré.</p>
-        <p>Note pour plus tard : même si votre vélo est immortel il faut en prendre soin.</p>
-
+        <p
+          >Note pour plus tard : même si votre vélo est immortel il faut en
+          prendre soin.</p
+        >
       </div>
     </section>
-
-
-
   </Exercise>
 </template>
 
 <script>
-  export default {
-    metaInfo: {
-      title: "Qui sommes-nous ?",
-      meta: [{
-        name: "description",
-        content: "Exercice d’audit et de mise en conformité RGAA"
-      }],
-    }
-  }
-
+export default {
+  metaInfo: {
+    title: 'Qui sommes-nous ?',
+    meta: [
+      {
+        name: 'description',
+        content: 'Exercice d’audit et de mise en conformité RGAA',
+      },
+    ],
+  },
+};
 </script>
 
 <style type="scoped">
-a[href] { 
+a[href] {
   box-shadow: none;
-  color: var(--background-active-blue-france);;
+  color: var(--background-active-blue-france);
 }
 </style>

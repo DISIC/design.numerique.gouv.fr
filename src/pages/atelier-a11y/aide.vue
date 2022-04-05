0faae4d696ea1ad553bef5e3a63f0c5ded79631b
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2022-04-05 17:33:48 +0200

    ajout d'une vidéo

--- src/pages/atelier-a11y/aide.vue
+++ src/pages/atelier-a11y/aide.vue
@@ -42,6 +42,9 @@
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.</p
           >
+          <div class="fr-responsive-vid">
+            <iframe class="fr-responsive-vid__player" src="https://www.youtube.com/embed/U8Pm5Cxe5cs?controls=0&amp;start=24" title="YouTube video player" frameborder="0"  ></iframe>
+          </div>
         </div>
       </div>
     </section>

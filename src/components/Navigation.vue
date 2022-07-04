6e2c2a759b88946349727e86e3fe5a4a46925624
Ugo <jeanmichelpaul@icloud.com>
2022-07-04 17:10:51 +0200

    Always show Recrutement page

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -26,7 +26,7 @@
           <li class="fr-nav__item">
             <a class="fr-nav__link" href="/articles/">Articles</a>
           </li>
-          <li class="fr-nav__item" v-if="$static.allJob.edges.length > 0">
+          <li class="fr-nav__item">
             <a class="fr-nav__link" href="/recrutement/">Recrutement <span v-if="$static.allJob.edges.length == 1" class="fr-badge fr-badge--sm fr-badge--pink-macaron">{{ $static.allJob.edges.length }} offre</span><span v-else-if="$static.allJob.edges.length > 1" class="fr-badge fr-badge--sm fr-badge--pink-macaron">{{ $static.allJob.edges.length }} offres</span></a>
           </li>
         </ul>

32c142e449209a33f3086615a76e3bb1ab771b77
Ugo <jeanmichelpaul@icloud.com>
2022-07-05 19:41:55 +0200

    Working on Poste pages

--- src/templates/Poste.vue
+++ src/templates/Poste.vue
@@ -3,7 +3,7 @@
     <div class="dg-content fr-px-2w">
 
       <div class="dg-cover dg-cover--linear fr-mb-4w">
-        <div class="dg-cover__container fr-mb-1w">
+        <div class="dg-cover__container dg-cover__container fr-mb-1w">
           <nav role="navigation" class="fr-breadcrumb" aria-label="vous êtes ici :">
             <ol class="fr-breadcrumb__list">
               <li>
@@ -13,38 +13,35 @@
                 <g-link to="/recrutement/" class="fr-breadcrumb__link">Recrutement</g-link>
               </li>
               <li>
-                <span aria-current="page">{{ $page.poste.intitule }} - {{ $page.poste.equipe }}</span>
+                <span aria-current="page">{{ $page.poste.intitule }}</span>
               </li>
             </ol>
           </nav>
-          <p v-if="Date.now() - new Date($page.poste.publication) < 1209600000" class="fr-badge fr-badge--new fr-mb-2w fr-mt-1v">Nouveau</p>
+          <p v-if="Date.now() - new Date($page.poste.publication) < 1209600000" class="fr-badge fr-badge--new fr-mb-2w">Nouveau</p>
           <h1 class="dg-cover__title" v-html="$page.poste.intitule" />
           <p class="fr-text--lead" v-html="$page.poste.descriptionCourte" />
-          <div class="dg-inline-block">
-            <p class="dg-text-bold dg-inline-block fr-text--sm fr-mb-1w  fr-mr-1w">Contrat :</p>
-            <g-link :to="{ path: '/recrutement/', query: { contrat: $page.poste.contrat }}" class="fr-tag fr-mr-4w">{{ $page.poste.contrat }}</g-link>
+          <div class="dg-inline-block fr-mb-0 fr-mb-md-n1w fr-mr-4w">
+            <p class="dg-text-bold dg-inline-block fr-mr-1w">Contrat :</p>
+            <g-link :to="{ path: '/recrutement/', query: { contrat: $page.poste.contrat }}" class="fr-tag">{{ $page.poste.contrat }}</g-link>
           </div>
-          <div class="dg-inline-block fr-mb-1w ">
-            <p v-if="$page.poste.tags.length == 1" class="dg-text-bold dg-inline-block fr-text--sm fr-mb-1w fr-mr-1w">Cathégorie :</p>
-            <p v-else-if="$page.poste.tags.length > 1" class="dg-text-bold dg-inline-block fr-text--sm fr-mb-1w fr-mr-1w">Cathégories :</p>
+          <div class="dg-inline-block fr-mb-0 fr-mb-md-n1w">
+            <p v-if="$page.poste.tags.length == 1" class="dg-text-bold dg-inline-block fr-mb-1w fr-mr-1w">Cathégorie :</p>
+            <p v-else-if="$page.poste.tags.length > 1" class="dg-text-bold dg-inline-block fr-mb-1w fr-mr-1w">Cathégories :</p>
             <div class="dg-inline-block">
               <ul class="dg-inline-flex fr-tags-group">
                 <li v-for="tag in $page.poste.tags"><g-link :to="{ path: '/recrutement/', query: { tag: tag }}" class="fr-tag ">{{ tag }}</g-link></li>
               </ul>
             </div>
           </div>
+          <ul class="dg-list-no-decoration">
+            <li class="dg-inline-block fr-mb-5v fr-mr-4w"><strong>Durée de la mission : </strong><span v-html="$page.poste.duree" class="fr-badge fr-badge--green-tilleul-verveine fr-badge--sm" /></li>
+            <li class="dg-inline-block fr-mb-5v fr-mb-1v"><strong>Date de début : </strong><span v-html="(new Date($page.poste.debut) - Date.now()) < 0 ? 'Dès que possible' : (new Date($page.poste.debut)).toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' })" class="fr-badge fr-badge--green-tilleul-verveine fr-badge--sm" /></li>
+            <li><strong>Localisation : </strong><span v-html="$page.poste.localisation" class="fr-badge fr-badge--green-tilleul-verveine fr-badge--sm" /></li>
+          </ul>
         </div>
       </div>
 
-      <div class="dg-content dg-content--xs fr-px-2w">
-
-        <h2>Informations générales</h2>
-        <ul class="dg-list-no-decoration">
-          <li class="fr-mb-1v"><strong>Durée de la mission : </strong><span v-html="$page.poste.duree" class="fr-badge fr-badge--green-tilleul-verveine fr-badge--sm" /></li>
-          <li class="fr-mb-1v"><strong>Date de début : </strong><span v-html="(new Date($page.poste.debut)).toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' })" class="fr-badge fr-badge--green-tilleul-verveine fr-badge--sm" /></li>
-          <li class="fr-mb-1v"><strong>Type de contrat : </strong><span v-html="$page.poste.contrat" class="fr-badge fr-badge--green-tilleul-verveine fr-badge--sm" /></li>
-          <li><strong>Localisation : </strong><span v-html="$page.poste.localisation" /></li>
-        </ul>
+      <div class="dg-content dg-content--xs">
 
         <h2>Le contexte</h2>
 
@@ -60,7 +57,7 @@
         <p>Vous pouvez nous contacter à l'adresse suivante : <strong>{{$page.poste.contact}}</strong></p></p>
 
         <h2>Pour candidater</h2>
-        <g-link :to="{ path: '/recrutement/partager-interet/', query: { poste: $page.poste.id }}" class="fr-btn">Partager votre intérêt</g-link>
+        <g-link :to="{ path: '/recrutement/partager-interet/', query: { poste: $page.poste.id, intitule: $page.poste.intitule }}" class="fr-btn">Partager votre intérêt</g-link>
 
         <p class="fr-mt-6w">
           <hr>

46e71506481720c7cd010de4c217c35c71ce2889
Ugo <jeanmichelpaul@icloud.com>
2022-07-07 12:34:35 +0200

    Add theme selector in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -32,25 +32,76 @@
             </div>
         </div>
        <div class="fr-footer__bottom">
-            <ul class="fr-footer__bottom-list">
-                <!-- <li class="fr-footer__bottom-item">
-                  <g-link class="fr-footer__bottom-link" to="/contact/">Contact</g-link>
-                </li> -->
-                <li class="fr-footer__bottom-item">
-                  <g-link class="fr-footer__bottom-link" to="/misc/plan-du-site/">Plan du site</g-link>
-                </li>
-                <li class="fr-footer__bottom-item">
-                  <g-link class="fr-footer__bottom-link" to="/misc/accessibilite/">Accessibilité : totalement conforme</g-link>
-                </li>
-                <li class="fr-footer__bottom-item">
-                  <g-link class="fr-footer__bottom-link" to="/misc/mentions-legales/">Mentions légales</g-link>
-                </li>
-                <li class="fr-footer__bottom-item">
-                  <g-link class="fr-footer__bottom-link"  to="/misc/donnees-personnelles/">Données personnelles et cookies</g-link>
-                </li>
-                <!-- <li class="fr-footer__bottom-item">
-                  <button class="fr-footer__bottom-link fr-fi-theme-fill fr-link--icon-left" aria-controls="fr-theme-modal" data-fr-opened="false">Paramètres d'affichage</button>
-                </li> -->
+          <ul class="fr-footer__bottom-list">
+            <li class="fr-footer__bottom-item">
+              <g-link class="fr-footer__bottom-link" to="/misc/plan-du-site/">Plan du site</g-link>
+            </li>
+            <li class="fr-footer__bottom-item">
+              <g-link class="fr-footer__bottom-link" to="/misc/accessibilite/">Accessibilité : totalement conforme</g-link>
+            </li>
+            <li class="fr-footer__bottom-item">
+              <g-link class="fr-footer__bottom-link" to="/misc/mentions-legales/">Mentions légales</g-link>
+            </li>
+            <li class="fr-footer__bottom-item">
+              <g-link class="fr-footer__bottom-link"  to="/misc/donnees-personnelles/">Données personnelles et cookies</g-link>
+            </li>
+            <li class="fr-footer__bottom-item">
+              <button class="fr-footer__bottom-link fr-fi-theme-fill fr-link--icon-left" aria-controls="fr-theme-modal" data-fr-opened="false">Paramètres d'affichage</button>
+              <dialog id="fr-theme-modal" class="fr-modal" role="dialog" aria-labelledby="fr-theme-modal-title">
+                <div class="fr-container fr-container--fluid fr-container-md">
+                  <div class="fr-grid-row fr-grid-row--center">
+                    <div class="fr-col-12 fr-col-md-6 fr-col-lg-4">
+                      <div class="fr-modal__body">
+                        <div class="fr-modal__header">
+                          <button class="fr-link--close fr-link" aria-controls="fr-theme-modal">Fermer</button>
+                        </div>
+                        <div class="fr-modal__content">
+                          <h1 id="fr-theme-modal-title" class="fr-modal__title">
+                            Paramètres d’affichage
+                          </h1>
+                          <div id="fr-display" class="fr-form-group fr-display">
+                            <div class="fr-form-group">
+                              <fieldset class="fr-fieldset">
+                                  <legend class="fr-fieldset__legend fr-text--regular" id='-legend'>
+                                    Choisissez un thème pour personnaliser l’apparence du site.
+                                  </legend>
+                                  <div class="fr-fieldset__content">
+                                    <div class="fr-radio-group fr-radio-rich">
+                                      <input value="light" type="radio" id="fr-radios-theme-light" name="fr-radios-theme">
+                                      <label class="fr-label" for="fr-radios-theme-light">Thème clair
+                                      </label>
+                                      <div class="fr-radio-rich__img">
+                                        <img svg-inline src="../assets/images/theme/light.svg" alt="" />
+                                      </div>
+                                    </div>
+                                    <div class="fr-radio-group fr-radio-rich">
+                                      <input value="dark" type="radio" id="fr-radios-theme-dark" name="fr-radios-theme">
+                                      <label class="fr-label" for="fr-radios-theme-dark">Thème sombre
+                                      </label>
+                                      <div class="fr-radio-rich__img">
+                                        <img svg-inline src="../assets/images/theme/dark.svg" class="fr-responsive-image" alt="" />
+                                      </div>
+                                    </div>
+                                    <div class="fr-radio-group fr-radio-rich">
+                                      <input value="system" type="radio" id="fr-radios-theme-system" name="fr-radios-theme">
+                                      <label class="fr-label" for="fr-radios-theme-system">Système
+                                          <span class="fr-hint-text">Utilise les paramètres système.</span>
+                                      </label>
+                                      <div class="fr-radio-rich__img">
+                                        <img svg-inline src="../assets/images/theme/system.svg" alt="" />
+                                      </div>
+                                    </div>
+                                  </div>
+                                </fieldset>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </dialog>
+              </li>
             </ul>
             <div class="fr-footer__bottom-copy">
                 <p>Sauf mention contraire, tous les contenus de ce site sont sous <a href="https://github.com/etalab/licence-ouverte/blob/master/LO.md" target="_blank">licence etalab-2.0</a>

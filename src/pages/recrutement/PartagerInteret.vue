5e44748b4bb3960dd054ad58c231c0187f9b3040
Ugo <jeanmichelpaul@icloud.com>
2022-07-11 19:01:25 +0200

    Add pilotage skill to Partage interet page

--- src/pages/recrutement/PartagerInteret.vue
+++ src/pages/recrutement/PartagerInteret.vue
@@ -91,6 +91,10 @@
                   <input type="checkbox" id="checkboxes-9" value="Rédaction UX" v-model="form.skills">
                   <label class="fr-label" for="checkboxes-9">Rédaction UX</label>
                 </div>
+                <div class="fr-checkbox-group">
+                  <input type="checkbox" id="checkboxes-10" value="Pilotage" v-model="form.skills">
+                  <label class="fr-label" for="checkboxes-10">Pilotage</label>
+                </div>
               </div>
             </fieldset>
           </div>

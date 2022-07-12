fe0fc6a2131226d632df1bcc1bb7bdcd25fd3fff
Ugo <jeanmichelpaul@icloud.com>
2022-07-12 15:50:54 +0200

    Disable recruitment form for DSN jobs

--- src/templates/Poste.vue
+++ src/templates/Poste.vue
@@ -47,7 +47,8 @@
         <p>Vous pouvez nous contacter à l'adresse suivante : <strong>{{$page.poste.contact}}</strong></p></p>
 
         <h2>Pour candidater</h2>
-        <g-link :to="{ path: '/recrutement/partager-interet/', query: { poste: $page.poste.id }}" class="fr-btn fr-btn--icon-right fr-icon-arrow-right-line">Postuler</g-link>
+        <p v-if="this.$page.poste.destination == 'DSN'" >Pour postuler à cette offre, envoyer nous votre CV et une lettre de motivation à l'adresse <strong>candidatures-dinum@pm.gouv.fr</strong>.</p>
+        <g-link v-else :to="{ path: '/recrutement/partager-interet/', query: { poste: $page.poste.id }}" class="fr-btn fr-btn--icon-right fr-icon-arrow-right-line">Postuler</g-link>
 
         <p class="fr-mt-8w">
           <hr>

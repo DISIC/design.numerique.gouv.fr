4f32cb3f929fd44391e70cf26db8c4b98d911df8
Yaacov <yaacov@goodimpact.studio>
2022-08-25 20:06:51 +0200

    Update Déclaration d'accessibilité

--- src/pages/misc/Accessibilite.vue
+++ src/pages/misc/Accessibilite.vue
@@ -18,35 +18,68 @@
       <section>
         <h2>Déclaration d’accessibilité</h2>
          <p>
-           Le site design.numerique.gouv.fr est développé selon les recommandations du référentiel général d’amélioration de l’accessibilité (RGAA) pour être accessible à tous les usagers quel que soit le matériel ou le logiciel utilisé pour naviguer sur internet.
+           Cette déclaration d’accessibilité s’applique au site DesignGouv (design.numerique.gouv.fr)
          </p>
          <h3>État de conformité</h3>
-         <p>DesignGouv est <strong>totalement conforme</strong> avec le RGAA 4.</p>
-         <h3>Échantillon évalué</h3>
-         <p>Un premier audit, effectué en septembre 2020, a permis d’identifier certaines anomalies et des axes de progressions. L’audit mené en décembre 2020 par nos soins n’a relevé aucune non-conformité sur l’échantillon suivant :</p>
+         <p>Le site DesignGouv est <strong>totalement conforme</strong> avec le référentiel général d’amélioration de l’accessibilité (RGAA) version 4.</p>
+         <h4>Résultats des tests</h4>
+				 <p>L’audit de conformité réalisé par DesignGouv révèle que 100% des critères du RGAA version 4 sont respectés ;</p>
+				 <h3>Établissement de cette déclaration d’accessibilité</h3>
+				 <p>Cette déclaration a été établie en septembre 2020.<br>Elle a été mise à jour :</p>
+				 <ul>
+					<li>en décembre 2020 ;</li>
+					<li>en août 2022.</li>
+				 </ul>
+				 <h4>Technologies utilisées pour la réalisation du site</h4>
+				 <ul>
+					<li><a href="https://gridsome.org/" title="Gridsome - nouvelle fenêtre" target="_blank" rel="noreferrer noopener">Gridsome</a> ;</li>
+					<li><a href="https://vuejs.org/" title="Vue.js - nouvelle fenêtre" target="_blank" rel="noreferrer noopener">Vue.js</a> ;</li>
+					<li><a href="https://sass-lang.com/" title="Sass - nouvelle fenêtre" target="_blank" rel="noreferrer noopener">Sass</a> ;</li>
+					<li>HTML 5 ;</li>
+					<li>CSS 3 ;</li>
+					<li>Javascript.</li>
+				 </ul>
+				 <h4>Environnement de test</h4>
+				 <p>Les vérifications de restitution de contenus ont été réalisées sur la base d’une combinaison fournie par la base de référence du RGAA, avec les versions suivantes :</p>
+				 <ul>
+					<li>Safari Version 15.2 (15612.3.6.1.8, 15612) avec VoiceOver Version 10 (708.14)</li>
+					<li>Brave Version 1.42.97 Chromium: 104.0.5112.102 (Build officiel) (x86_64)</li>
+				 </ul>
+				 <h4>Outils pour évaluer l’accessibilité</h4>
+				 <ul>
+					<li>Extension Chrome "Assistant RGAA" ;</li>
+					<li>Extension Chrome "Web Developer" ;</li>
+					<li>Extension Chrome "HeadingsMap" ;</li>
+					<li>Extension Chrome ArcToolkit ;</li>
+					<li>Application "Colour Contrast Analyser".</li>
+				 </ul>
+				 <h4>Pages du site ayant fait l’objet de la vérification de conformité</h4>
          <ul>
-           <li><a href="/">Accueil</a></li>
-           <li><a href="/newsletter/">Newsletter</a></li>
+           <li><a href="/">DesignGouv</a></li>
+           <li><a href="/contact/">Contact</a></li>
+           <li><a href="/misc/accessibilite/">Accessibilité</a></li>
            <li><a href="/misc/mentions-legales/">Mentions légales</a></li>
-           <li><a href="/misc/accessibilite">Accessibilité</a></li>
+           <li><a href="/accessibilite-numerique/">Accessibilité numérique</a></li>
+           <li><a href="/misc/plan-du-site/">Plan du site</a></li>
+           <li><a href="/accompagnement/">Nos offres d’accompagnement</a></li>
+           <li><a href="/articles/">Articles</a></li>
+           <li><a href="/a-propos">Équipe</a></li>
+           <li><a href="/accessibilite-numerique/roles-cles/">Rôles clés</a></li>
+           <li><a href="/accessibilite-numerique/roles-cles/developer/">Développeur ou développeuse</a></li>
            <li><a href="/commando-ux/">Commando UX</a></li>
-           <li><a href="/equipe/">Notre équipe</a></li>
+           <li><a href="/articles/2021-03-21-evenement-design/">L’État centré usager ? Oui c’est possible</a></li>
+           <li><a href="/newsletter/">Newsletter</a></li>
+           <li><a href="/accessibilite-numerique/jeu-de-oaa/">Le jeu de l’OAA</a></li>
          </ul>
-       </section>
-
-       <section>
-         <h2>Retour d’information et contact</h2>
-         <p>La DINUM s’engage à prendre les moyens nécessaires afin de donner accès, dans un délai raisonnable, aux informations et fonctionnalités recherchées par la personne handicapée, que le contenu fasse l’objet d’une dérogation ou non.</p>
+         <h3>Retour d’information et contact</h3>
+         <p>La direction interministérielle du numérique (DINUM) s’engage à prendre les moyens nécessaires afin de donner accès, dans un délai raisonnable, aux informations et fonctionnalités recherchées par la personne handicapée, que le contenu fasse l’objet d’une dérogation ou non.</p>
          <p>La DINUM invite les personnes qui rencontreraient des difficultés à la contacter par courriel à communication.dinum@modernisation.gouv.fr afin qu’une assistance puisse être apportée (alternative accessible, information et contenu donnés sous une autre forme).</p>
-       </section>
-
-       <section>
-         <h2>Défenseur des droits</h2>
-         <p>Si vous constatez un défaut d’accessibilité vous empêchant d’accéder à un contenu ou une fonctionnalité du site, que vous nous le signalez et que vous ne parvenez pas à obtenir une réponse rapide de notre part, vous êtes en droit de faire parvenir vos doléances ou une demande de saisine au Défenseur des droits. Plusieurs moyens sont à votre disposition :</p>
+         <h2>Voies de recours</h2>
+         <p>Si vous constatez un défaut d’accessibilité vous empêchant d’accéder à un contenu ou une fonctionnalité du site, que vous nous le signalez et que vous ne parvenez pas à obtenir une réponse rapide de notre part, vous êtes en droit de faire parvenir vos doléances ou une demande de saisine au Défenseur des droits. Plusieurs moyens sont à votre disposition :</p>
          <ul>
            <li>un <a href="https://www.defenseurdesdroits.fr/nous-contacter" title="formulaire de contact - nouvelle fenêtre" target="_blank" rel="noreferrer noopener">formulaire de contact</a></li>
            <li>la <a href="https://www.defenseurdesdroits.fr/fr/saisir/delegues" title="liste des délégués de votre région - nouvelle fenêtre"  target="_blank" rel="noreferrer noopener">liste des délégués de votre région</a> avec leurs informations de contact directs</li>
-           <li>un numéro de téléphone : le 09 69 39 00 00 du lundi au vendredi de 8h00 à 20h00 (coût d’un appel local)</li>
+           <li>un numéro de téléphone : le <a href="tel:+33500000000">+33 (0) 9 69 39 00 00</a> du lundi au vendredi de 8h30 à 19h30 (coût d’un appel local)</li>
            <li>une adresse postale dans l’<a href="https://lannuaire.service-public.fr/autorites-independantes/autorite-administrative-independante_195381" title="annuaire de l’administration - nouvelle fenêtre" target="_blank" rel="noreferrer noopener">annuaire de l’administration</a></li>
          </ul>
        </section>

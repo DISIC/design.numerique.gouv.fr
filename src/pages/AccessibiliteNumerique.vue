2e5d2a150650d5b9df9421df81f0393355e323b5
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2023-08-11 08:59:53 +0200

    add anchors

--- src/pages/AccessibiliteNumerique.vue
+++ src/pages/AccessibiliteNumerique.vue
@@ -15,7 +15,18 @@
         </nav>
         <h1 class="dg-cover__title"><img class="dg-picto fr-mr-2w" svg-inline src="../assets/images/accessibilite-picto.svg" aria-hidden="true">Accessibilité numérique</h1>
         <p class="fr-text--lead">L’accessibilité numérique consiste à rendre les contenus et services numériques compréhensibles et utilisables par les personnes en situation de handicap.</p>
-      </div>
+        <ul class="fr-tags-group">
+          <li>
+              <a href="#comprendre" class="fr-tag" target="_self">Comprendre</a>
+          </li>
+          <li>
+              <a href="#action" class="fr-tag" target="_self">Passer à l’action</a>
+          </li>
+          <li>
+              <a href="#emporter" class="fr-tag" target="_self">À emporter</a>
+          </li>
+        </ul>
+      </div>      
     </section>
 
     <section class="dg-content fr-px-2w">
@@ -26,7 +37,7 @@
       </section>
 
       <section class="fr-mt-6w">
-        <h2>Comprendre</h2>
+        <h2 id="comprendre">Comprendre</h2>
         <div class="fr-grid-row fr-grid-row--gutters">
           <div class="fr-col-xl-3 fr-col-md-6 fr-col-12">
               <div class="fr-tile fr-enlarge-link">
@@ -74,22 +85,22 @@
       </section>
 
       <section class="fr-mt-6w">
-        <h2>Passer à l’action</h2>
+        <h2 id="action">Passer à l’action</h2>
         <p>Pour atteindre un taux de conformité de 100 %, rien de plus simple : il faut former vos équipes, intégrer l’accessibilité dès la phase de design et tout au long de la vie du projet. Et surtout <strong>avoir une volonté forte de rendre vos services accessibles à tous les usagers</strong>, partagée à tous les niveaux de la hiérarchie.</p>
 
-        <h3>Qui doit faire quoi ?</h3>
+        <h3 class="fr-h4">Qui doit faire quoi ?</h3>
         <p>Les <g-link to="/outils/roles-cles/">fiches métiers</g-link> décrivent votre rôle, vos actions et vos outils.</p>
 
-        <h3>Je souhaite faire un état des lieux</h3>
+        <h3 class="fr-h4">Je souhaite faire un état des lieux</h3>
         <p>Si vous n’avez pas les ressources nécessaires en interne pour le faire, faites appel à un <g-link to="/accessibilite-numerique/prestations-accessibilite-numerique">prestataire</g-link> et sollicitez un ou une experte pour effectuer un <strong>diagnostic rapide</strong>, des <strong>audits</strong> complets ou <strong>accompagner vos équipes</strong>.</p>
 
-        <h3>Je souhaite établir une déclaration d’accessibilité</h3>
+        <h3 class="fr-h4">Je souhaite établir une déclaration d’accessibilité</h3>
         <p>C’est très simple. Vous pouvez utiliser le <g-link to="/outils/exemple-declaration-accessibilite/">modèle de déclaration</g-link>.</p>
 
-        <h3>Je souhaite former mes équipes</h3>
+        <h3 class="fr-h4">Je souhaite former mes équipes</h3>
         <p>Vous pouvez faire appel à des <g-link to="/accessibilite-numerique/prestations-accessibilite-numerique">prestataires</g-link> pour <strong>former et sensibiliser</strong> vos équipes.<br>Le <abbr title="Fonds pour l’Insertion des Personnes Handicapées dans la Fonction Publique">FIPHFP</abbr> peut vous aider à financer la formation des agents publics.</p>
 
-        <h3>Dans tous les cas, utilisez le <g-link to="/accessibilite-numerique/jeu-de-oaa">jeu de l’<abbr title="organisation de l’amélioration de l’accessiblité">oaa</abbr></g-link>.</h3>
+        <h3 class="fr-h4">Dans tous les cas, utilisez le <g-link to="/accessibilite-numerique/jeu-de-oaa">jeu de l’<abbr title="organisation de l’amélioration de l’accessiblité">oaa</abbr></g-link>.</h3>
 
         <p><strong>Sensibilisez</strong> et <strong>formez</strong> les décideurs et décideuses, <span lang="en">designers</span>, développeurs et développeuses, <span lang="en">product owner</span>, cheffes et chefs de projets sur le sujet de l’accessibilité numérique.</p>
         <p>Intégrez la conformité au RGAA dans vos process.</p>
@@ -97,7 +108,7 @@
       </section>
 
       <section class="fr-mt-6w">
-        <h2>À emporter</h2>
+        <h2 id="emporter">À emporter</h2>
         <p><g-link to="/accessibilite-numerique/a-emporter">Un mémo</g-link> pour savoir ce qu‘il faut faire, comment le faire et avec quels moyens.
         </p>
       </section>

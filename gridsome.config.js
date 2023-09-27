aa6dab250cde94ef148bdd3f63c1f772f062f275
Yaacov <yaacov@goodimpact.studio>
2023-09-27 10:56:54 +0200

    ⏪️ feat(scss): revert scss build rework

--- gridsome.config.js
+++ gridsome.config.js
@@ -11,7 +11,7 @@ function addStyleResource (rule) {
     .loader('style-resources-loader')
     .options({
       patterns: [
-        path.resolve(__dirname, './src/assets/scss/*.scss'),
+        path.resolve(__dirname, './src/assets/scss/_vars.scss'),
       ],
     })
 }

ac92233604331c92c830aca851fea21245121f85
Yaacov <yaacov@goodimpact.studio>
2023-09-27 17:54:33 +0200

    📦️ Chore(yarn): add sindresorhus/slugify + clean
    Attempt to make it build on prod

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,8 +5,6 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-const slugify = require('@sindresorhus/slugify');
-
 module.exports = function (api) {
   api.loadSource(({ addSchemaResolvers }) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/

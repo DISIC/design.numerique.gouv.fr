65826ea7e0745150d1110bc3af4b28ee65bcc5a2
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2024-01-15 12:32:16 +0100

    add images

--- src/pages/atelier-a11y/aide.vue
+++ src/pages/atelier-a11y/aide.vue
@@ -23,8 +23,114 @@
     <section class="fr-container fr-px-2w fr-mb-8w">
       <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--center">
         <div class="fr-col-8">
-          <h3 class="fr-h1">Bien choisir son vélo</h3>
-        <div class="fr-table fr-table--bordered fr-table--no-caption">
+          <h3 class="fr-h1">Vue éclatée d’un vélo</h3>
+          <img src="/assets/images/veloeclate.jpg" alt="Vue éclatée d’une bicyclette" />
+          <div class="fr-container">
+            <div class="fr-grid-row fr-grid-row--right "><p class="fr-text--xs"><a rel="noopener noreferrer" href="https://wiklou.org/wiki/Fichier:Velo_vueEclatee_diffWeb_HD.jpg#file" target="_blank" title="Source de l’image - nouvelle fenêtre">Source de l’image</a></p>
+            </div>
+          </div>
+          <section class="fr-accordion">
+            <div class="fr-accordion__title fr-content-media__transcription">
+              <button class="fr-accordion__btn" aria-expanded="false" aria-controls="accordion-bike">Voir la transcription de l’image</button>
+            </div>
+            <div class="fr-collapse" id="accordion-bike">
+              <h4>Guidon</h4>
+              <ul>
+                <li>Cintre</li>
+                <li>Potence à plongeur, écrou, vis</li>
+                <li>Poignée droite/gauche</li>
+                <li>Levier de frein droit/gauche</li>
+                <li>Levier de vitesses arrière/avant</li>
+                <li>cables vitesses</li>
+                <li>butée</li>
+                <li>gaine</li>
+                <li>Cable de frein</li>
+              </ul>
+              <h4>Jeu de direction</h4>
+              <ul>
+                <li>Contre-écrou</li>
+                <li>Écrou</li>
+                <li>Cage de roulement</li>
+                <li>Roulement à bille</li>
+                <li>Cage de roulement</li>
+                <li>Colonne de direction du cadre</li>
+                <li>Cage de roulement</li>
+                <li>Roulement à bille</li>
+                <li>Fourche</li>
+              </ul>
+              <h4>Jeu de direction <span lang="en">A-headset</span></h4>
+              <ul>
+                <li>Étoile</li>
+                <li>Vis</li>
+                <li>Potence <span lang="en">A-headset</span></li>
+                <li>Rondelles</li>
+                <li>Cage de roulement</li>
+                <li>Roulement à bille</li>
+                <li>Cage de roulement</li>
+                <li>Colonne de direction du cadre</li>
+                <li>Cage de roulement</li>
+                <li>Roulement à bille</li>
+                <li>Cône</li>
+                <li>Fourche</li>
+              </ul>
+              <h4>Freins</h4>
+              <h5>Machoire classique</h5>
+              <ul>
+                <li>Vis percée</li>
+                <li>Écrou de réglage</li>
+                <li>Écrou</li>
+                <li>Rondelle</li>
+                <li>Vis percée</li>
+                <li>Écrou</li>
+                <li>Rondelle</li>
+                <li>Patin de frein</li>
+                <li>Rondelle</li>
+                <li>Écrou</li>
+              </ul>
+              <h5><span lang="en">V-brake</span></h5>
+              <ul>
+                <li>Plot de fixation à la fourche</li>
+                <li>Ressort</li>
+                <li>Écrou</li>
+                <li>Vis</li>
+                <li>Patin de frein</li>
+                <li>Ressort</li>
+                <li>Écrou</li>
+                <li>Vis</li>
+                <li>Flexible</li>
+                <li>Écrou vis percée</li>
+              </ul>
+              <h4>Roue avant</h4>
+              <ul>
+                <li>Rayon</li>
+                <li>Tête de rayon</li>
+                <li>Jante</li>
+                <li>Fond de jante</li>
+                <li>Chambre à air</li>
+                <li>Valve</li>
+                <li>Pneu</li>
+              </ul>
+              <h4>Pédalier</h4>
+              <p>...</p>
+              <h5>à clavettes</h5>
+              <p>...</p>
+              <h5>axe carré</h5>
+              <p>...</p>
+              <h4>Moyeu arrière</h4>
+              <p>...</p>
+              <h5>à roue libre</h5>
+              <p>...</p>
+              <h5>à cassette</h5>
+              <p>...</p>
+              <h4>Dérailleur arrière</h4>
+              <p>...</p>
+              <h4>Selle</h4>
+              <p>...</p>
+            </div>
+          </section>  
+
+          <h3 class="fr-h1 fr-mt-8v">Bien choisir son vélo</h3>
+          <div class="fr-table fr-table--bordered fr-table--no-caption">
           <table summary="Quel type de vélo choisir ?"><caption>Comparatif de type de vélo</caption>
             <thead><tr>
               <th scope="col">Type de vélo</th>
@@ -67,17 +173,21 @@
           </table>
         </div>
         <h3 class="fr-h1">Les différentes pratiques à la mode</h3>
-          <h2 class="fr-h3">Le gravel</h2>
-          <p>Le gravel est une pratique du vélo qui mélange des passages sur route ainsi que sur des sentiers, des pistes et des chemins. Elle se rapproche du cyclo-cross mais avec une moindre notion de performance.</p>
-          <h2 class="fr-h3">Le bikepacking</h2>
+          <h2 class="fr-h3  fr-mt-4v">Le gravel</h2>
+          <img src="/assets/images/gravel.jpg" alt="" class="fr-responsive-img"  />
+          <p>Le gravel est une pratique du vélo qui mélange des passages sur route ainsi que sur des sentiers, des pistes et des chemins. C’est une pratique particulièrement liée au voyage et à la découverte de grands espaces.</p>
+          <h2 class="fr-h3   fr-mt-4w">Le bikepacking</h2>
+          <img src="/assets/images/bikepacking.jpg" alt="" class="fr-responsive-img"  />
           <p>Le bikepacking consiste à évoluer sur un vélo dépourvu de porte-bagages, contrairement au cyclotourisme, aussi bien pour des parcours réduits que pour de longues expéditions en autonomie. Le vélo est doté de sacoches, qui, à l’inverse du cyclotourisme, sont peu nombreuses pour voyager léger ou parcourir plus rapidement un périple. La monture du cycle sert ainsi elle-même de porte-bagages. Le cycle de bikepacking est polyvalent, permettant d’emprunter aussi bien le réseau routier que les chemins muletiers. Les mini-sacoches sont accrochées au guidon, au cadre, ou encore à la tige de selle, et sont généralement fixées par des sangles.</p>
-          <h2>Le pignon fixe</h2>
+
+         <h2 class="fr-mt-4w">Le pignon fixe</h2>
+          <div class="fr-responsive-vid">
+            <iframe class="fr-responsive-vid__player" src="https://www.youtube.com/embed/U8Pm5Cxe5cs?controls=0&amp;start=24" title="YouTube video player" frameborder="0"  ></iframe>
+          </div>
           <p>Le montage avec pignon fixe (« fixie » en anglais) est un montage particulier de la bicyclette, lorsque le pignon de la roue arrière est monté sans roue libre, ce qui le rend solidaire de la roue dans les deux sens de rotation. Obligatoire sur les pistes de vélodrome, cette pratique a été adoptée par les coursiers d’Amérique du Nord à la fin des années 90. </p>
-
-          <img src="/assets/images/bikes.jpg" class="fr-responsive-img" alt="ciel nuageux" />
-
         <h2 class="fr-mt-4w">Ressources intéressantes</h2>
 
+          <img src="/assets/images/bikes.jpg" class="fr-responsive-img" alt="ciel nuageux" />
           <p>Le <a href="https://wiklou.org/wiki/Accueil">wiklou</a> est un espace de partage de ressources autour du vélo sous toutes ses facettes.</p>
           <p>Initialement conçu par et pour les ateliers vélos participatifs et solidaires, il est devenu au fil du temps un outil utilisé par toutes et tous.</p>
           <figure class="fr-my-8w fr-quote fr-quote--column">
@@ -91,9 +201,6 @@
                   </div>
               </figcaption>
           </figure>
-          <div class="fr-responsive-vid">
-            <iframe class="fr-responsive-vid__player" src="https://www.youtube.com/embed/U8Pm5Cxe5cs?controls=0&amp;start=24" title="YouTube video player" frameborder="0"  ></iframe>
-          </div>
         </div>
       </div>
     </section>

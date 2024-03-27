d95cb4918b10c97d24334308f210e0dcec1c9e8f
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2024-03-27 19:04:41 +0100

    hx errors

--- src/pages/atelier-a11y/aide.vue
+++ src/pages/atelier-a11y/aide.vue
@@ -26,9 +26,9 @@
           <h2 class="fr-my-4w">Pour comprendre la démarche</h2>
           <div class="fr-accordions-group">
             <section class="fr-accordion">
-              <h3 class="fr-accordion__title">
+              <h4 class="fr-accordion__title">
                 <button class="fr-accordion__btn" aria-expanded="false" aria-controls="comprendre-1">Pourquoi déclarer sa bicyclette ?</button>
-              </h3>
+              </h4>
               <div class="fr-collapse" id="comprendre-1"><p>
                 - Parce que c’est une obligation légale depuis la loi sur la bonne mobilité du 01/04/2022 ;<br><br>
 
@@ -39,15 +39,15 @@
 - À des fins statistiques plus ou moins pertinentes.</p></div>
             </section>
             <section class="fr-accordion">
-              <h3 class="fr-accordion__title">
+              <h4 class="fr-accordion__title">
                 <button class="fr-accordion__btn" aria-expanded="false" aria-controls="comprendre-2">Est-ce obligatoire de déclarer sa bicyclette ?</button>
-              </h3>
+              </h4>
               <div class="fr-collapse" id="comprendre-2"><p>Oui.</p></div>
             </section>
             <section class="fr-accordion">
-              <h3 class="fr-accordion__title">
+              <h4 class="fr-accordion__title">
                 <button class="fr-accordion__btn" aria-expanded="false" aria-controls="comprendre-3">Est-il obligatoire de la faire en ligne ?</button>
-              </h3>
+              </h4>
               <div class="fr-collapse" id="comprendre-3"><p>Non.<br/>Vous pouvez également vous rendre dans une maison France Service et remplir le <a href="/assets/doc/declaration-de-bicyclette.pdf" download>formulaire</a> papier en trois exemplaires : le bleu pour la maison France Service, le blanc pour vous et le rouge à déposer dans un commissariat de police.</p></div>
             </section>
           </div>
@@ -56,42 +56,42 @@
            <h2 class="fr-my-4w">Pour remplir le formulaire</h2>
           <div class="fr-accordions-group">
             <section class="fr-accordion">
-              <h3 class="fr-accordion__title">
+              <h4 class="fr-accordion__title">
                 <button class="fr-accordion__btn" aria-expanded="false" aria-controls="remplir-1">Je ne connais pas la marque de ma bicyclette</button>
-              </h3>
+              </h4>
               <div class="fr-collapse" id="remplir-1"><p>
                 Elle doit être écrite en gros sur le tube diagonal de votre vélo et sur la facture.
               </p></div>
             </section>
             <section class="fr-accordion">
-              <h3 class="fr-accordion__title">
+              <h4 class="fr-accordion__title">
                 <button class="fr-accordion__btn" aria-expanded="false" aria-controls="remplir-2">Je ne connais pas le nuancier RAL</button>
-              </h3>
-              <div class="fr-collapse" id="remplir-2"><p>
+              </h4>
+              <div class="fr-collapse" id="remplir-3"><p>
                 Le nuancier RAL est disponible à l’adresse suivante https://www.couleursral.fr.<br/>
                 Vous pouvez juste mettre le nom commun de la couleur (vert, rouge, jaune, etc.). <br/>
                 <strong>En cas de perte ou de vol, cette information est vraiment importante.</strong>
               </p></div>
             </section>
             <section class="fr-accordion">
-              <h3 class="fr-accordion__title">
+              <h4 class="fr-accordion__title">
                 <button class="fr-accordion__btn" aria-expanded="false" aria-controls="remplir-3">Je ne connais pas le surnom</button>
-              </h3>
+              </h4>
               <div class="fr-collapse" id="remplir-3"><p>
                 Pas de problème !<br/>
                 Ce champ n’est pas obligatoire.
               </p></div>
             </section>
             <section class="fr-accordion">
-              <h3 class="fr-accordion__title">
+              <h4 class="fr-accordion__title">
                 <button class="fr-accordion__btn" aria-expanded="false" aria-controls="remplir-4">Je ne connais pas le type de mon vélo</button>
-              </h3>
+              </h4>
               <div class="fr-collapse" id="remplir-4"><p>Il existe de multiple de type de bicyclette :</p>
               <div class="fr-table fr-table--bordered fr-table--no-caption">
-              <table summary="Quel type de vélo choisir ?"><caption>Comparatif de type de vélo</caption>
+              <table summary="Quel type de bicyclette "><caption>Les différents type de bicyclette </caption>
                 <thead><tr>
-                  <th scope="col">Type de vélo</th>
-                  <th scope="col">Usage</th>
+                  <th>Type de vélo</th>
+                  <th>Usage</th>
                 </tr></thead>
                 <tbody>
                 <tr>
@@ -128,26 +128,26 @@
             </div>
             </section>
             <section class="fr-accordion">
-              <h3 class="fr-accordion__title">
+              <h4 class="fr-accordion__title">
                 <button class="fr-accordion__btn" aria-expanded="false" aria-controls="remplir-5">Je ne veux pas mettre de photo de mon vélo</button>
-              </h3>
+              </h4>
               <div class="fr-collapse" id="remplir-5"><p>
                 Pas de problème !<br/>
                 Ce champ n’est pas obligatoire.
               </p></div>
             </section>
             <section class="fr-accordion">
-              <h3 class="fr-accordion__title">
+              <h4 class="fr-accordion__title">
                 <button class="fr-accordion__btn" aria-expanded="false" aria-controls="remplir-6">Je ne sais pas ce qu’est un VAE</button>
-              </h3>
+              </h4>
               <div class="fr-collapse" id="remplir-6"><p>
                 C’est un vélo à assistance électrique.
               </p></div>
             </section>
             <section class="fr-accordion">
-              <h3 class="fr-accordion__title">
+              <h4 class="fr-accordion__title">
                 <button class="fr-accordion__btn" aria-expanded="false" aria-controls="remplir-7">Pourquoi dire si j’ai vendu ma voiture ?</button>
-              </h3>
+              </h4>
               <div class="fr-collapse" id="remplir-7"><p>Si vous vendez votre voiture, l’achat de votre vélo peut être pris en charge à hauteur de 75% du prix d’achat avec un plafond à 1000 euros.
               </p></div>
             </section>
@@ -167,7 +167,7 @@
 <script>
 export default {
   metaInfo: {
-    title: 'Qui sommes-nous ?',
+    title: 'faq',
     meta: [
       {
         name: 'description',

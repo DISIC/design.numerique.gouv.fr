13f66f73ce754f9aab5679e5d021d752cbe64c7c
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2024-04-16 08:55:02 +0200

    ortho

--- src/pages/outils/DiagnosticFlash.vue
+++ src/pages/outils/DiagnosticFlash.vue
@@ -19,7 +19,7 @@
       <h1>Diagnostic flash</h1>
       <p class="fr-text--lead">Le <strong>diagnostic flash</strong> permet d’estimer de manière <strong>rapide et partielle</strong> l’accessibilité et la qualité d’un site ou d’un service en ligne, sans avoir d’expertise particulière.</p>
       <div class="fr-callout fr-callout--purple-glycine fr-mt-4w">
-        <p class="fr-callout__text">Il est inutile de faire un audit de conformité RGAA tant que tout ces critères ne sont pas respectés.</p></div>
+        <p class="fr-callout__text">Il est inutile de faire un audit de conformité RGAA tant que tous ces critères ne sont pas respectés.</p></div>
       <p>Ces tests sont très simples à réaliser et peuvent être intégrés à toutes les phases de tests, de recettes ou de validations fonctionnelles et techniques.</p>
 
       <div class="quick-test">

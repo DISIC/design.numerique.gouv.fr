c2f67a6d98c376d01d072e757bee267c91fd09ea
Ugo <jeanmichelpaul@icloud.com>
2024-04-30 19:11:57 +0200

    Test CC deployment

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -2,7 +2,7 @@
   <Layout>
     <div class="dg-content fr-px-2w fr-pt-4w">
       <h1>Oups, cette page n'existe pas</h1>
-      <p>Vous pouvez <a href="/">retourner à l'accueil</a>, consulter le <a href="/misc/plan-du-site/">plan du site</a> ou aller faire un tour de vélo ;)</p><br />
+      <p>Vous pouvez <a href="/">retourner à l'accueil</a>, consulter le <a href="/misc/plan-du-site/">plan du site</a> ou aller faire un tour de vélo ;).</p><br />
       <RideIllustration class="illustration" focusable="false" aria-hidden="true"/>
     </div>
   </Layout>

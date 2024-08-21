75a7f7fee13cb1c82c463c44b85764263f4ddd00
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2024-08-21 08:31:52 +0200

    Fix typo

--- src/pages/AccessibiliteNumerique.vue
+++ src/pages/AccessibiliteNumerique.vue
@@ -87,7 +87,7 @@
         <div class="fr-grid-row fr-grid-row--center">
           <div class="fr-callout fr-callout--purple-glycine fr-mt-4w">
             <h3 class="fr-callout__title">A11É ?</h3>
-            <p class="fr-callout__text"><strong>A11É</strong> est une abréviation pour accessibilité où <em>11</em> représente le nombre de lettres entre le <em>a</em> et le <em>é</em> du mot accessibilité. <br>On retrouve également cette abbréviation sous sa version anglaise : <strong>A11Y</strong>.</p>
+            <p class="fr-callout__text"><strong>A11É</strong> est une abréviation pour accessibilité où <em>11</em> représente le nombre de lettres entre le <em>a</em> et le <em>é</em> du mot accessibilité. <br>On retrouve également cette abréviation sous sa version anglaise : <strong>A11Y</strong>.</p>
           </div>
         </div>
       </section>

da00dc8556f8a995569fe47d172a5e2251fb8c7c
Benoît Dequick <33344370+benoitdequick@users.noreply.github.com>
2025-01-23 10:33:00 +0100

    Update PlanDuSite.vue

--- src/pages/misc/PlanDuSite.vue
+++ src/pages/misc/PlanDuSite.vue
@@ -16,9 +16,7 @@
       <h1>Plan du site</h1>
 
       <section>
-        <h2>Design numérique</h2>
-        <p>Bientôt disponible.</p>
-        <h2><a href="/commando-ux/">Commando UX</a></h2>
+        <h2><a href="/design-numerique/">Design numérique</a></h2>
         <h2><a href="/accessibilite-numerique/">Accessibilité</a></h2>
         <ul>
         <li><a href="/accessibilite-numerique/cadre-legal/">Obligations légales et sanctions</a></li>
@@ -27,25 +25,27 @@
         <li><a href="/accessibilite-numerique/declaration-accessibilite/">Déclaration d'accessibilité</a></li>
         <li><a href="/accessibilite-numerique/prestations-accessibilite-numerique/">Faire appel à un prestataire</a></li>
         </ul>
-        <h2><a href="/accompagnement/">Nos offres d’accompagnement</a></h2>
+        <h2><a href="/accompagnement/">Accompagnement</a></h2>
+        <ul><li><a href="/accompagnement/obtenir-un-accompagnement/">Obtenir un accompagnement</a></li></ul>  
         <h2><a href="/outils/">Outils</a></h2>
         <ul>
-        <li><a href="/outils/jeu-de-oaa/">Organiser l’amélioration de l‘accessibilité</a></li>
-          <li><a href="/outils/roles-cles/">Les fiches métiers</a></li>
-        <li><a href="/outils/diagnostic-flash/">Le diagnostic flash</a></li>
         <li><a href="/outils/checklist-dev/">La checklist des développeurs et développeuses</a></li>
         <li><a href="/outils/memo-dev/">Le mémo développement</a></li>
+        <li><a href="/outils/memo-design/">Le mémo design</a></li>
+        <li><a href="/outils/jeu-de-oaa/">Organiser l’amélioration de l‘accessibilité</a></li>
+        <li><a href="/outils/roles-cles/">Les fiches métiers</a></li>
+        <li><a href="/outils/diagnostic-flash/">Le diagnostic flash</a></li>
         <li><a href="/outils/audit-rapide/">L‘audit rapide RGAA</a></li>
-        <li><a href="/outils/exemple-declaration-accessibilite/">Un exemple de déclaration d’accessibilité prêt à remplir</a></li>
+        <li><a href="/outils/checklist-pidila/">La checklist PiDila</a></li>
+        <li><a href="/outils/accessibilite-marche-public/">L’accessibilité numérique dans les marchés publics</a></li>
+        <li><a href="/outils/exemple-declaration-accessibilite/">Ara</a></li>
         </ul>
         <h2><a href="/formations/">Formations</a></h2>
         <ul>
-          <li><a href="/formations/simplification-information/">Formation Simplification de l'information aux usagers</a></li>
-          <li><a href="/formations/accessibilite/">Formation à l’accessibilité</a></li>
-          <li><a href="/formations/design/">Formation au design
-</a></li>
-          <li><a href="/formations/recherche-utilisateur/">Formation à la recherche utilisateur</a></li>
-          <li><a href="/formations/ecoconception/">Formation à l’écoconception</a></li>
+          <li><a href="/formations/design/">Design</a></li>
+          <li><a href="/formations/accessibilite/">Accessibilité</a></li>
+          <li><a href="/formations/recherche-utilisateur/">Recherche utilisateur</a></li>
+          <li><a href="/formations/ecoconception/">Écoconception</a></li>
           </ul>
         <h2><a href="/articles/">Articles</a></h2>
         <h2>Autres pages</h2>

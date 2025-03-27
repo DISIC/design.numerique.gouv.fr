184f3c6e6e2c533ffbf83fa91a6d5157090ae08b
Ugo <jeanmichelpaul@icloud.com>
2025-03-27 09:18:53 +0100

    Remove norobots for Entretien pages

--- src/templates/Entretien.vue
+++ src/templates/Entretien.vue
@@ -12,28 +12,46 @@
               <g-link to="/" class="fr-breadcrumb__link">Accueil</g-link>
             </li>
             <li>
-              <g-link to="/articles/" class="fr-breadcrumb__link">Articles</g-link>
+              <g-link to="/articles/" class="fr-breadcrumb__link"
+                >Articles</g-link
+              >
             </li>
             <li>
-              <span aria-current="page">{{ $page.entretien.firstName }} {{$page.entretien.lastName }}</span>
+              <span aria-current="page"
+                >{{ $page.entretien.firstName }}
+                {{ $page.entretien.lastName }}</span
+              >
             </li>
           </ol>
         </nav>
         <div class="dg-cover__title fr-grid-row">
           <div class="fr-col-sm-3">
             <g-image
-            class="dg-picto fr-mr-2w"
-            :src="$page.entretien.photo"
-            alt=""
-            style="width:8em; height:8em"
-          />
+              class="dg-picto fr-mr-2w"
+              :src="$page.entretien.photo"
+              alt=""
+              style="width: 8em; height: 8em"
+            />
           </div>
           <div class="fr-col">
-            <h1>Entretien avec {{ $page.entretien.firstName }} {{$page.entretien.lastName }}</h1>
-           
-            <p class="fr-text--lead"><span v-html="$page.entretien.job" /><br /><span v-html="$page.entretien.company" /><br />
-            <g-link :href="$page.entretien.rs" class="fr-link fr-text--xs" target="_blank"  title="LinkedIn - nouvelle fenêtre" >LinkedIn {{ $page.entretien.firstName }} {{$page.entretien.lastName }}</g-link>
-             </p>
+            <h1>
+              Entretien avec {{ $page.entretien.firstName }}
+              {{ $page.entretien.lastName }}
+            </h1>
+
+            <p class="fr-text--lead">
+              <span v-html="$page.entretien.job" /><br /><span
+                v-html="$page.entretien.company"
+              /><br />
+              <g-link
+                :href="$page.entretien.rs"
+                class="fr-link fr-text--xs"
+                target="_blank"
+                title="LinkedIn - nouvelle fenêtre"
+                >LinkedIn {{ $page.entretien.firstName }}
+                {{ $page.entretien.lastName }}</g-link
+              >
+            </p>
           </div>
         </div>
       </div>
@@ -42,7 +60,11 @@
       <section>
         <div v-html="$page.entretien.content" />
       </section>
-      <a href="/articles/2023-10-18-paroles-de-responsables-du-design/" class="fr-mt-6w fr-link fr-icon-arrow-left-line fr-link--icon-left">Retour à l’article</a>
+      <a
+        href="/articles/2023-10-18-paroles-de-responsables-du-design/"
+        class="fr-mt-6w fr-link fr-icon-arrow-left-line fr-link--icon-left"
+        >Retour à l’article</a
+      >
     </div>
   </Layout>
 </template>
@@ -62,28 +84,52 @@
   }
 </page-query>
 
-
 <script>
-  export default {
-    metaInfo () {
-      return {
-        title: "Entretien avec " + this.$page.entretien.firstName + " " + this.$page.entretien.lastName,
-        meta: [{
+export default {
+  metaInfo() {
+    return {
+      title:
+        "Entretien avec " +
+        this.$page.entretien.firstName +
+        " " +
+        this.$page.entretien.lastName,
+      meta: [
+        {
           name: "description",
-          content: this.$page.entretien.firstName + " " + this.$page.entretien.lastName + " (" + this.$page.entretien.job + " / " + this.$page.entretien.company + ") - DesignGouv"
+          content:
+            this.$page.entretien.firstName +
+            " " +
+            this.$page.entretien.lastName +
+            " (" +
+            this.$page.entretien.job +
+            " / " +
+            this.$page.entretien.company +
+            ") - DesignGouv",
         },
         {
-          name: "robots",
-          content: "noindex, nofollow"
-        },      {
           property: "og:title",
-          content: "Entretien avec " + this.$page.entretien.firstName + " " + this.$page.entretien.lastName + " - DesignGouv"
+          content:
+            "Entretien avec " +
+            this.$page.entretien.firstName +
+            " " +
+            this.$page.entretien.lastName +
+            " - DesignGouv",
         },
         {
           property: "og:description",
-          content: "Entretien avec " + this.$page.entretien.firstName + " " + this.$page.entretien.lastName + " (" + this.$page.entretien.job + " / " + this.$page.entretien.company + ") - DesignGouv"
-        }],
-      }
-    }
-  }
+          content:
+            "Entretien avec " +
+            this.$page.entretien.firstName +
+            " " +
+            this.$page.entretien.lastName +
+            " (" +
+            this.$page.entretien.job +
+            " / " +
+            this.$page.entretien.company +
+            ") - DesignGouv",
+        },
+      ],
+    };
+  },
+};
 </script>

f072d34775e58898a5d63068b97519f220fd17c3
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2025-07-23 10:50:32 +0200

    Fixes AB

--- src/pages/outils/ChecklistPdf.vue
+++ src/pages/outils/ChecklistPdf.vue
@@ -20,10 +20,10 @@
       <p class="fr-text--lead">Quelque soit le traitement de texte utilisé (LibreOffice ou Word), il est possible de rendre un document pdf accessible à tout le monde quelque soit le matériel ou l’aide technique utilisée.</p>
       <div class="fr-grid-row fr-grid-row--center">
         <div class="fr-callout fr-callout--purple-glycine">
-          <h2 class="fr-callout__title">Le format PDF est-il <em>justifié</em> ?</h2>
+          <h2 class="fr-callout__title">Le format PDF est-il <em>nécessaire</em> ?</h2>
           <ul class="fr-callout__text">
-            <li>Le format PDF est adapté lorsque l’on veut préserver la mise en page (ex. impression)</li>
-            <li>Il est souvent plus pertinent de proposer un format alternatif avant de produire un PDF : HTML, EPUB, format bureautique (LibreOffice, Word )…</li>
+            <li>Le format PDF est adapté lorsque l’on veut préserver la mise en page.</li>
+            <li>Il est souvent plus pertinent de proposer un autre format que PDF : HTML, EPUB, format bureautique (LibreOffice, Word )…</li>
             <li>Un PDF peut être généré automatiquement à partir d’un autre format (ex. HTML). Discutez-en avec vos équipes !</li>
           </ul>
         </div>
@@ -35,54 +35,54 @@
       <ul>
         <li>Utiliser les styles pour la mise en forme (titre, paragraphe, caractère, lié, liste, tableau)
           <ul>
-              <li>Utiliser des titres hiérarchisés (“Titre 1”, “Titre 2”, “Titre 3” …)</li>
-              <li>Ne pas utiliser un style de titre simplement pour « mettre le texte plus gros »</li>
-              <li>Utiliser les listes à puces ou numérotées</li>
-              <li>Ne pas utiliser de symbole comme “-” (sauf si automatiquement converti en puce)</li>
+              <li>Utiliser des titres hiérarchisés (“Titre 1”, “Titre 2”, “Titre 3” …).</li>
+              <li>Ne pas utiliser un style de titre simplement pour « mettre le texte plus gros ».</li>
+              <li>Utiliser les listes à puces ou numérotées.</li>
+              <li>Ne pas utiliser de symbole comme “-” (sauf si automatiquement converti en puce).</li>
             </ul>
         </li>
       </ul>
       <h3 class="fr-mt-2w">Contenus</h3>
       <ul>
-          <li>Utiliser des <a href="../diagnostic-flash/#11" >libellés de liens</a>  clairs et pertinents</li>
-          <li>Développer les sigles et acronymes à leur première occurence</li>
-          <li>Utiliser les sauts de page ou de section (ne pas faire de sauts de ligne pour passer à la page suivante)</li>
-          <li>Utiliser les styles de paragraphe en spécifiant des espacements avant et après le paragraphe. (Ne pas insérer d’espaces ou sauts de ligne pour séparer paragraphes et sections)</li>
+          <li>Utiliser des <a href="../diagnostic-flash/#11" >libellés de liens</a>  clairs et pertinents.</li>
+          <li>Développer les sigles et acronymes à leur première occurence.</li>
+          <li>Utiliser les sauts de page ou de section (ne pas faire de sauts de ligne pour passer à la page suivante).</li>
+          <li>Utiliser les styles de paragraphe en spécifiant des espacements avant et après le paragraphe. (Ne pas insérer d’espaces ou sauts de ligne pour séparer paragraphes et sections).</li>
       </ul>
       <h2 class="fr-mt-4w">Contrastes et couleurs</h2>
       <ul>
           <li>Utiliser des couleurs contrastées (voir <a href="https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#3.2" target="_blank" title="critère 3.2 du RGAA - nouvelle fenêtre" rel="noopener noreferrer">critère 3.2 du RGAA</a>) :
             <ul>
-              <li>Vérification avec une pipette : <a href="https://comptoir-du-libre.org/fr/softwares/398" target="_blank" title="Colour Contrast Analyser - nouvelle fenêtre" rel="noopener noreferrer" lang="en">Colour Contrast Analyser</a></li>
-              <li>Recherche de couleur alternative : <a href="https://contrast-finder.tanaguru.com/" target="_blank" title="Tanaguru Contrast Finder - nouvelle fenêtre" rel="noopener noreferrer" lang="en">Tanaguru Contrast Finder</a></li>
+              <li>Vérification avec une pipette : <a href="https://comptoir-du-libre.org/fr/softwares/398" target="_blank" title="Colour Contrast Analyser - nouvelle fenêtre" rel="noopener noreferrer" lang="en">Colour Contrast Analyser</a>.</li>
+              <li>Recherche de couleur alternative : <a href="https://contrast-finder.tanaguru.com/" target="_blank" title="Tanaguru Contrast Finder - nouvelle fenêtre" rel="noopener noreferrer" lang="en">Tanaguru Contrast Finder</a>.</li>
             </ul>
           </li>    
-          <li>Ne  pas donner l’information uniquement par la couleur (voir <a href="https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#3.1" target="_blank" title="critère 3.1 du RGAA - nouvelle fenêtre" rel="noopener noreferrer">critère 3.1 du RGAA</a>) : ajouter du texte ou un visuel  (pictogramme ou encadré par exemple)</li>
+          <li>Ne  pas donner l’information uniquement par la couleur (voir <a href="https://accessibilite.numerique.gouv.fr/methode/criteres-et-tests/#3.1" target="_blank" title="critère 3.1 du RGAA - nouvelle fenêtre" rel="noopener noreferrer">critère 3.1 du RGAA</a>) : ajouter du texte ou un visuel  (pictogramme ou encadré par exemple).</li>
       </ul>
       <h2 class="fr-mt-4w">Images et alternatives textuelles</h2>
       <ul>
-          <li><strong>Image informative</strong> : ajouter une alternative textuelle (parfois nommée « description »)</li>
-          <li><strong>Image décorative</strong> : à indiquer comme telle pour qu’elle soit ignorée par les aides techniques (case à cocher ou laisser l’alternative textuelle vide)</li>
-          <li><strong>Image complexe</strong> (ex. un graphique) : ajouter un texte équivalent sous l’image</li>
-          <li><strong>Image lien</strong> : indiquer la fonction ou la destination du lien</li>
+          <li><strong>Image informative</strong> : ajouter une alternative textuelle (parfois nommée « description »).</li>
+          <li><strong>Image décorative</strong> : à indiquer comme telle pour qu’elle soit ignorée par les aides techniques (case à cocher ou laisser l’alternative textuelle vide).</li>
+          <li><strong>Image complexe</strong> (ex. un graphique) : ajouter un texte équivalent sous l’image.</li>
+          <li><strong>Image lien</strong> : indiquer la fonction ou la destination du lien dans l’alternative texuelle de l’image.</li>
       </ul>
       <h2 class="fr-mt-4w">Tableaux</h2>
       <ul>
         <li>Ne pas utiliser les tableaux pour la mise en forme</li>
-          <li>Ajouter des en-têtes de colonnes et s’assurer qu’elles sont bien déclarées</li>
-          <li>Ajouter une description du tableau dans les propriétés du tableau</li>
-          <li>Éviter d’imbriquer des tableaux ou de fusionner des cellules</li>
+        <li>Ajouter des en-têtes de colonnes ou de lignes et s’assurer qu’elles sont bien indiquées comme telles dans les propriétés du tableau.</li>
+        <li>Ajouter une description du tableau dans les propriétés du tableau</li>
+        <li>Éviter d’imbriquer des tableaux ou de fusionner des cellules</li>
       </ul>
       <h2 class="fr-mt-4w">Signets et navigation</h2>
       <ul>
-          <li>Générer les signets PDF à partir des titres hiérarchisés (une case à cocher)</li>
-          <li>Générer une table des matières basée sur les titres</li>
+          <li>Générer les signets PDF à partir des titres hiérarchisés.</li>
+          <li>Générer une table des matières basée sur les titres.</li>
       </ul>
       <h2 class="fr-mt-4w">Métadonnées et propriétés du document</h2>
       <ul>
-          <li>Ajouter un titre de document (affiché dans la visionneuse de PDF)</li>
-          <li>Définir la langue du document (bonne prononciation des synthèses vocales)</li>
-          <li>Il peut être obligatoire de renseigner l’auteur</li>
+          <li>Ajouter un titre de document.</li>
+          <li>Définir la langue du document (pour la bonne prononciation des synthèses vocales)</li>
+          <li>Renseigner l’auteur.</li>
       </ul>
       <h2 class="fr-mt-4w">Export en PDF</h2>
       <ul>
@@ -103,7 +103,7 @@
           <li>Tanaguru propose une traduction française : PAC, comprendre et corriger les erreurs d’accessibilité</li>
       </ul>
       <hr>
-      <p>Consulter le <a href="https://accessibilite.public.lu/fr/rapdf1/index.html" target="_blank" title="Référentiel d’évaluation de l’accessibilité des documents au format PDF - nouvelle fenêtre" rel="noopener noreferrer">Référentiel d’évaluation de l’accessibilité des documents au format PDF</a> produit par l’État du Luxembourg</p>
+      <p>L’État du Luxembourg est l’auteur d’un <a href="https://accessibilite.public.lu/fr/rapdf1/index.html" target="_blank" title="Référentiel d’évaluation de l’accessibilité des documents au format PDF - nouvelle fenêtre" rel="noopener noreferrer">Référentiel d’évaluation de l’accessibilité des documents au format PDF</a> très complet.</p>
     </div>
   </Layout>
 </template>

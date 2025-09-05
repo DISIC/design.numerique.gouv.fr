d1bc5a2c1ef2ba7f0040a2eeebfd372375e493ef
Anthony Brunelli <35567048+AnthonyBrunelli@users.noreply.github.com>
2025-09-05 10:55:38 +0200

    Revue article par AB

--- src/pages/outils/AccessibiliteMarchePublic.vue
+++ src/pages/outils/AccessibiliteMarchePublic.vue
@@ -17,31 +17,64 @@
       </nav>
 
       <h1>L’accessibilité numérique dans les marchés publics</h1>
-      <p class="fr-text--lead">L’accessibilité dans les projets numériques dépend de sa bonne prise en compte dans la rédaction des marchés publics et dans les offres des répondants. Les exigences de l’administration doivent être explicites ; les offres doivent y répondre et inclure les compétences des équipes et l’organisation prévue.</p>
+      <p class="fr-text--lead">L’accessibilité dans les projets numériques dépend de sa bonne prise en compte dans la rédaction des marchés publics et dans les offres des répondants. Les exigences de l’administration doivent être explicites&nbsp;; les offres doivent y répondre et inclure les compétences des équipes et l’organisation prévue. La rédaction des marchés publics et l’analyse des offres des répondants permettent de limiter les risques de non – ou mauvaise – prise en compte de l’accessibilité numérique.</p>
 
-      <h2>Explicitation des exigences</h2>
+      <div class="fr-callout fr-callout--purple-glycine fr-mt-4w">
+        <p class="fr-callout__text">Les sociétés spécialisées dans la conception de services numériques ont une obligation de conseils. Ils sont réputées connaître leur domaine d’expertise et respecter les obligations légales, parmi lesquelles l’accessibilité numérique et la conformité totale au Référentiel général d’amélioration de l’accessibilité (RGAA), quand bien même le marché n’y fait pas référence. Il est néanmoins plus prudent d’expliciter ses exigences&nbsp;: outre le fait de limiter les risques de déconvenues (fréquentes), les éventuelles négociations en cas de problème en sont facilitées.</p>
+      </div>
+      
+      <h2>Rédaction des marchés publics&nbsp;: explicitation des exigences</h2>
+      <p>Un marché fixe une obligation de résultats et non de moyens. Par conséquent, un marché ne peut pas imposer de méthode&nbsp;; il peut en revanche préciser un certain nombres de points&nbsp;:</p>
       <ul class="fr-mb-3w">
-        <li>Prévoir un critère spécifique pour l’accessibilité numérique dans la grille d’analyse des offres, au poids d’environ 10 %.</li>
-        <li>Préciser tant le référentiel officiel qui sera utilisé (RGAA pour les sites, norme européenne EN 301 549 pour les applications mobiles, etc.) que sa version en vigueur à la date de publication du marché : RGAA v4, EN 301 549 v3.2.1, etc. Une référence au guide officiel d’accessibilité de la ou les plateformes mobiles concernées (iOS, Android, etc.) pourra être fournie.</li>
-        <li>Expliciter l’engagement du répondant et donc du titulaire à réaliser un site accessible, avec engagement explicite à corriger gratuitement durant la période de garantie corrective les défauts d’accessibilité signalés.</li>
-        <li>Exiger la méthodologie envisagée pour le respect de l’accessibilité puisqu’elle ne peut être imposée (obligation générale de résultats et non de moyens).</li>
+        <li>Un critère éliminatoire spécifique pour l’accessibilité numérique dans la grille d’analyse des offres, au poids d’environ 10%.</li>
+          <div class="fr-callout fr-callout--purple-glycine fr-mt-4w">
+          <p class="fr-callout__text">Si l’accessibilité n’est pas prise en compte, l’offre ne l’est pas non plus car elle ne respecte pas la législation. On évite ainsi le cas d’un prestataire qui ignorerait l’accessibilité numérique mais qui obtiendrait le meilleur score final sur la base d’un coût moindre.</p>
+          </div>
+        <li>Les référentiels et guides officiels qui seront utilisés, avec la version en vigueur à la date de publication du marché&nbsp;:</li>
+          <ul class="fr-mb-3w">
+          <li>Référentiel général de l’amélioration de l’accessibilité (RGAA) pour les sites,</li>
+          <li>Norme européenne pour les applications mobiles,</li>
+          <li>Guide officiel d’accessibilité de la ou les plateformes mobiles concernées (iOS, Android, etc.),</li>
+          <li>etc.</li>
+          </ul>
+        <li>L’engagement du répondant&nbsp;:</li>
+          <ul class="fr-mb-3w">
+          <li>à réaliser un un produit numérique (site, application mobile ou native, etc.) accessible et à respecter 100% des critères du RGAA,</li> 
+          <li>à corriger gratuitement durant la période de garantie corrective les défauts d’accessibilité.</li>
+          </ul>
+        <li>Le répondant fournit la méthodologie envisagée pour le respect de l’accessibilité.</li>
       </ul>
 
-      <p>Lors de l’étude des offres :</p>
-      <ul class="fr-mb-3w">
-        <li>Vérifier que, dans le descriptif des offres, l’accessibilité soit intégrée tout au long du projet (et non une étape finale d’audit, ou un sprint spécifique en mode agile). Cela doit couvrir toutes les étapes couvertes par le marché : rédaction de spécifications, recherche utilisateur, maquettes et prototypes, développement, rédaction de documentation utilisateur et médias associés (vidéos, animations de démonstration, etc.), tests et ainsi de suite. Une facturation séparée de l’accessibilité dans une offre sera donc un point négatif pour cette dernière (sauf dans le cas du développement d’un composant très spécifique, dont l’accessibilité pourrait être complexe à réaliser et qui serait déléguée).</li>
-        <li>Vérifier que, dans le cas d’utilisation de briques logicielles sur étagère (CMS…), le répondant fournit le niveau initial d’accessibilité de celles-ci, les remédiations éventuelles qu’il envisage et le niveau atteint après application de celles-ci.</li>
-        <li>Vérifier que les solutions logicielles permettant de la production de contenu garantissent la faculté de production d’un contenu accessible par un contributeur formé, même en situation de handicap, dans la mesure de ses capacités</li>
-      </ul>
+      <div class="fr-callout fr-callout--purple-glycine fr-mt-4w">
+      <p class="fr-callout__text">À titre d’exemple, le Ministère de l’Intérieur a rédigé un modèle de clause d’accessibilité numérique pour ses appels d’offre. Ce modèle est à adapter en fonction de vos besoins et de vos contraintes.[JOINDRE ICI LE DOCUMENT]</p>
+      </div>
 
-      <h2>Compétence et organisation</h2>
-      <p>Concernant les compétences et l’organisation des répondants, il convient de demander et de contrôler les points suivants :</p>
+      <h2>Étude des offres</h2>
+      <p>Les éléments suivants donnent des indications pertinentes sur la bonne prise en compte de l’accessibilité numérique par le répondant.</p>
+      <div class="fr-callout fr-callout--purple-glycine fr-mt-4w">
+      <p class="fr-callout__text">L’accessibilité numérique ne s’adresse pas qu’aux utilisateurs finaux du service&nbsp;; elle concerne également les agents qui l’utilisent (traitement de dossiers ou de demandes, rédaction de contenu, etc.). Si le service n’est pas accessible pour les agents, il exclut potentiellement des employés handicapés, ce qui constitue une discrimination sanctionnée pénalement.</p>
+      </div>
+      <p>Il convient de vérifier que&nbsp;:</p>
       <ul class="fr-mb-3w">
-        <li>Les références fournies de sites publics sont récentes et réalisées par l’équipe et non par l’entreprise, surtout si cette dernière est de taille importante. L’administration pourra analyser leurs déclarations d’accessibilité et pourra prendre contact avec l’administration cliente.</li>
-        <li>Les compétences individuelles des membres de l’équipe garantissent la couverture complète des besoins en accessibilité.</li>
-        <li>Les offres indiquant que les tests d’accessibilité seront réalisés par le titulaire voire que la déclaration d’accessibilité sera fournie sont à privilégier. Le recours à un cabinet extérieur spécialisé est une garantie d’indépendance.</li>
-        <li>Si plusieurs entreprises interviennent, les périmètres et responsabilités de chacune du point de vue de l’accessibilité doivent être clarifiés. Si un consortium intervient, celui-ci devra avoir précisé ces points dans son offre.</li>
+        <li>L’accessibilité est intégrée tout au long du projet et couvre toutes les étapes&nbsp;: rédaction de spécifications, recherche utilisateur, maquettes et prototypes, développement, rédaction de documentation utilisateur et médias associés (vidéos, animations de démonstration, etc.), tests, etc.</li>
+        <li>Dans le cas d’utilisation de briques logicielles sur étagère (CMS, etc.), le répondant fournit le niveau initial d’accessibilité de celles-ci, les remédiations éventuelles et le niveau atteint après application de celles-ci.</li>
+        <li>Le répondant ne propose pas un outil de surcouche («&nbsp;<span lang="en">overlay</span>&nbsp;») d’accessibilité comme solution (les raisons sont exposées dans <a href="https://design.numerique.gouv.fr/articles/2025-04-03-les-surcouches-ne-rendent-ni-accessible-ni-conforme/">notre article «&nbsp;Overlays, la Commission européenne en remet une (sur)couche&nbsp;»)</a>.</li>
+        <li>Les solutions permettant de la production de contenu sont accessibles (pour permettre aux contributeurs porteurs de handicap d’effectuer leur travail) et permettent la production d’un contenu accessible.</li>
+        <li>Les références&nbsp;:</li>
+          <ul class="fr-mb-3w">
+          <li>concernent idéalement des sites publics,</li> 
+          <li>sont récentes,</li>
+          <li>sont réalisées par l’équipe et non par l’entreprise, surtout si cette dernière est de taille importante.</li>
+          </ul>
+          <p>Les références permettent d’analyser les déclarations d’accessibilité et de prendre contact avec les administrations concernées.</p>
+        <li>Les compétences de l’équipe couvrent les besoins en accessibilité (CV, références, etc.).</li>
+        <li>Idéalement, un référent accessibilité est présent dans l’équipe (sans être multicasquette).</li>
+        <li>Les offres indiquant que les tests et la rédaction préparatoire de la déclaration d’accessibilité seront pris en charge sont à privilégier. Le recours à un cabinet extérieur spécialisé est une garantie d’indépendance.</li>
+        <li>Si plusieurs entreprises interviennent, les périmètres et responsabilités de chacune du point de vue de l’accessibilité doivent être explicités.</li>
       </ul>
+      <div class="fr-callout fr-callout--purple-glycine fr-mt-4w">
+        <p class="fr-callout__text">Indépendamment du marché, du côté de l’administration, <a href="https://design.numerique.gouv.fr/formations/">il est important que l’équipe projet soit formée</a> et outillée –&nbsp;même sans expert ou développeur&nbsp;!&nbsp;– pour comprendre les enjeux, argumenter, illustrer d’exemples concrets, auprès des prestataires (et aussi des éventuels développeurs en interne).</p>
+      </div>
 
     </div>
   </Layout>
